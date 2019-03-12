4658e7868c1136c7daf44b866e2c875620fd3668
U. Artie Eoff <ullysses.a.eoff@intel.com>
2019-03-12 10:55:03 -0700

    ffmpeg-qsv/decode: refactor tests for better code reuse
    This improves code reuse and design for ffmpeg-qsv/encode
    tests.
    
    10bit decode tests have moved into its own sub-directory.
    
    Updated test names in baseline.
    
    Update tools/xform-baseline-1.sh script to convert old
    baseline test names to new test names.
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/ffmpeg-qsv/decode/10bit/hevc.py
+++ test/ffmpeg-qsv/decode/10bit/hevc.py
@@ -4,11 +4,11 @@
 ### SPDX-License-Identifier: BSD-3-Clause
 ###
 
-from ....lib import *
-from ..util import *
-from .decoder import DecoderTest
+from .....lib import *
+from ...util import *
+from ..decoder import DecoderTest
 
-spec = load_test_spec("vp8", "decode")
+spec = load_test_spec("hevc", "decode", "10bit")
 
 class default(DecoderTest):
   def before(self):
@@ -16,14 +16,14 @@ class default(DecoderTest):
     self.metric = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0)
     super(default, self).before()
 
-  @platform_tags(VP8_DECODE_PLATFORMS)
-  @slash.requires(have_ffmpeg_vp8_qsv_decode)
+  @platform_tags(HEVC_DECODE_10BIT_PLATFORMS)
+  @slash.requires(have_ffmpeg_hevc_qsv_decode)
   @slash.parametrize(("case"), sorted(spec.keys()))
   def test(self, case):
     vars(self).update(spec[case].copy())
     vars(self).update(
       case      = case,
-      ffdecoder = "vp8_qsv",
-      hwformat  = "nv12",
+      ffdecoder = "hevc_qsv",
+      hwformat  = "p010le",
     )
     self.decode()
--- test/ffmpeg-qsv/decode/hevc.py
+++ test/ffmpeg-qsv/decode/hevc.py
@@ -6,77 +6,25 @@
 
 from ....lib import *
 from ..util import *
+from .decoder import DecoderTest
 
 spec = load_test_spec("hevc", "decode", "8bit")
 
-@slash.requires(have_ffmpeg)
-@slash.requires(have_ffmpeg_qsv_accel)
-@slash.requires(have_ffmpeg_hevc_qsv_decode)
-@slash.requires(using_compatible_driver)
-@slash.parametrize(("case"), sorted(spec.keys()))
-@platform_tags(HEVC_DECODE_8BIT_PLATFORMS)
-def test_8bit(case):
-  params = spec[case].copy()
+class default(DecoderTest):
+  def before(self):
+    # default metric
+    self.metric = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0)
+    super(default, self).before()
 
-  params.update(mformat = mapformat(params["format"]))
-  if params["mformat"] is None:
-    slash.skip_test("{format} format not supported".format(**params))
+  @platform_tags(HEVC_DECODE_8BIT_PLATFORMS)
+  @slash.requires(have_ffmpeg_hevc_qsv_decode)
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      case      = case,
+      ffdecoder = "hevc_qsv",
+      hwformat  = "nv12",
+    )
+    self.decode()
 
-  params["decoded"] = get_media()._test_artifact(
-    "{}_{width}x{height}_{format}.yuv".format(case, **params))
-
-  output = call(
-    "ffmpeg -hwaccel qsv -hwaccel_device /dev/dri/renderD128 -v verbose"
-    " -c:v hevc_qsv -load_plugin hevc_hw"
-    " -i {source} -vf 'hwdownload,format=nv12' -pix_fmt {mformat}"
-    " -f rawvideo -vsync passthrough"
-    " -vframes {frames} -y {decoded}".format(**params))
-
-  m = re.search("not supported for hardware decode", output, re.MULTILINE)
-  assert m is None, "Failed to use hardware decode"
-
-  m = re.search("Initialize MFX session", output, re.MULTILINE)
-  assert m is not None, "It appears that the QSV plugin did not load"
-
-  params.setdefault(
-    "metric", dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0))
-  check_metric(**params)
-
-#-------------------------------------------------#
-#---------------------10BIT-----------------------#
-#-------------------------------------------------#
-
-spec10 = load_test_spec("hevc", "decode", "10bit")
-
-@slash.requires(have_ffmpeg)
-@slash.requires(have_ffmpeg_qsv_accel)
-@slash.requires(have_ffmpeg_hevc_qsv_decode)
-@slash.requires(using_compatible_driver)
-@slash.parametrize(("case"), sorted(spec10.keys()))
-@platform_tags(HEVC_DECODE_10BIT_PLATFORMS)
-def test_10bit(case):
-  params = spec10[case].copy()
-
-  params.update(mformat = mapformat(params["format"]))
-  if params["mformat"] is None:
-    slash.skip_test("{format} format not supported".format(**params))
-
-  params["decoded"] = get_media()._test_artifact(
-    "{}_{width}x{height}_{format}.yuv".format(case, **params))
-
-  output = call(
-    "ffmpeg -hwaccel qsv -hwaccel_device /dev/dri/renderD128 -v verbose"
-    " -c:v hevc_qsv -load_plugin hevc_hw"
-    " -i {source} -vf 'hwdownload,format=p010le' -pix_fmt {mformat}"
-    " -f rawvideo -vsync passthrough"
-    " -vframes {frames} -y {decoded}".format(**params))
-
-  m = re.search("not supported for hardware decode", output, re.MULTILINE)
-  assert m is None, "Failed to use hardware decode"
-
-  m = re.search("Initialize MFX session", output, re.MULTILINE)
-  assert m is not None, "It appears that the QSV plugin did not load"
-
-  params.setdefault(
-    "metric", dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0))
-  check_metric(**params)
