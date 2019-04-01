fe8fe1a5e996bdf4b55477d51cb2ff3b88158be1
Wang Zhanjun <zhanjunx.wang@intel.com>
2019-03-31 20:59:50 -0400

    gst-vaapi: AVC Transcoding feature support
      HWtoHW, HWtoSW and SWtoHW avc transcoding support for gst-vaapi
    
    Signed-off-by: Wang Zhanjun <zhanjunx.wang@intel.com>

---
+++ test/gst-vaapi/transcode/transcoder.py
@@ -0,0 +1,54 @@
+###
+### Copyright (C) 2018-2019 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ..util import *
+import os
+
+@slash.requires(have_gst)
+@slash.requires(*have_gst_element("vaapi"))
+@slash.requires(*have_gst_element("checksumsink2"))
+class TranscoderTest(slash.Test):
+  def before(self):
+    self.refctx = []
+
+  def transcode_1to1(self):
+    self.transcoded = get_media()._test_artifact(
+      "{case}_{width}x{height}_{mode}.{dstextension}".format(**vars(self)))
+    call("gst-launch-1.0 -vf filesrc location={source} ! {gsttrans}"
+      " ! filesink location={transcoded}".format(**vars(self)))
+
+    self.convert_toyuv()
+    self.check_metrics()
+
+  def convert_toyuv(self):
+    self.decodedYUV = get_media()._test_artifact(
+       "{case}_{width}x{height}_{mode}_{trans_type}.yuv".format(**vars(self)))
+    call("gst-launch-1.0 -vf filesrc location={transcoded}"
+      " ! {gstdecoder2}"
+      " ! videoconvert ! video/x-raw,format=I420"
+      " ! checksumsink2 file-checksum=false qos=false"
+      " frame-checksum=false plane-checksum=false dump-output=true"
+      " dump-location={decodedYUV}".format(**vars(self)))
+
+    #src file to yuv
+    self.referenceFile = get_media()._test_artifact(
+       "{case}_{width}x{height}_{mode}_ref.yuv".format(**vars(self)))
+    call("gst-launch-1.0 -vf filesrc location={source}"
+      " ! {gstdecoder1}"
+      " ! videoconvert ! video/x-raw,format=I420"
+      " ! checksumsink2 file-checksum=false qos=false"
+      " frame-checksum=false plane-checksum=false dump-output=true"
+      " dump-location={referenceFile}".format(**vars(self)))
+
+  def check_metrics(self):
+    get_media().baseline.check_psnr(
+      psnr = calculate_psnr(
+        self.referenceFile, self.decodedYUV,
+        self.width, self.height,
+        self.frames),
+      context = self.refctx,
+    )
