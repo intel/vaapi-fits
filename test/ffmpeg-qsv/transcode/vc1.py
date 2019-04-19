6bc1f69492d0bc4603bafd196a3fd1382d518266
hbomminx <haribabux.bommineni@intel.com>
2019-04-19 11:19:56 -0700

    ffmpeg-qsv/transcode: transcode 1to N tests added
        1. Refactored transcode test scripts to support transcode 1toN tests
        2. It supports AVC/HEVC to AVC/HEVC/MPEG2/MJPEG transcoding for N times
    
    Signed-off-by: hbomminx <haribabux.bommineni@intel.com>

--- test/ffmpeg-qsv/transcode/vc1.py
+++ test/ffmpeg-qsv/transcode/vc1.py
@@ -9,66 +9,13 @@ from ..util import *
 from .transcoder import TranscoderTest
 
 spec = load_test_spec("vc1", "transcode")
-class to_avc(TranscoderTest):
-  @slash.requires(have_ffmpeg_vc1_qsv_decode)
-  @slash.requires(have_ffmpeg_h264_qsv_encode)
-  @slash.parametrize(*gen_transcode_1to1_parameters(spec, "avc", "hwhw"))
-  @platform_tags(set(VC1_DECODE_PLATFORMS) & set(AVC_ENCODE_PLATFORMS))
-  def test_hwhw_1to1(self, case):
-    vars(self).update(spec[case].copy())
-    vars(self).update(
-      case         = case,
-      dstextension = 'h264',
-      ffdecoder    = 'vc1_qsv',
-      ffencoder    = 'h264_qsv',
-      mode         = 'hwhw',
-    )
-    self.transcode_1to1()
 
-class to_hevc(TranscoderTest):
-  @slash.requires(have_ffmpeg_vc1_qsv_decode)
-  @slash.requires(have_ffmpeg_hevc_qsv_encode)
-  @slash.parametrize(*gen_transcode_1to1_parameters(spec, "hevc", "hwhw"))
-  @platform_tags(set(VC1_DECODE_PLATFORMS) & set(HEVC_ENCODE_8BIT_PLATFORMS))
-  def test_hwhw_1to1(self, case):
+class default(TranscoderTest):
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
     vars(self).update(spec[case].copy())
     vars(self).update(
-      case         = case,
-      dstextension = 'h265',
-      ffdecoder    = 'vc1_qsv',
-      ffencoder    = 'hevc_qsv',
-      mode         = 'hwhw',
+      case  = case,
+      codec = "vc1",
     )
-    self.transcode_1to1()
-
-class to_mjpeg(TranscoderTest):
-  @slash.requires(have_ffmpeg_vc1_qsv_decode)
-  @slash.requires(have_ffmpeg_mjpeg_qsv_encode)
-  @slash.parametrize(*gen_transcode_1to1_parameters(spec, "mjpeg", "hwhw"))
-  @platform_tags(set(VC1_DECODE_PLATFORMS) & set(JPEG_ENCODE_PLATFORMS))
-  def test_hwhw_1to1(self, case):
-    vars(self).update(spec[case].copy())
-    vars(self).update(
-      case         = case,
-      dstextension = 'mjpeg',
-      ffdecoder    = 'vc1_qsv',
-      ffencoder    = 'mjpeg_qsv',
-      mode         = 'hwhw',
-    )
-    self.transcode_1to1()
-
-class to_mpeg2(TranscoderTest):
-  @slash.requires(have_ffmpeg_vc1_qsv_decode)
-  @slash.requires(have_ffmpeg_mpeg2_qsv_encode)
-  @slash.parametrize(*gen_transcode_1to1_parameters(spec, "mpeg2", "hwhw"))
-  @platform_tags(set(VC1_DECODE_PLATFORMS) & set(MPEG2_ENCODE_PLATFORMS))
-  def test_hwhw_1to1(self, case):
-    vars(self).update(spec[case].copy())
-    vars(self).update(
-      case         = case,
-      dstextension = 'm2v',
-      ffdecoder    = 'vc1_qsv',
-      ffencoder    = 'mpeg2_qsv',
-      mode         = 'hwhw',
-    )
-    self.transcode_1to1()
+    self.transcode()
