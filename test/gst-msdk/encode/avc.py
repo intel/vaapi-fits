9a903866296de0530c02709300cc6404fe20dcb2
Zhu Qingliang <qingliangx.zhu@intel.com>
2019-04-20 14:40:15 -0400

    gst-msdk/encode: refactor tests for better code reuse
    This improves code reuse and design for gst-msdk/encode
    tests.
    
    10bit encode tests have moved into its own sub-directory.
    
    Updated test names in baseline.
    
    Update tools/xform-baseline-1.sh script to convert old
    baseline test names to new test names.
    
    Signed-off-by: Zhu Qingliang <qingliangx.zhu@intel.com>

--- test/gst-msdk/encode/avc.py
+++ test/gst-msdk/encode/avc.py
@@ -6,214 +6,106 @@
 
 from ....lib import *
 from ..util import *
+from .encoder import EncoderTest
 
 spec = load_test_spec("avc", "encode")
 
-def check_psnr(params):
-  call(
-    "gst-launch-1.0 -vf filesrc location={encoded}"
-    " ! h264parse ! msdkh264dec hardware=true"
-    " ! videoconvert ! video/x-raw,format={mformatu}"
-    " ! checksumsink2 file-checksum=false frame-checksum=false"
-    " plane-checksum=false dump-output=true qos=false"
-    " dump-location={decoded}".format(
-      mformatu = mapformatu(params["format"]), **params
+class AVCEncoderTest(EncoderTest):
+  def before(self):
+    vars(self).update(
+      codec         = "avc",
+      gstencoder    = "msdkh264enc",
+      gstdecoder    = "h264parse ! msdkh264dec hardware=true",
+      gstmediatype  = "video/x-h264",
+      gstparser     = "h264parse",
     )
-  )
+    super(AVCEncoderTest, self).before()
 
-  get_media().baseline.check_psnr(
-    psnr = calculate_psnr(
-      params["source"], params["decoded"],
-      params["width"], params["height"],
-      params["frames"], params["format"]),
-    context = params.get("refctx", []),
-  )
+  def get_file_ext(self):
+    return "h264"
 
-#-------------------------------------------------#
-#---------------------- CQP ----------------------#
-#-------------------------------------------------#
-@slash.requires(have_gst)
-@slash.requires(have_gst_msdk)
-@slash.requires(have_gst_msdkh264enc)
-@slash.requires(have_gst_msdkh264dec)
-@slash.requires(*have_gst_element("checksumsink2"))
-@slash.requires(using_compatible_driver)
-@slash.parametrize(*gen_avc_cqp_parameters(spec, ['main', 'high', 'constrained-baseline']))
-@platform_tags(AVC_ENCODE_PLATFORMS)
-def test_cqp(case, gop, slices, bframes, qp, quality, profile):
-  params = spec[case].copy()
+class cqp(AVCEncoderTest):
+  @platform_tags(AVC_ENCODE_PLATFORMS)
+  @slash.requires(have_gst_msdkh264enc)
+  @slash.requires(have_gst_msdkh264dec)
+  @slash.parametrize(*gen_avc_cqp_parameters(spec, ['main', 'high', 'constrained-baseline']))
+  def test(self, case, gop, slices, bframes, qp, quality, profile):
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      bframes   = bframes,
+      case      = case,
+      gop       = gop,
+      lowpower  = False,
+      profile   = profile,
+      qp        = qp,
+      quality   = quality,
+      rcmode    = "cqp",
+      slices    = slices,
+    )
+    self.encode()
 
-  mprofile = mapprofile("avc", profile)
-  if mprofile is None:
-    slash.skip_test("{} profile is not supported".format(profile))
+class cqp_lp(AVCEncoderTest):
+  @platform_tags(AVC_ENCODE_LP_PLATFORMS)
+  @slash.requires(have_gst_msdkh264enc)
+  @slash.requires(have_gst_msdkh264dec)
+  @slash.parametrize(*gen_avc_cqp_lp_parameters(spec, ['main', 'high', 'constrained-baseline']))
+  def test(self, case, gop, slices, qp, quality, profile):
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      case      = case,
+      gop       = gop,
+      lowpower  = True,
+      profile   = profile,
+      qp        = qp,
+      quality   = quality,
+      rcmode    = "cqp",
+      slices    = slices,
+    )
+    self.encode()
 
-  params.update(
-    gop = gop, slices = slices, bframes = bframes, qp = qp, quality = quality,
-    profile = mprofile, mformat = mapformat(params["format"]))
+class cbr(AVCEncoderTest):
+  @platform_tags(AVC_ENCODE_PLATFORMS)
+  @slash.requires(have_gst_msdkh264enc)
+  @slash.requires(have_gst_msdkh264dec)
+  @slash.parametrize(*gen_avc_cbr_parameters(spec, ['main', 'high', 'constrained-baseline']))
+  def test(self, case, gop, slices, bframes, bitrate, fps, profile):
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      bframes   = bframes,
+      bitrate   = bitrate,
+      case      = case,
+      fps       = fps,
+      gop       = gop,
+      lowpower  = False,
+      maxrate   = bitrate,
+      minrate   = bitrate,
+      profile   = profile,
+      rcmode    = "cbr",
+      slices    = slices,
+    )
+    self.encode()
 
-  params["encoded"] = get_media()._test_artifact(
-    "{}-{gop}-{slices}-{bframes}-{qp}-{quality}-{profile}"
-    ".h264".format(case, **params))
-  params["decoded"] = get_media()._test_artifact(
-    "{}-{gop}-{slices}-{bframes}-{qp}-{quality}-{profile}-{width}x{height}-{format}"
-    ".yuv".format(case, **params))
-
-  call(
-    "gst-launch-1.0 -vf filesrc location={source} num-buffers={frames}"
-    " ! rawvideoparse format={mformat} width={width} height={height}"
-    " ! videoconvert ! video/x-raw,format=NV12"
-    " ! msdkh264enc rate-control=cqp qpp={qp} gop-size={gop} num-slices={slices}"
-    " b-frames={bframes} target-usage={quality} hardware=true"
-    " ! video/x-h264,profile={profile} ! h264parse"
-    " ! filesink location={encoded}".format(**params))
-
-  check_psnr(params)
-
-@slash.requires(have_gst)
-@slash.requires(have_gst_msdk)
-@slash.requires(have_gst_msdkh264enc)
-@slash.requires(have_gst_msdkh264dec)
-@slash.requires(*have_gst_element("checksumsink2"))
-@slash.requires(using_compatible_driver)
-@slash.parametrize(*gen_avc_cqp_lp_parameters(spec, ['main', 'high', 'constrained-baseline']))
-@platform_tags(AVC_ENCODE_LP_PLATFORMS)
-def test_cqp_lp(case, gop, slices, qp, quality, profile):
-  params = spec[case].copy()
-
-  mprofile = mapprofile("avc", profile)
-  if mprofile is None:
-    slash.skip_test("{} profile is not supported".format(profile))
-
-  params.update(
-    profile = mprofile, gop = gop, slices = slices, qp = qp,
-    quality = quality, mformat = mapformat(params["format"]))
-
-  params["encoded"] = get_media()._test_artifact(
-    "{}-{gop}-{slices}-{qp}-{quality}-{profile}"
-    ".h264".format(case, **params))
-  params["decoded"] = get_media()._test_artifact(
-    "{}-{gop}-{slices}-{qp}-{quality}-{profile}-{width}x{height}-{format}"
-    ".yuv".format(case, **params))
-
-  call(
-    "gst-launch-1.0 -vf filesrc location={source} num-buffers={frames}"
-    " ! rawvideoparse format={mformat} width={width} height={height}"
-    " ! videoconvert ! video/x-raw,format=NV12"
-    " ! msdkh264enc rate-control=cqp qpp={qp} gop-size={gop} num-slices={slices}"
-    " low-power=true target-usage={quality} hardware=true"
-    " ! video/x-h264,profile={profile} ! h264parse"
-    " ! filesink location={encoded}".format(**params))
-
-  check_psnr(params)
-
-
-#-------------------------------------------------#
-#---------------------- CBR ----------------------#
-#-------------------------------------------------#
-@slash.requires(have_gst)
-@slash.requires(have_gst_msdk)
-@slash.requires(have_gst_msdkh264enc)
-@slash.requires(have_gst_msdkh264dec)
-@slash.requires(*have_gst_element("checksumsink2"))
-@slash.requires(using_compatible_driver)
-@slash.parametrize(*gen_avc_cbr_parameters(spec, ['main', 'high', 'constrained-baseline']))
-@platform_tags(AVC_ENCODE_PLATFORMS)
-def test_cbr(case, gop, slices, bframes, bitrate, fps, profile):
-  params = spec[case].copy()
-
-  mprofile = mapprofile("avc", profile)
-  if mprofile is None:
-    slash.skip_test("{} profile is not supported".format(profile))
-
-  params.update(
-    gop = gop, slices = slices, bframes = bframes, bitrate = bitrate,
-    profile = mprofile, fps = fps, mformat = mapformat(params["format"]))
-
-  params["encoded"] = get_media()._test_artifact(
-    "{}-{gop}-{slices}-{bframes}-{bitrate}-{profile}-{fps}"
-    ".h264".format(case, **params))
-  params["decoded"] = get_media()._test_artifact(
-    "{}-{gop}-{slices}-{bframes}-{bitrate}-{profile}-{fps}-{width}x{height}-{format}"
-    ".yuv".format(case, **params))
-
-  call(
-    "gst-launch-1.0 -vf filesrc location={source} num-buffers={frames}"
-    " ! rawvideoparse format={mformat} width={width} height={height}"
-    " framerate={fps} ! videoconvert ! video/x-raw,format=NV12"
-    " ! msdkh264enc rate-control=cbr bitrate={bitrate} gop-size={gop}"
-    " num-slices={slices} b-frames={bframes} hardware=true"
-    " ! video/x-h264,profile={profile} ! h264parse"
-    " ! filesink location={encoded}".format(**params))
-
-  # calculate actual bitrate
-  encsize = os.path.getsize(params["encoded"])
-  bitrate_actual = encsize * 8 * params["fps"] / 1024.0 / params["frames"]
-  bitrate_gap = abs(bitrate_actual - bitrate) / bitrate
-
-  get_media()._set_test_details(
-    size_encoded = encsize,
-    bitrate_actual = "{:-.2f}".format(bitrate_actual),
-    bitrate_gap = "{:.2%}".format(bitrate_gap))
-
-  assert(bitrate_gap <= 0.10)
-
-  check_psnr(params)
-
-
-#-------------------------------------------------#
-#---------------------- VBR ----------------------#
-#-------------------------------------------------#
-@slash.requires(have_gst)
-@slash.requires(have_gst_msdk)
-@slash.requires(have_gst_msdkh264enc)
-@slash.requires(have_gst_msdkh264dec)
-@slash.requires(*have_gst_element("checksumsink2"))
-@slash.requires(using_compatible_driver)
-@slash.parametrize(*gen_avc_vbr_parameters(spec, ['main', 'high', 'constrained-baseline']))
-@platform_tags(AVC_ENCODE_PLATFORMS)
-def test_vbr(case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
-  params = spec[case].copy()
-
-  mprofile = mapprofile("avc", profile)
-  if mprofile is None:
-    slash.skip_test("{} profile is not supported".format(profile))
-
-  # target percentage 50%
-  minrate = bitrate
-  maxrate = bitrate * 2
-
-  params.update(
-    gop = gop, slices = slices, bframes = bframes, bitrate = bitrate,
-    profile = mprofile, fps = fps, quality = quality, refs = refs,
-    minrate = minrate, maxrate = maxrate, mformat = mapformat(params["format"]))
-
-  params["encoded"] = get_media()._test_artifact(
-    "{}-{gop}-{slices}-{bframes}-{bitrate}-{profile}-{fps}-{quality}-{refs}"
-    ".h264".format(case, **params))
-  params["decoded"] = get_media()._test_artifact(
-    "{}-{gop}-{slices}-{bframes}-{bitrate}-{profile}-{fps}-{quality}-{refs}"
-    "-{width}x{height}-{format}"
-    ".yuv".format(case, **params))
-
-  call(
-    "gst-launch-1.0 -vf filesrc location={source} num-buffers={frames}"
-    " ! rawvideoparse format={mformat} width={width} height={height}"
-    " framerate={fps} ! videoconvert ! video/x-raw,format=NV12"
-    " ! msdkh264enc rate-control=vbr bitrate={minrate}"
-    " max-vbv-bitrate={maxrate} gop-size={gop}"
-    " num-slices={slices} b-frames={bframes} hardware=true ref-frames={refs}"
-    " target-usage={quality} ! video/x-h264,profile={profile} ! h264parse"
-    " ! filesink location={encoded}".format(**params))
-
-  # calculate actual bitrate
-  encsize = os.path.getsize(params["encoded"])
-  bitrate_actual = encsize * 8 * params["fps"] / 1024.0 / params["frames"]
-
-  get_media()._set_test_details(
-    size_encoded = encsize,
-    bitrate_actual = "{:-.2f}".format(bitrate_actual))
-
-  # acceptable bitrate within 25% of minrate and 10% of maxrate
-  assert(minrate * 0.75 <= bitrate_actual <= maxrate * 1.10)
-
-  check_psnr(params)
+class vbr(AVCEncoderTest):
+  @platform_tags(AVC_ENCODE_PLATFORMS)
+  @slash.requires(have_gst_msdkh264enc)
+  @slash.requires(have_gst_msdkh264dec)
+  @slash.parametrize(*gen_avc_vbr_parameters(spec, ['main', 'high', 'constrained-baseline']))
+  def test(self, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      bframes   = bframes,
+      bitrate   = bitrate,
+      case      = case,
+      fps       = fps,
+      gop       = gop,
+      lowpower  = False,
+      # target percentage 50%
+      maxrate   = bitrate * 2,
+      minrate   = bitrate,
+      profile   = profile,
+      quality   = quality,
+      rcmode    = "vbr",
+      refs      = refs,
+      slices    = slices,
+    )
+    self.encode()
--- test/gst-msdk/encode/vp8.py
+++ test/gst-msdk/encode/vp8.py
@@ -6,64 +6,42 @@
 
 from ....lib import *
 from ..util import *
+from .encoder import EncoderTest
 
 spec = load_test_spec("vp8", "encode")
 
-def check_psnr(params):
-  call(
-    "gst-launch-1.0 -vf filesrc location={encoded}"
-    " ! matroskademux ! msdkvp8dec hardware=true"
-    " ! videoconvert ! video/x-raw,format={mformatu}"
-    " ! checksumsink2 file-checksum=false frame-checksum=false"
-    " plane-checksum=false dump-output=true qos=false"
-    " dump-location={decoded}".format(
-      mformatu = mapformatu(params["format"]), **params
+class VP8EncoderTest(EncoderTest):
+  def before(self):
+    vars(self).update(
+      codec         = "vp8",
+      gstencoder    = "msdkvp8enc",
+      gstdecoder    = "matroskademux ! msdkvp8dec hardware=true",
+      gstmediatype  = "video/vp8",
+      gstmuxer      = "matroskamux",
+      profile       = "version0_3",
     )
-  )
+    super(VP8EncoderTest, self).before()
 
-  get_media().baseline.check_psnr(
-    psnr = calculate_psnr(
-      params["source"], params["decoded"],
-      params["width"], params["height"],
-      params["frames"], params["format"]),
-    context = params.get("refctx", []),
-  )
+  def get_file_ext(self):
+    return "webm"
 
-#-------------------------------------------------#
-#---------------------- CQP ----------------------#
-#-------------------------------------------------#
-@slash.requires(have_gst)
-@slash.requires(have_gst_msdk)
-@slash.requires(have_gst_msdkvp8enc)
-@slash.requires(have_gst_msdkvp8dec)
-@slash.requires(*have_gst_element("checksumsink2"))
-@slash.requires(using_compatible_driver)
-@slash.parametrize(*gen_vp8_cqp_parameters(spec))
-@platform_tags(VP8_ENCODE_PLATFORMS)
-def test_cqp(case, ipmode, qp, quality, looplvl, loopshp):
-  if looplvl != 0 or loopshp != 0:
-    # will looplvl or loopshp be supported later?
-    slash.skip_test("looplvl != 0 or loopshp != 0 not supported")
-
-  params = spec[case].copy()
-  params.update(
-    ipmode = ipmode, qp = qp, quality = quality, looplvl = looplvl,
-    loopshp = loopshp, gop = 30 if ipmode != 0 else 1,
-    mformat = mapformat(params["format"]))
-  params["encoded"] = get_media()._test_artifact(
-    "{}-{ipmode}-{qp}-{quality}-{looplvl}-{loopshp}"
-    ".webm".format(case, **params))
-  params["decoded"] = get_media()._test_artifact(
-    "-{}-{ipmode}-{qp}-{quality}-{looplvl}-{loopshp}-{width}x{height}-{format}"
-    ".yuv".format(case, **params))
-
-  call(
-    "gst-launch-1.0 -vf filesrc location={source} num-buffers={frames}"
-    " ! rawvideoparse format={mformat} width={width} height={height}"
-    " ! videoconvert ! video/x-raw,format=NV12"
-    " ! msdkvp8enc rate-control=cqp qpp={qp} gop-size={gop} hardware=true"
-    " target-usage={quality}"
-    " ! matroskamux ! filesink location={encoded}".format(**params))
-
-  check_psnr(params)
-
+class cqp(VP8EncoderTest):
+  @platform_tags(VP8_ENCODE_PLATFORMS)
+  @slash.requires(have_gst_msdkvp8enc)
+  @slash.requires(have_gst_msdkvp8dec)
+  @slash.parametrize(*gen_vp8_cqp_parameters(spec))
+  def test(self, case, ipmode, qp, quality, looplvl, loopshp):
+    if looplvl != 0 or loopshp != 0:
+      # will looplvl or loopshp be supported later?
+      slash.skip_test("looplvl != 0 or loopshp != 0 not supported")
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      case      = case,
+      gop       = 30 if ipmode != 0 else 1,
+      looplvl   = looplvl,
+      loopshp   = loopshp,
+      qp        = qp,
+      quality   = quality,
+      rcmode    = "cqp",
+    )
+    self.encode()
