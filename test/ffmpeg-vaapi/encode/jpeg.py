bd6a13d91bc70ee3a1d7b05d2d15596e9d688e36
hbomminx <haribabux.bommineni@intel.com>
2019-05-09 13:27:29 -0700

    ffmpeg-vaapi/encode: JPEG R2R support added
      1. It supports JPEG encoding for multiple iterations in single test run
      2. md5 is used for validating each encode iteration
    
    Signed-off-by: hbomminx <haribabux.bommineni@intel.com>

--- test/ffmpeg-vaapi/encode/jpeg.py
+++ test/ffmpeg-vaapi/encode/jpeg.py
@@ -25,16 +25,28 @@ class JPEGEncoderTest(EncoderTest):
     return "VAProfileJPEGBaseline"
 
 spec = load_test_spec("jpeg", "encode")
+spec_r2r = load_test_spec("jpeg", "encode", "r2r")
 
 class cqp(JPEGEncoderTest):
-  @platform_tags(JPEG_ENCODE_PLATFORMS)
-  @slash.requires(have_ffmpeg_mjpeg_vaapi_encode)
-  @slash.parametrize(*gen_jpeg_cqp_parameters(spec))
-  def test(self, case, quality):
-    vars(self).update(spec[case].copy())
+  def init(self, tspec, case, quality):
+    vars(self).update(tspec[case].copy())
     vars(self).update(
       case    = case,
       quality = quality,
       rcmode  = "cqp",
     )
+
+  @platform_tags(JPEG_ENCODE_PLATFORMS)
+  @slash.requires(have_ffmpeg_mjpeg_vaapi_encode)
+  @slash.parametrize(*gen_jpeg_cqp_parameters(spec))
+  def test(self, case, quality):
+    self.init(spec, case, quality)
+    self.encode()
+
+  @platform_tags(JPEG_ENCODE_PLATFORMS)
+  @slash.requires(have_ffmpeg_mjpeg_vaapi_encode)
+  @slash.parametrize(*gen_jpeg_cqp_parameters(spec_r2r))
+  def test_r2r(self, case, quality):
+    self.init(spec_r2r, case, quality)
+    vars(self).setdefault("r2r", 5)
     self.encode()
