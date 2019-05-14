8365b0ab9851a1705d28c5331da9b26c40f6c7ff
hbomminx <haribabux.bommineni@intel.com>
2019-05-14 15:05:27 -0700

    ffmpeg-qsv/decode: VC1 R2R support added
      1. It supports VC1 decoding for multiple iterations in single test run
      2. md5 is used for validating each decode iteration
    
    Signed-off-by: hbomminx <haribabux.bommineni@intel.com>

--- test/ffmpeg-qsv/decode/vc1.py
+++ test/ffmpeg-qsv/decode/vc1.py
@@ -9,6 +9,7 @@ from ..util import *
 from .decoder import DecoderTest
 
 spec = load_test_spec("vc1", "decode")
+spec_r2r = load_test_spec("vc1", "decode", "r2r")
 
 class default(DecoderTest):
   def before(self):
@@ -16,14 +17,25 @@ class default(DecoderTest):
     self.metric = dict(type = "ssim", miny = 0.99, minu = 0.99, minv = 0.99)
     super(default, self).before()
 
-  @platform_tags(VC1_DECODE_PLATFORMS)
-  @slash.requires(have_ffmpeg_vc1_qsv_decode)
-  @slash.parametrize(("case"), sorted(spec.keys()))
-  def test(self, case):
-    vars(self).update(spec[case].copy())
+  def init(self, tspec, case):
+    vars(self).update(tspec[case].copy())
     vars(self).update(
       case      = case,
       ffdecoder = "vc1_qsv",
       hwformat  = "nv12",
     )
+
+  @platform_tags(VC1_DECODE_PLATFORMS)
+  @slash.requires(have_ffmpeg_vc1_qsv_decode)
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    self.init(spec, case)
+    self.decode()
+
+  @platform_tags(VC1_DECODE_PLATFORMS)
+  @slash.requires(have_ffmpeg_vc1_qsv_decode)
+  @slash.parametrize(("case"), sorted(spec_r2r.keys()))
+  def test_r2r(self, case):
+    self.init(spec_r2r, case)
+    vars(self).setdefault("r2r", 5)
     self.decode()
