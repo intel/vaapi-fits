b7c2c8e73053d19e5759c2bc738a53f6ee8ad36c
hbomminx <haribabux.bommineni@intel.com>
2019-05-15 16:37:13 -0700

    ffmpeg-vaapi/vpp: R2R support added to procamp tests
    Signed-off-by: hbomminx <haribabux.bommineni@intel.com>

--- test/ffmpeg-vaapi/vpp/contrast.py
+++ test/ffmpeg-vaapi/vpp/contrast.py
@@ -8,8 +8,39 @@ from ....lib import *
 from ..util import *
 
 spec = load_test_spec("vpp", "contrast")
+spec_r2r = load_test_spec("vpp", "contrast", "r2r")
 
 _NOOP_LEVEL = 10 # i.e. 1.0 in ffmpeg range
+
+def init(tspec, case, level):
+  tparams = tspec[case].copy()
+  tparams.update(
+    level = level, mlevel = mapRange(level, [0, 100], [0.0, 10.0]),
+    mformat = mapformat(tparams["format"]))
+
+  if tparams["mformat"] is None:
+    slash.skip_test("{format} format not supported".format(**tparams))
+
+  return tparams
+
+def call_ffmpeg(params):
+  call(
+    "ffmpeg -hwaccel vaapi -vaapi_device /dev/dri/renderD128 -v debug"
+    " -f rawvideo -pix_fmt {mformat} -s:v {width}x{height} -i {source}"
+    " -vf 'format=nv12,hwupload,procamp_vaapi=c={mlevel}"
+    ",hwdownload,format=nv12'"
+    " -pix_fmt {mformat} -vframes {frames} -y {ofile}".format(**params))
+
+def gen_output(case, params):
+  name = "{case}_contrast_{level}_{format}_{width}x{height}".format(case = case, **params)
+
+  if params.get("r2r", None) is not None:
+    name += "_r2r"
+
+  name += ".yuv"
+  params["ofile"] = get_media()._test_artifact(name)
+
+  call_ffmpeg(params)
 
 @slash.requires(have_ffmpeg)
 @slash.requires(have_ffmpeg_vaapi_accel)
@@ -17,23 +48,9 @@ _NOOP_LEVEL = 10 # i.e. 1.0 in ffmpeg range
 @slash.parametrize(*gen_vpp_contrast_parameters(spec))
 @platform_tags(VPP_PLATFORMS)
 def test_default(case, level):
-  params = spec[case].copy()
-  params.update(
-    level = level, mlevel = mapRange(level, [0, 100], [0.0, 10.0]),
-    mformat = mapformat(params["format"]))
-  params["ofile"] = get_media()._test_artifact(
-    "{}_contrast_{level}_{format}_{width}x{height}"
-    ".yuv".format(case, **params))
+  params = init(spec, case, level)
 
-  if params["mformat"] is None:
-    slash.skip_test("{format} format not supported".format(**params))
-
-  call(
-    "ffmpeg -hwaccel vaapi -vaapi_device /dev/dri/renderD128 -v debug"
-    " -f rawvideo -pix_fmt {mformat} -s:v {width}x{height} -i {source}"
-    " -vf 'format=nv12,hwupload,procamp_vaapi=c={mlevel}"
-    ",hwdownload,format=nv12'"
-    " -pix_fmt {mformat} -vframes {frames} -y {ofile}".format(**params))
+  gen_output(case, params)
 
   psnr = calculate_psnr(
     params["source"], params["ofile"],
@@ -53,3 +70,30 @@ def test_default(case, level):
       assert abs(ref[-1] - actual[-1]) < 0.2, "Cr (V) out of baseline range"
     get_media().baseline.check_result(
       compare = compare, context = params.get("refctx", []), psnr = psnr)
+
+@slash.requires(have_ffmpeg)
+@slash.requires(have_ffmpeg_vaapi_accel)
+@slash.requires(*have_ffmpeg_filter("procamp_vaapi"))
+@slash.parametrize(*gen_vpp_contrast_parameters(spec_r2r))
+@platform_tags(VPP_PLATFORMS)
+def test_r2r(case, level):
+  params = init(spec_r2r, case, level)
+  params.setdefault("r2r", 5)
+  assert type(params["r2r"]) is int and params["r2r"] > 1, "invalid r2r value"
+
+  gen_output(case, params)
+
+  md5ref = md5(params["ofile"])
+  get_media()._set_test_details(md5_ref = md5ref)
+
+  for i in xrange(1, params["r2r"]):
+    params["ofile"] = get_media()._test_artifact(
+      "{case}_contrast_{level}_{format}_{width}x{height}_{i}"
+      ".yuv".format(case = case, i = i, **params))
+
+    call_ffmpeg(params)
+    result = md5(params["ofile"])
+    get_media()._set_test_details(**{ "md5_{:03}".format(i) : result})
+    assert result == md5ref, "r2r md5 mismatch"
+    #delete output file after each iteration
+    get_media()._purge_test_artifact(params["ofile"])
