2336184a050cfd30eafbf4990fdf8ce8b9b1ca26
Wang Zhanjun <zhanjunx.wang@intel.com>
2019-05-14 20:21:53 -0400

    test: enable r2r encoding test for gst-msdk
    Signed-off-by: Wang Zhanjun <zhanjunx.wang@intel.com>

--- test/gst-msdk/encode/hevc.py
+++ test/gst-msdk/encode/hevc.py
@@ -9,6 +9,7 @@ from ..util import *
 from .encoder import EncoderTest
 
 spec = load_test_spec("hevc", "encode", "8bit")
+spec_r2r = load_test_spec("hevc", "encode", "8bit", "r2r")
 
 class HEVC8EncoderTest(EncoderTest):
   def before(self):
@@ -25,92 +26,133 @@ class HEVC8EncoderTest(EncoderTest):
     return "h265"
 
 class cqp(HEVC8EncoderTest):
+  def init(self, tspec, case, gop, slices, bframes, qp, quality, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bframes = bframes,
+      case    = case,
+      gop     = gop,
+      qp      = qp,
+      quality = quality,
+      profile = profile,
+      rcmode  = "cqp",
+      slices  = slices,
+    )
+    self.encode()
+
   @platform_tags(HEVC_ENCODE_8BIT_PLATFORMS)
   @slash.requires(have_gst_msdkh265enc)
   @slash.requires(have_gst_msdkh265dec)
   @slash.parametrize(*gen_hevc_cqp_parameters(spec, ['main']))
   def test(self, case, gop, slices, bframes, qp, quality, profile):
-    vars(self).update(spec[case].copy())
+    self.init(spec, case, gop, slices, bframes, qp, quality, profile)
+    self.encode()
+
+  @platform_tags(HEVC_ENCODE_8BIT_PLATFORMS)
+  @slash.requires(have_gst_msdkh265enc)
+  @slash.parametrize(*gen_hevc_cqp_parameters(spec_r2r, ['main']))
+  def test_r2r(self, case, gop, slices, bframes, qp, quality, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, qp, quality, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class cqp_lp(HEVC8EncoderTest):
+  def init(self, tspec, case, gop, slices, qp, quality, profile):
+    vars(self).update(tspec[case].copy())
     vars(self).update(
-      bframes = bframes,
       case    = case,
       gop     = gop,
       qp      = qp,
+      lowpower= True,
       quality = quality,
       profile = profile,
       rcmode  = "cqp",
       slices  = slices,
     )
-    self.encode()
 
-class cqp_lp(HEVC8EncoderTest):
   @platform_tags(HEVC_ENCODE_8BIT_LP_PLATFORMS)
   @slash.requires(have_gst_msdkh265enc)
   @slash.requires(have_gst_msdkh265dec)
   @slash.parametrize(*gen_hevc_cqp_lp_parameters(spec, ['main']))
   def test(self, case, gop, slices, qp, quality, profile):
-    vars(self).update(spec[case].copy())
-    vars(self).update(
-      case    = case,
-      gop     = gop,
-      qp      = qp,
-      lowpower= True,
-      quality = quality,
-      profile = profile,
-      rcmode  = "cqp",
-      slices  = slices,
-    )
+    self.init(spec, case, gop, slices, qp, quality, profile)
+    self.encode()
+
+  @platform_tags(HEVC_ENCODE_8BIT_LP_PLATFORMS)
+  @slash.requires(have_gst_msdkh265enc)
+  @slash.parametrize(*gen_hevc_cqp_lp_parameters(spec_r2r, ['main']))
+  def test_r2r(self, case, gop, slices, qp, quality, profile):
+    self.init(spec_r2r, case, gop, slices, qp, quality, profile)
+    vars(self).setdefault("r2r", 5)
     self.encode()
 
 class cbr(HEVC8EncoderTest):
+  def init(self, tspec, case, gop, slices, bframes, bitrate, fps, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bframes = bframes,
+      bitrate = bitrate,
+      case    = case,
+      fps     = fps,
+      gop     = gop,
+      maxrate = bitrate,
+      minrate = bitrate,
+      profile = profile,
+      rcmode  = "cbr",
+      slices  = slices,
+    )
+
   @platform_tags(HEVC_ENCODE_8BIT_PLATFORMS)
   @slash.requires(have_gst_msdkh265enc)
   @slash.requires(have_gst_msdkh265dec)
   @slash.parametrize(*gen_hevc_cbr_parameters(spec, ['main']))
   def test(self, case, gop, slices, bframes, bitrate, fps, profile):
-    vars(self).update(spec[case].copy())
+    self.init(spec, case, gop, slices, bframes, bitrate, fps, profile)
+    self.encode()
+
+  @platform_tags(HEVC_ENCODE_8BIT_PLATFORMS)
+  @slash.requires(have_gst_msdkh265enc)
+  @slash.parametrize(*gen_hevc_cbr_parameters(spec_r2r, ['main']))
+  def test_r2r(self, case, gop, slices, bframes, bitrate, fps, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, bitrate, fps, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class cbr_lp(HEVC8EncoderTest):
+  def init(self, tspec, case, gop, slices, bitrate, fps, profile):
+    vars(self).update(tspec[case].copy())
     vars(self).update(
-      bframes = bframes,
       bitrate = bitrate,
       case    = case,
       fps     = fps,
       gop     = gop,
+      lowpower= True,
       maxrate = bitrate,
       minrate = bitrate,
       profile = profile,
       rcmode  = "cbr",
       slices  = slices,
     )
-    self.encode()
 
-class cbr_lp(HEVC8EncoderTest):
   @platform_tags(HEVC_ENCODE_8BIT_LP_PLATFORMS)
   @slash.requires(have_gst_msdkh265enc)
   @slash.requires(have_gst_msdkh265dec)
   @slash.parametrize(*gen_hevc_cbr_lp_parameters(spec, ['main']))
   def test(self, case, gop, slices, bitrate, fps, profile):
-    vars(self).update(spec[case].copy())
-    vars(self).update(
-      bitrate = bitrate,
-      case    = case,
-      fps     = fps,
-      gop     = gop,
-      lowpower= True,
-      maxrate = bitrate,
-      minrate = bitrate,
-      profile = profile,
-      rcmode  = "cbr",
-      slices  = slices,
-    )
+    self.init(spec, case, gop, slices, bitrate, fps, profile)
+    self.encode()
+
+  @platform_tags(HEVC_ENCODE_8BIT_LP_PLATFORMS)
+  @slash.requires(have_gst_msdkh265enc)
+  @slash.parametrize(*gen_hevc_cbr_lp_parameters(spec_r2r, ['main']))
+  def test_r2r(self, case, gop, slices, bitrate, fps, profile):
+    self.init(spec_r2r, case, gop, slices, bitrate, fps, profile)
+    vars(self).setdefault("r2r", 5)
     self.encode()
 
 class vbr(HEVC8EncoderTest):
-  @platform_tags(HEVC_ENCODE_8BIT_PLATFORMS)
-  @slash.requires(have_gst_msdkh265enc)
-  @slash.requires(have_gst_msdkh265dec)
-  @slash.parametrize(*gen_hevc_vbr_parameters(spec, ['main']))
-  def test(self, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
-    vars(self).update(spec[case].copy())
+  def init(self, tspec, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
+    vars(self).update(tspec[case].copy())
     vars(self).update(
       bframes = bframes,
       bitrate = bitrate,
@@ -126,15 +168,26 @@ class vbr(HEVC8EncoderTest):
       refs    = refs,
       slices  = slices,
     )
+
+  @platform_tags(HEVC_ENCODE_8BIT_PLATFORMS)
+  @slash.requires(have_gst_msdkh265enc)
+  @slash.requires(have_gst_msdkh265dec)
+  @slash.parametrize(*gen_hevc_vbr_parameters(spec, ['main']))
+  def test(self, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
+    self.init(spec, case, gop, slices, bframes, bitrate, fps, quality, refs, profile)
+    self.encode()
+
+  @platform_tags(HEVC_ENCODE_8BIT_PLATFORMS)
+  @slash.requires(have_gst_msdkh265enc)
+  @slash.parametrize(*gen_hevc_vbr_parameters(spec_r2r, ['main']))
+  def test_r2r(self, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, bitrate, fps, quality, refs, profile)
+    vars(self).setdefault("r2r", 5)
     self.encode()
 
 class vbr_lp(HEVC8EncoderTest):
-  @platform_tags(HEVC_ENCODE_8BIT_LP_PLATFORMS)
-  @slash.requires(have_gst_msdkh265enc)
-  @slash.requires(have_gst_msdkh265dec)
-  @slash.parametrize(*gen_hevc_vbr_lp_parameters(spec, ['main']))
-  def test(self, case, gop, slices, bitrate, fps, quality, refs, profile):
-    vars(self).update(spec[case].copy())
+  def init(self, tspec, case, gop, slices, bitrate, fps, quality, refs, profile):
+    vars(self).update(tspec[case].copy())
     vars(self).update(
       bitrate = bitrate,
       case    = case,
@@ -150,4 +203,19 @@ class vbr_lp(HEVC8EncoderTest):
       refs    = refs,
       slices  = slices,
     )
+
+  @platform_tags(HEVC_ENCODE_8BIT_LP_PLATFORMS)
+  @slash.requires(have_gst_msdkh265enc)
+  @slash.requires(have_gst_msdkh265dec)
+  @slash.parametrize(*gen_hevc_vbr_lp_parameters(spec, ['main']))
+  def test(self, case, gop, slices, bitrate, fps, quality, refs, profile):
+    self.init(spec, case, gop, slices, bitrate, fps, quality, refs, profile)
+    self.encode()
+
+  @platform_tags(HEVC_ENCODE_8BIT_LP_PLATFORMS)
+  @slash.requires(have_gst_msdkh265enc)
+  @slash.parametrize(*gen_hevc_vbr_lp_parameters(spec_r2r, ['main']))
+  def test_r2r(self, case, gop, slices, bitrate, fps, quality, refs, profile):
+    self.init(spec_r2r, case, gop, slices, bitrate, fps, quality, refs, profile)
+    vars(self).setdefault("r2r", 5)
     self.encode()
