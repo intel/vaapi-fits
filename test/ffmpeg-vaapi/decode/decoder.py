c5c66fb0e0f17b229afc039c8089304eb54c2979
U. Artie Eoff <ullysses.a.eoff@intel.com>
2019-05-16 12:13:35 -0700

    test/ffmpeg-vaapi: consolidate common r2r vs non-r2r code
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/ffmpeg-vaapi/decode/decoder.py
+++ test/ffmpeg-vaapi/decode/decoder.py
@@ -19,6 +19,12 @@ class DecoderTest(slash.Test):
       " -i {source} -pix_fmt {mformat} -f rawvideo -vsync passthrough"
       " -vframes {frames} -y {decoded}".format(**vars(self)))
 
+  def gen_name(self):
+    name = "{case}_{width}x{height}_{format}"
+    if vars(self).get("r2r", None) is not None:
+      name += "_r2r"
+    return name
+
   def decode(self):
     self.mformat = mapformat(self.format)
 
@@ -27,28 +33,23 @@ class DecoderTest(slash.Test):
 
     get_media().test_call_timeout = vars(self).get("call_timeout", 0)
 
+    name = self.gen_name().format(**vars(self))
+    self.decoded = get_media()._test_artifact("{}.yuv".format(name))
+    self.call_ffmpeg()
+
     if vars(self).get("r2r", None) is not None:
       assert type(self.r2r) is int and self.r2r > 1, "invalid r2r value"
-      for self.i in xrange(self.r2r):
-        self.decoded = get_media()._test_artifact(
-          "{case}_{width}x{height}_{format}_{i}.yuv".format(**vars(self)))
-
+      md5ref = md5(self.decoded)
+      get_media()._set_test_details(md5_ref = md5ref)
+      for i in xrange(1, self.r2r):
+        self.decoded = get_media()._test_artifact("{}_{}.yuv".format(name, i))
         self.call_ffmpeg()
         result = md5(self.decoded)
-        get_media()._set_test_details(**{ "md5_{}".format(self.i) : result})
-
-        if 0 == self.i:
-          md5ref = result
-          continue
-
+        get_media()._set_test_details(**{"md5_{:03}".format(i) : result})
         assert result == md5ref, "r2r md5 mismatch"
         # delete decoded file after each iteration
         get_media()._purge_test_artifact(self.decoded)
     else:
-      self.decoded = get_media()._test_artifact(
-        "{case}_{width}x{height}_{format}.yuv".format(**vars(self)))
-
-      self.call_ffmpeg()
       self.check_output()
       self.check_metrics()
 
