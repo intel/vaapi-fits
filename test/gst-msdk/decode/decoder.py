2ab478c98ee6b5314c63da5b7d62477bc79a89b6
U. Artie Eoff <ullysses.a.eoff@intel.com>
2019-06-11 11:27:03 -0700

    gst-msdk/decode: add support for hw decode scaling
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/gst-msdk/decode/decoder.py
+++ test/gst-msdk/decode/decoder.py
@@ -7,6 +7,12 @@
 from ....lib import *
 from ..util import *
 
+__scalers__ = {
+  "hw"  : lambda: "! msdkvpp scaling-mode=1 ! video/x-raw,width={width},height={height},format={hwformat}",
+  "sw"  : lambda: "! videoscale ! video/x-raw,width={width},height={height}",
+  True  : lambda: __scalers__["sw"](),
+}
+
 @slash.requires(have_gst)
 @slash.requires(*have_gst_element("msdk"))
 @slash.requires(*have_gst_element("checksumsink2"))
@@ -17,16 +23,11 @@ class DecoderTest(slash.Test):
 
   @timefn("gst")
   def call_gst(self):
-    self.gstscaler = ""
-    if vars(self).get("scale_output", False):
-      self.gstscaler = (
-        " ! videoscale"
-        " ! video/x-raw,width={width},height={height}".format(**vars(self)))
-
     call(
       "gst-launch-1.0 -vf filesrc location={source}"
-      " ! {gstdecoder} ! videoconvert ! video/x-raw,format={mformatu}"
-      " {gstscaler} ! checksumsink2 file-checksum=false qos=false"
+      " ! {gstdecoder} {gstscaler}"
+      " ! videoconvert ! video/x-raw,format={mformatu}"
+      " ! checksumsink2 file-checksum=false qos=false"
       " frame-checksum=false plane-checksum=false dump-output=true"
       " dump-location={decoded}".format(**vars(self)))
 
@@ -37,12 +38,15 @@ class DecoderTest(slash.Test):
     return name
 
   def decode(self):
+    self.hwformat = maphwformat(self.format)
     self.mformatu = mapformatu(self.format)
     if self.mformatu is None:
       slash.skip_test("{format} format not supported".format(**vars(self)))
 
     get_media().test_call_timeout = vars(self).get("call_timeout", 0)
 
+    self.gstscaler = __scalers__.get(
+      vars(self).get("scale_output", False), lambda: "")().format(**vars(self))
     name = self.gen_name().format(**vars(self))
     self.decoded = get_media()._test_artifact("{}.yuv".format(name))
     self.call_gst()
