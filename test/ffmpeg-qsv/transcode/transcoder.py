27693d5704fc887c6935503bab6dbabb91366f5d
U. Artie Eoff <ullysses.a.eoff@intel.com>
2019-06-18 08:46:53 -0700

    ffmpeg/transcode: list should be extended, not appended
    Issue introduced in commit 649e9e4d4ed2 appends a list
    to the filters list.  It should actually extend the
    filters list, instead.
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/ffmpeg-qsv/transcode/transcoder.py
+++ test/ffmpeg-qsv/transcode/transcoder.py
@@ -168,7 +168,7 @@ class TranscoderTest(slash.Test):
       if ("hw", "sw") == tmode:   # HW to SW transcode
         filters.extend(["hwdownload", "format=nv12"])
       elif ("sw", "hw") == tmode: # SW to HW transcode
-        filters.append(["format=nv12", "hwupload=extra_hw_frames=64"])
+        filters.extend(["format=nv12", "hwupload=extra_hw_frames=64"])
 
       vppscale = self.get_vpp_scale(
         output.get("width", None), output.get("height", None), mode)
