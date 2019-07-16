31cc17786d4844e6a70c6aec685b1174529496b5
U. Artie Eoff <ullysses.a.eoff@intel.com>
2019-07-16 15:36:03 -0700

    lib/platform: add new platform caps model
    This new platform capabilities model will allow
    for more flexibility and finer-grained control
    for tests.  It allows for more descriptive
    definitions around constraints and capabilities
    for the seemingly endless combinations of
    supported formats, resolutions, profiles, codecs,
    vpp ops, etc.
    
    Tests can use the caps information to determine
    if a specific combination of test runtime
    parameters "should" be supported or not by the
    currently selected driver/platform.  Then the
    tests can skip or proceed with a negative-based
    or positive-based execution according to this
    information.
    
    For example, vpp tests can call:
    
      platform.get_caps("vpp", "csc")
    
    ...if the return value of the above call is
    empty, then it would indicate that we should
    not expect vpp csc to be supported by the
    platform/driver combination.  Otherwise, a map
    of expected hardware input and hardware output
    supported formats is returned and can be used
    to check if the current test parameters are
    included in these supported formats or not.
    
    Another example:
    
      platform.get_caps("decode", "avc")
    
    ...to see if avc decode should be supported
    and the expected maximimum supported resolution
    (and potentially other constraints added later,
    like profiles, etc.).
    
    Eventually, this model will eliminate the
    @platform_tags mechanism from this test
    framework.  The only benefit of the current tag
    mechanism is that it filters out tests before
    test execution.  The downside of this "tagging"
    is that test runtime parameters aren't known
    at the time the tags are evaluated.  This makes
    it nearly impossible or hard to filter tests
    before execution begins based on the test runtime
    parameters.  And therefore, results in various
    unsightly hacks in both the test framework and
    the user config/spec... for example, see:
      ffmpeg-vaapi/decode/10bit/hevc.py
      gst-vaapi/decode/hevc.py
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- lib/platform.py
+++ lib/platform.py
@@ -5,6 +5,7 @@
 ###
 
 from __future__ import absolute_import
+from .common import memoize
 import os
 
 ALL_PLATFORMS                  = [
@@ -92,6 +93,59 @@ elif "iHD" == driver:
   DECODE_10BIT_444_PLATFORMS     = [                                                                            "ICL"]
   VPP_PLATFORMS                  = [                    "BDW",        "SKL", "APL", "KBL",        "CFL", "WHL", "ICL"]
   VPP_TRANSFORM_PLATFORMS        = VPP_PLATFORMS
+
+@memoize
+def load_caps():
+  from .common import get_media
+  namespace = dict(
+    res2k   = ( 2048,  2048),
+    res4k   = ( 4096,  4096),
+    res8k   = ( 8192,  8192),
+    res16k  = (16384, 16384),
+  )
+  capsfile = os.path.abspath(
+    os.path.join(
+      os.path.dirname(__file__), "caps",
+      str(get_media()._get_platform_name()),
+      str(get_media()._get_driver_name()),
+    )
+  )
+  if os.path.exists(capsfile):
+    execfile(capsfile, namespace)
+  caps = namespace.get("caps", None)
+
+  # map some aliases for convenience
+  if caps is not None:
+    if caps.get("vpp", dict()).get("procamp", None) is not None:
+      for op in ["brightness", "contrast", "hue", "saturation"]:
+        caps["vpp"].setdefault(op, caps["vpp"]["procamp"])
+    if caps.get("vpp", dict()).get("transpose", None) is not None:
+      for op in ["mirroring", "rotation"]:
+        caps["vpp"].setdefault(op, caps["vpp"]["transpose"])
+    di = caps.get("vpp", dict()).get("deinterlace", dict())
+    if di.get("motion_adaptive", None) is not None:
+      di.setdefault("advanced", di["motion_adaptive"])
+
+  return caps
+
+@memoize
+def get_caps(*args):
+  caps = load_caps()
+  for key in args:
+    if caps is None:
+      break
+    caps = caps.get(key, None)
+  return caps
+
+@memoize
+def have_caps(*args):
+  from .common import get_media
+  failmsg = "{0}.{1}.{2} caps".format(
+    get_media()._get_platform_name(),
+    get_media()._get_driver_name(),
+    '.'.join(args)
+  )
+  return get_caps(*args) is not None, failmsg
 
 def platform_tags(platforms):
   def wrap(f):
