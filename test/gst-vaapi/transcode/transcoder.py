a81eaef1e42a5eb307c4feeaf521f9e0a902228d
U. Artie Eoff <ullysses.a.eoff@intel.com>
2019-07-24 10:38:21 -0700

    gst-vaapi/transcode: convert to new caps model
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/gst-vaapi/transcode/transcoder.py
+++ test/gst-vaapi/transcode/transcoder.py
@@ -11,34 +11,33 @@ import os
 @slash.requires(have_gst)
 @slash.requires(*have_gst_element("vaapi"))
 @slash.requires(*have_gst_element("checksumsink2"))
-@platform_tags(ALL_PLATFORMS)
 class TranscoderTest(slash.Test):
   requirements = dict(
     decode = {
       "avc" : dict(
-        sw = (ALL_PLATFORMS, have_gst_element("avdec_h264"), "h264parse ! avdec_h264"),
-        hw = (AVC_DECODE_PLATFORMS, have_gst_element("vaapih264dec"), "h264parse ! vaapih264dec"),
+        sw = (dict(maxres = (16384, 16384)), have_gst_element("avdec_h264"), "h264parse ! avdec_h264"),
+        hw = (platform.get_caps("decode", "avc"), have_gst_element("vaapih264dec"), "h264parse ! vaapih264dec"),
       ),
       "hevc-8" : dict(
-        sw = (ALL_PLATFORMS, have_gst_element("avdec_h265"), "h265parse ! avdec_h265"),
-        hw = (HEVC_DECODE_8BIT_PLATFORMS, have_gst_element("vaapih265dec"), "h265parse ! vaapih265dec"),
+        sw = (dict(maxres = (16384, 16384)), have_gst_element("avdec_h265"), "h265parse ! avdec_h265"),
+        hw = (platform.get_caps("decode", "hevc_8"), have_gst_element("vaapih265dec"), "h265parse ! vaapih265dec"),
       ),
       "mpeg2" : dict(
-        sw = (ALL_PLATFORMS, have_gst_element("avdec_mpeg2video"), "mpegvideoparse ! avdec_mpeg2video"),
-        hw = (MPEG2_DECODE_PLATFORMS, have_gst_element("vaapimpeg2dec"), "mpegvideoparse ! vaapimpeg2dec"),
+        sw = (dict(maxres = (2048, 2048)), have_gst_element("avdec_mpeg2video"), "mpegvideoparse ! avdec_mpeg2video"),
+        hw = (platform.get_caps("decode", "mpeg2"), have_gst_element("vaapimpeg2dec"), "mpegvideoparse ! vaapimpeg2dec"),
       ),
       "mjpeg" : dict(
-        sw = (ALL_PLATFORMS, have_gst_element("jpegdec"), "jpegparse ! jpegdec"),
-        hw = (JPEG_DECODE_PLATFORMS, have_gst_element("vaapijpegdec"), "jpegparse ! vaapijpegdec"),
+        sw = (dict(maxres = (16384, 16384)), have_gst_element("jpegdec"), "jpegparse ! jpegdec"),
+        hw = (platform.get_caps("decode", "jpeg"), have_gst_element("vaapijpegdec"), "jpegparse ! vaapijpegdec"),
       ),
       "vc1" : dict(
         sw = (
-          ALL_PLATFORMS, have_gst_element("avdec_vc1"),
+          dict(maxres = (16384, 16384)), have_gst_element("avdec_vc1"),
           "'video/x-wmv,profile=(string)advanced'"
           ",width={width},height={height},framerate=14/1 ! avdec_vc1"
         ),
         hw = (
-          VC1_DECODE_PLATFORMS, have_gst_element("vaapivc1dec"),
+          platform.get_caps("decode", "vc1"), have_gst_element("vaapivc1dec"),
           "'video/x-wmv,profile=(string)advanced'"
           ",width={width},height={height},framerate=14/1 ! vaapivc1dec"
         ),
@@ -46,26 +45,26 @@ class TranscoderTest(slash.Test):
     },
     encode = {
       "avc" : dict(
-        sw = (ALL_PLATFORMS, have_gst_element("x264enc"), "x264enc ! video/x-h264,profile=main ! h264parse"),
-        hw = (AVC_ENCODE_PLATFORMS, have_gst_element("vaapih264enc"), "vaapih264enc ! video/x-h264,profile=main ! h264parse"),
+        sw = (dict(maxres = (16384, 16384)), have_gst_element("x264enc"), "x264enc ! video/x-h264,profile=main ! h264parse"),
+        hw = (platform.get_caps("encode", "avc"), have_gst_element("vaapih264enc"), "vaapih264enc ! video/x-h264,profile=main ! h264parse"),
       ),
       "hevc-8" : dict(
-        sw = (ALL_PLATFORMS, have_gst_element("x265enc"), "x265enc ! video/x-h265,profile=main ! h265parse"),
-        hw = (HEVC_ENCODE_8BIT_PLATFORMS, have_gst_element("vaapih265enc"), "vaapih265enc ! video/x-h265,profile=main ! h265parse"),
+        sw = (dict(maxres = (16384, 16384)), have_gst_element("x265enc"), "x265enc ! video/x-h265,profile=main ! h265parse"),
+        hw = (platform.get_caps("encode", "hevc_8"), have_gst_element("vaapih265enc"), "vaapih265enc ! video/x-h265,profile=main ! h265parse"),
       ),
       "mpeg2" : dict(
-        sw = (ALL_PLATFORMS, have_gst_element("avenc_mpeg2video"), "avenc_mpeg2video ! mpegvideoparse"),
-        hw = (MPEG2_ENCODE_PLATFORMS, have_gst_element("vaapimpeg2enc"), "vaapimpeg2enc ! mpegvideoparse"),
+        sw = (dict(maxres = (2048, 2048)), have_gst_element("avenc_mpeg2video"), "avenc_mpeg2video ! mpegvideoparse"),
+        hw = (platform.get_caps("encode", "mpeg2"), have_gst_element("vaapimpeg2enc"), "vaapimpeg2enc ! mpegvideoparse"),
       ),
       "mjpeg" : dict(
-        sw = (ALL_PLATFORMS, have_gst_element("jpegenc"), "jpegenc ! jpegparse"),
-        hw = (JPEG_ENCODE_PLATFORMS, have_gst_element("vaapijpegenc"), "vaapijpegenc ! jpegparse"),
+        sw = (dict(maxres = (16384, 16384)), have_gst_element("jpegenc"), "jpegenc ! jpegparse"),
+        hw = (platform.get_caps("vdenc", "jpeg"), have_gst_element("vaapijpegenc"), "vaapijpegenc ! jpegparse"),
       ),
     },
     vpp = {
       "scale" : dict(
-        sw = (ALL_PLATFORMS, have_gst_element("videoscale"), "videoscale ! video/x-raw,width={width},height={height}"),
-        hw = (VPP_PLATFORMS, have_gst_element("vaapipostproc"), "vaapipostproc ! video/x-raw,width={width},height={height}"),
+        sw = (True, have_gst_element("videoscale"), "videoscale ! video/x-raw,width={width},height={height}"),
+        hw = (platform.get_caps("vpp", "scale"), have_gst_element("vaapipostproc"), "vaapipostproc ! video/x-raw,width={width},height={height}"),
       ),
     },
   )
@@ -80,7 +79,7 @@ class TranscoderTest(slash.Test):
   def get_requirements_data(self, ttype, codec, mode):
     return  self.requirements[ttype].get(
       codec, {}).get(
-        mode, ([], (False, "{}:{}:{}".format(ttype, codec, mode)), None))
+        mode, (None, (False, "{}:{}:{}".format(ttype, codec, mode)), None))
 
   def get_decoder(self, codec, mode):
     _, _, decoder = self.get_requirements_data("decode", codec, mode)
@@ -108,46 +107,56 @@ class TranscoderTest(slash.Test):
       "mjpeg"   : "mjpeg",
     }.get(codec, "???")
 
-  def validate_spec(self):
-    from slash.utils.pattern_matching import Matcher
-
+  def validate_caps(self):
     assert len(self.outputs), "Invalid test case specification, outputs data empty"
     assert self.mode in ["sw", "hw"], "Invalid test case specification as mode type not valid"
 
-    # generate platform list based on test runtime parameters
-    iplats, ireq, _ =  self.get_requirements_data("decode", self.codec, self.mode)
-    platforms = set(iplats)
+    icaps, ireq, _ = self.get_requirements_data("decode", self.codec, self.mode)
     requires = [ireq,]
+
+    if icaps is None:
+      slash.skip_test(
+        "decode.{codec}.{mode} unsupported".format(**vars(self)))
+
+    maxw, maxh = icaps["maxres"]
+    if self.width > maxw or self.height > maxh:
+      slash.skip_test(
+        "decode.{codec}.{mode}.{width}x{height} unsupported".format(**vars(self)))
 
     for output in self.outputs:
       codec = output["codec"]
       mode  = output["mode"]
       assert mode in ["sw", "hw"], "Invalid test case specification as output mode type not valid"
-      oplats, oreq, _ = self.get_requirements_data("encode", codec, mode)
-      platforms &= set(oplats)
+      ocaps, oreq, _ = self.get_requirements_data("encode", codec, mode)
       requires.append(oreq)
 
-      if output.get("width", None) is not None or output.get("height", None) is not None:
-        oplats, oreq, _ = self.get_requirements_data("vpp", "scale", mode)
-        platforms &= set(oplats)
+      if ocaps is None:
+        slash.skip_test(
+          "encode.{codec}.{mode} unsupported".format(codec = codec, mode = mode))
+
+      maxw, maxh = ocaps["maxres"]
+      w = output.get("width", None)
+      h = output.get("height", None)
+      if (w or self.width) > maxw or (h or self.height) > maxh:
+        slash.skip_test(
+          "encode.{codec}.{mode}.{width}x{height} unsupported".format(
+            codec = codec, mode = mode,
+            width = (w or self.width),
+            height = (h or self.height)))
+
+      if w is not None or h is not None:
+        ocaps, oreq, _ = self.get_requirements_data("vpp", "scale", mode)
         requires.append(oreq)
 
-    # create matchers based on command-line filters
-    matchers = [Matcher(s) for s in slash.config.root.run.filter_strings]
-
-    # check if user supplied any platform tag on command line
-    pmatch = any(map(lambda p: any([m.matches(p) for m in matchers]), ALL_PLATFORMS))
-
-    # if user supplied a platform tag, check if this test can support it via the
-    # test param-based required platforms
-    if pmatch and not any(map(lambda p: any([m.matches(p) for m in matchers]), platforms)):
-      slash.skip_test("unsupported platform")
+        if ocaps is None:
+          slash.skip_test(
+            "vpp.scale.{mode} unsupported".format(mode = mode))
 
     # check required
-    if not all([t for t,m in requires]):
+    unmet = set([m for t,m in requires if not t])
+    if len(unmet) != 0:
       slash.skip_test(
-        "One or more software requirements not met: {}".format(
-          str([m for t,m in requires if not t])))
+        "Missing one or more required gstreamer elements: {}".format(list(unmet)))
 
   def gen_input_opts(self):
     opts = "filesrc location={source}"
@@ -156,7 +165,7 @@ class TranscoderTest(slash.Test):
 
   def gen_output_opts(self):
     self.goutputs = dict()
-    opts = "! tee name=transcoder"
+    opts = "tee name=transcoder"
 
     for n, output in enumerate(self.outputs):
       codec = output["codec"]
@@ -188,29 +197,36 @@ class TranscoderTest(slash.Test):
 
     return opts.format(**vars(self))
 
+  @timefn("gst")
+  def call_gst(self, iopts, oopts):
+    call("gst-launch-1.0 -vf {iopts} ! {oopts}".format(
+      iopts = iopts, oopts = oopts))
+
   def transcode(self):
-    self.validate_spec()
+    self.validate_caps()
     iopts = self.gen_input_opts()
     oopts = self.gen_output_opts()
 
     get_media().test_call_timeout = vars(self).get("call_timeout", 0)
 
-    call("gst-launch-1.0 -vf {} {}".format(iopts, oopts))
+    self.call_gst(iopts, oopts)
 
     for n, output in enumerate(self.outputs):
+      get_media()._set_test_details(**{"output.{}".format(n) : output})
       for channel in xrange(output.get("channels", 1)):
         encoded = self.goutputs[n][channel]
         yuv = get_media()._test_artifact(
           "{}_{}_{}.yuv".format(self.case, n, channel))
-        vppscale = self.get_vpp_scale(self.width, self.height, "hw")
-        call(
-          "gst-launch-1.0 -vf filesrc location={}"
-          " ! {} ! {}"
-          " ! videoconvert ! video/x-raw,format=I420"
-          " ! checksumsink2 file-checksum=false qos=false"
-          " frame-checksum=false plane-checksum=false dump-output=true"
-          " dump-location={}".format(
-            encoded, self.get_decoder(output["codec"], "hw"), vppscale, yuv))
+        iopts = "filesrc location={} ! {}"
+        oopts =  "{} ! videoconvert ! video/x-raw,format=I420"
+        oopts += " ! checksumsink2 file-checksum=false qos=false"
+        oopts += " frame-checksum=false plane-checksum=false dump-output=true"
+        oopts += " dump-location={}"
+
+        self.call_gst(
+          iopts.format(encoded, self.get_decoder(output["codec"], "hw")),
+          oopts.format(self.get_vpp_scale(self.width, self.height, "hw"), yuv))
+
         self.check_metrics(yuv, refctx = [(n, channel)])
         get_media()._purge_test_artifact(yuv)
 
