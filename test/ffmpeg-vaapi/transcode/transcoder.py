c94246cce94c8477fe28be6159b8ee7ab03f1087
U. Artie Eoff <ullysses.a.eoff@intel.com>
2019-08-19 13:56:08 -0700

    ffmpeg-vaapi/transcode: convert to new caps model
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/ffmpeg-vaapi/transcode/transcoder.py
+++ test/ffmpeg-vaapi/transcode/transcoder.py
@@ -10,54 +10,53 @@ import os
 
 @slash.requires(have_ffmpeg)
 @slash.requires(have_ffmpeg_vaapi_accel)
-@platform_tags(ALL_PLATFORMS)
 class TranscoderTest(slash.Test):
   requirements = dict(
     # ffmpeg-vaapi HW decoders are built-in
     decode = {
       "avc" : dict(
-        sw = (ALL_PLATFORMS, have_ffmpeg_decoder("h264"), "h264"),
-        hw = (AVC_DECODE_PLATFORMS, have_ffmpeg_decoder("h264"), "h264"),
+        sw = (dict(maxres = (16384, 16384)), have_ffmpeg_decoder("h264"), "h264"),
+        hw = (platform.get_caps("decode", "avc"), have_ffmpeg_decoder("h264"), "h264"),
       ),
       "hevc-8" : dict(
-        sw = (ALL_PLATFORMS, have_ffmpeg_decoder("hevc"), "hevc"),
-        hw = (HEVC_DECODE_8BIT_PLATFORMS, have_ffmpeg_decoder("hevc"), "hevc"),
+        sw = (dict(maxres = (16384, 16384)), have_ffmpeg_decoder("hevc"), "hevc"),
+        hw = (platform.get_caps("decode", "hevc_8"), have_ffmpeg_decoder("hevc"), "hevc"),
       ),
       "mpeg2" : dict(
-        sw = (ALL_PLATFORMS, have_ffmpeg_decoder("mpeg2video"), "mpeg2video"),
-        hw = (MPEG2_DECODE_PLATFORMS, have_ffmpeg_decoder("mpeg2video"), "mpeg2video"),
+        sw = (dict(maxres = (2048, 2048)), have_ffmpeg_decoder("mpeg2video"), "mpeg2video"),
+        hw = (platform.get_caps("decode", "mpeg2"), have_ffmpeg_decoder("mpeg2video"), "mpeg2video"),
       ),
       "mjpeg" : dict(
-        sw = (ALL_PLATFORMS, have_ffmpeg_decoder("mjpeg"), "mjpeg"),
-        hw = (JPEG_DECODE_PLATFORMS, have_ffmpeg_decoder("mjpeg"), "mjpeg"),
+        sw = (dict(maxres = (16384, 16384)), have_ffmpeg_decoder("mjpeg"), "mjpeg"),
+        hw = (platform.get_caps("decode", "jpeg"), have_ffmpeg_decoder("mjpeg"), "mjpeg"),
       ),
       "vc1" : dict(
-        sw = (ALL_PLATFORMS, have_ffmpeg_decoder("vc1"), "vc1"),
-        hw = (VC1_DECODE_PLATFORMS, have_ffmpeg_decoder("vc1"), "vc1"),
+        sw = (dict(maxres = (16384, 16384)), have_ffmpeg_decoder("vc1"), "vc1"),
+        hw = (platform.get_caps("decode", "vc1"), have_ffmpeg_decoder("vc1"), "vc1"),
       ),
     },
     encode = {
       "avc" : dict(
-        sw = (ALL_PLATFORMS, have_ffmpeg_encoder("libx264"), "libx264"),
-        hw = (AVC_ENCODE_PLATFORMS, have_ffmpeg_encoder("h264_vaapi"), "h264_vaapi"),
+        sw = (dict(maxres = (16384, 16384)), have_ffmpeg_encoder("libx264"), "libx264"),
+        hw = (platform.get_caps("encode", "avc"), have_ffmpeg_encoder("h264_vaapi"), "h264_vaapi"),
       ),
       "hevc-8" : dict(
-        sw = (ALL_PLATFORMS, have_ffmpeg_encoder("libx265"), "libx265"),
-        hw = (HEVC_ENCODE_8BIT_PLATFORMS, have_ffmpeg_encoder("hevc_vaapi"), "hevc_vaapi"),
+        sw = (dict(maxres = (16384, 16384)), have_ffmpeg_encoder("libx265"), "libx265"),
+        hw = (platform.get_caps("encode", "hevc_8"), have_ffmpeg_encoder("hevc_vaapi"), "hevc_vaapi"),
       ),
       "mpeg2" : dict(
-        sw = (ALL_PLATFORMS, have_ffmpeg_encoder("mpeg2video"), "mpeg2video"),
-        hw = (MPEG2_ENCODE_PLATFORMS, have_ffmpeg_encoder("mpeg2_vaapi"), "mpeg2_vaapi"),
+        sw = (dict(maxres = (2048, 2048)), have_ffmpeg_encoder("mpeg2video"), "mpeg2video"),
+        hw = (platform.get_caps("encode", "mpeg2"), have_ffmpeg_encoder("mpeg2_vaapi"), "mpeg2_vaapi"),
       ),
       "mjpeg" : dict(
-        sw = (ALL_PLATFORMS, have_ffmpeg_encoder("mjpeg"), "mjpeg"),
-        hw = (JPEG_ENCODE_PLATFORMS, have_ffmpeg_encoder("mjpeg_vaapi"), "mjpeg_vaapi"),
+        sw = (dict(maxres = (16384, 16384)), have_ffmpeg_encoder("mjpeg"), "mjpeg"),
+        hw = (platform.get_caps("vdenc", "jpeg"), have_ffmpeg_encoder("mjpeg_vaapi"), "mjpeg_vaapi"),
       ),
     },
     vpp = {
       "scale" : dict(
-        sw = (ALL_PLATFORMS, have_ffmpeg_filter("scale"), "scale=w={width}:h={height}"),
-        hw = (VPP_PLATFORMS, have_ffmpeg_filter("scale_vaapi"), "scale_vaapi=w={width}:h={height}"),
+        sw = (True, have_ffmpeg_filter("scale"), "scale=w={width}:h={height}"),
+        hw = (platform.get_caps("vpp", "scale"), have_ffmpeg_filter("scale_vaapi"), "scale_vaapi=w={width}:h={height}"),
       ),
     },
   )
@@ -72,7 +71,7 @@ class TranscoderTest(slash.Test):
   def get_requirements_data(self, ttype, codec, mode):
     return  self.requirements[ttype].get(
       codec, {}).get(
-        mode, ([], (False, "{}:{}:{}".format(ttype, codec, mode)), None))
+        mode, (None, (False, "{}:{}:{}".format(ttype, codec, mode)), None))
 
   def get_decoder(self, codec, mode):
     _, _, decoder = self.get_requirements_data("decode", codec, mode)
@@ -100,46 +99,56 @@ class TranscoderTest(slash.Test):
       "mjpeg"   : "mjpeg",
     }.get(codec, "???")
 
-  def validate_spec(self):
-    from slash.utils.pattern_matching import Matcher
-
+  def validate_caps(self):
     assert len(self.outputs), "Invalid test case specification, outputs data empty"
     assert self.mode in ["sw", "hw"], "Invalid test case specification as mode type not valid"
 
-    # generate platform list based on test runtime parameters
-    iplats, ireq, _ =  self.get_requirements_data("decode", self.codec, self.mode)
-    platforms = set(iplats)
+    icaps, ireq, _ =  self.get_requirements_data("decode", self.codec, self.mode)
     requires = [ireq,]
+
+    if icaps is None:
+      slash.skip_test(
+        "decode.{codec}.{mode} unsupported".format(**vars(self)))
+
+    maxw, maxh = icaps["maxres"]
+    if self.width > maxw or self.height > maxh:
+      slash.skip_test(
+        "decode.{codec}.{mode}.{width}x{height} unsupported".format(**vars(self)))
 
     for output in self.outputs:
       codec = output["codec"]
       mode  = output["mode"]
       assert mode in ["sw", "hw"], "Invalid test case specification as output mode type not valid"
-      oplats, oreq, _ = self.get_requirements_data("encode", codec, mode)
-      platforms &= set(oplats)
+      ocaps, oreq, _ = self.get_requirements_data("encode", codec, mode)
       requires.append(oreq)
 
-      if output.get("width", None) is not None or output.get("height", None) is not None:
-        oplats, oreq, _ = self.get_requirements_data("vpp", "scale", mode)
-        platforms &= set(oplats)
+      if ocaps is None:
+        slash.skip_test(
+          "encode.{codec}.{mode} unsupported".format(codec = codec, mode = mode))
+
+      maxw, maxh = ocaps["maxres"]
+      w = output.get("width", None)
+      h = output.get("height", None)
+      if (w or self.width) > maxw or (h or self.height) > maxh:
+        slash.skip_test(
+          "encode.{codec}.{mode}.{width}x{height} unsupported".format(
+            codec = codec, mode = mode,
+            width = (w or self.width),
+            height = (h or self.height)))
+
+      if w is not None or h is not None:
+        ocaps, oreq, _ = self.get_requirements_data("vpp", "scale", mode)
         requires.append(oreq)
 
-    # create matchers based on command-line filters
-    matchers = [Matcher(s) for s in slash.config.root.run.filter_strings]
-
-    # check if user supplied any platform tag on command line
-    pmatch = any(map(lambda p: any([m.matches(p) for m in matchers]), ALL_PLATFORMS))
-
-    # if user supplied a platform tag, check if this test can support it via the
-    # test param-based required platforms
-    if pmatch and not any(map(lambda p: any([m.matches(p) for m in matchers]), platforms)):
-      slash.skip_test("unsupported platform")
+        if ocaps is None:
+          slash.skip_test(
+            "vpp.scale.{mode} unsupported".format(mode = mode))
 
     # check required
-    if not all([t for t,m in requires]):
+    unmet = set([m for t,m in requires if not t])
+    if len(unmet) != 0:
       slash.skip_test(
-        "One or more software requirements not met: {}".format(
-          str([m for t,m in requires if not t])))
+        "Missing one or more required ffmpeg elements: {}".format(list(unmet)))
 
   def gen_input_opts(self):
     opts = "-init_hw_device vaapi=hw:/dev/dri/renderD128 -filter_hw_device hw"
@@ -205,26 +214,31 @@ class TranscoderTest(slash.Test):
       "hwaccel initialisation returned error", self.output, re.MULTILINE)
     assert m is None, "Failed to use hardware decode"
 
+  @timefn("ffmpeg")
+  def call_ffmpeg(self, iopts, oopts):
+    return call("ffmpeg -v verbose {} {}".format(iopts, oopts))
+
   def transcode(self):
-    self.validate_spec()
+    self.validate_caps()
     iopts = self.gen_input_opts()
     oopts = self.gen_output_opts()
 
     get_media().test_call_timeout = vars(self).get("call_timeout", 0)
-    self.output = call("ffmpeg -v verbose {} {}".format(iopts, oopts))
+    self.output = self.call_ffmpeg(iopts, oopts)
 
     self.check_output()
 
     for n, output in enumerate(self.outputs):
+      get_media()._set_test_details(**{"output.{}".format(n) : output})
       for channel in xrange(output.get("channels", 1)):
         encoded = self.goutputs[n][channel]
         yuv = get_media()._test_artifact(
           "{}_{}_{}.yuv".format(self.case, n, channel))
         vppscale = self.get_vpp_scale(self.width, self.height, "sw")
-        call(
-          "ffmpeg -v verbose -i {} -vf '{}' -pix_fmt yuv420p -f rawvideo"
-          " -vframes {} -y {}".format(
-            encoded, vppscale, self.frames, yuv))
+        iopts = "-i {}"
+        oopts = "-vf '{}' -pix_fmt yuv420p -f rawvideo -vframes {} -y {}"
+        self.call_ffmpeg(
+          iopts.format(encoded), oopts.format(vppscale, self.frames, yuv))
         self.check_metrics(yuv, refctx = [(n, channel)])
         # delete yuv file after each iteration
         get_media()._purge_test_artifact(yuv)
