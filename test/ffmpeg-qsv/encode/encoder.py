c7c12e13d9da21a93c4e47af5f6feb959ab6f988
U. Artie Eoff <ullysses.a.eoff@intel.com>
2019-08-19 18:39:53 -0700

    encode: handle AVC LP CBR/VBR caps
    i965 driver does not support this capability.
    iHD only supports this capability on ICL+.
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/ffmpeg-qsv/encode/encoder.py
+++ test/ffmpeg-qsv/encode/encoder.py
@@ -116,6 +116,11 @@ class EncoderTest(slash.Test):
         format_value(
           "{platform}.{driver}.slice > 1 unsupported in this mode", **vars(self)))
 
+    if not self.caps.get(self.rcmode, True):
+      slash.skip_test(
+        format_value(
+          "{platform}.{driver}.{rcmode} unsupported in this mode", **vars(self)))
+
     self.mprofile = mapprofile(self.codec, self.profile)
     if self.mprofile is None:
       slash.skip_test("{profile} profile is not supported".format(**vars(self)))
@@ -125,10 +130,6 @@ class EncoderTest(slash.Test):
       slash.skip_test("{format} format not supported".format(**vars(self)))
 
     self.hwformat = mapformat(self.hwformat)
-
-    # TODO: add rcmode caps check
-    #       e.g. not all rc modes for LP are supported on all platforms/drivers?
-    #       e.g. iHD drivers supports more rc modes than i965.
 
   def encode(self):
     self.validate_caps()
--- test/gst-msdk/encode/encoder.py
+++ test/gst-msdk/encode/encoder.py
@@ -138,6 +138,11 @@ class EncoderTest(slash.Test):
         format_value(
           "{platform}.{driver}.slice > 1 unsupported in this mode", **vars(self)))
 
+    if not self.caps.get(self.rcmode, True):
+      slash.skip_test(
+        format_value(
+          "{platform}.{driver}.{rcmode} unsupported in this mode", **vars(self)))
+
     self.mprofile = mapprofile(self.codec, self.profile)
     if self.mprofile is None:
       slash.skip_test("{profile} profile is not supported".format(**vars(self)))
@@ -148,10 +153,6 @@ class EncoderTest(slash.Test):
       slash.skip_test("{format} format not supported".format(**vars(self)))
 
     self.hwformat = mapformatu(self.hwformat)
-
-    # TODO: add rcmode caps check
-    #       e.g. not all rc modes for LP are supported on all platforms/drivers?
-    #       e.g. iHD drivers supports more rc modes than i965.
 
   def encode(self):
     self.validate_caps()
