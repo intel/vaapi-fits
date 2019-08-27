3496bb9c0164a31314e7a0f2ef2950bf50d1c9c8
U. Artie Eoff <ullysses.a.eoff@intel.com>
2019-08-27 11:11:44 -0700

    test/ffmpeg-qsv/encode/jpeg: remove decode WA
    mjpeg_qsv decode plugin is available in upstream
    now.  Thus, remove vaapi decode WA.
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/ffmpeg-qsv/encode/jpeg.py
+++ test/ffmpeg-qsv/encode/jpeg.py
@@ -24,12 +24,6 @@ class JPEGEncoderTest(EncoderTest):
   def get_file_ext(self):
     return "mjpeg" if self.frames > 1 else "jpg"
 
-## NOTE: Temporary Workaround for qsv mjpeg encode test until
-## a qsv mjpeg decoder is available.
-@memoize
-def have_ffmpeg_vaapi_accel():
-  return try_call("ffmpeg -hide_banner -hwaccels | grep vaapi")
-
 class cqp(JPEGEncoderTest):
   def init(self, tspec, case, quality):
     self.caps = platform.get_caps("vdenc", "jpeg")
@@ -42,10 +36,7 @@ class cqp(JPEGEncoderTest):
 
   @slash.requires(*platform.have_caps("vdenc", "jpeg"))
   @slash.requires(*have_ffmpeg_encoder("mjpeg_qsv"))
-  ## NOTE: Temporary Workaround for qsv mjpeg encode test until
-  ## a qsv mjpeg decoder is available.
-  @slash.requires(have_ffmpeg_vaapi_accel)
-  #@slash.requires(have_ffmpeg_mjpeg_qsv_decode)
+  @slash.requires(*have_ffmpeg_decoder("mjpeg_qsv"))
   @slash.parametrize(*gen_jpeg_cqp_parameters(spec))
   def test(self, case, quality):
     self.init(spec, case, quality)
@@ -53,32 +44,9 @@ class cqp(JPEGEncoderTest):
 
   @slash.requires(*platform.have_caps("vdenc", "jpeg"))
   @slash.requires(*have_ffmpeg_encoder("mjpeg_qsv"))
-  ## NOTE: Temporary Workaround for qsv mjpeg encode test until
-  ## a qsv mjpeg decoder is available.
-  @slash.requires(have_ffmpeg_vaapi_accel)
-  #@slash.requires(have_ffmpeg_mjpeg_qsv_decode)
+  @slash.requires(*have_ffmpeg_decoder("mjpeg_qsv"))
   @slash.parametrize(*gen_jpeg_cqp_parameters(spec_r2r))
   def test_r2r(self, case, quality):
     self.init(spec_r2r, case, quality)
     vars(self).setdefault("r2r", 5)
     self.encode()
-
-  ## NOTE: Temporary Workaround for qsv mjpeg encode test until
-  ## a qsv mjpeg decoder is available.
-  def check_metrics(self):
-    name = self.gen_name().format(**vars(self))
-    self.decoded = get_media()._test_artifact(
-      "{}-{width}x{height}-{format}.yuv".format(name, **vars(self)))
-
-    call(
-      "ffmpeg -hwaccel vaapi -vaapi_device /dev/dri/renderD128 -v verbose"
-      " -i {encoded} -pix_fmt {mformat} -f rawvideo -vsync passthrough"
-      " -vframes {frames} -y {decoded}".format(**vars(self)))
-
-    get_media().baseline.check_psnr(
-      psnr = calculate_psnr(
-        self.source, self.decoded,
-        self.width, self.height,
-        self.frames, self.format),
-      context = self.refctx,
-    )
