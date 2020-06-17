aa23556ff201e459bfc0c4afbea681a2d3ea6e71
Luo Focus <focus.luo@intel.com>
2020-06-17 06:53:21 +0000

    added hevc 12bit 420 decode implement for gst-msdk
    added hevc 12bit decode implement for gst-msdk in
    vaapi-fits
    
    Signed-off-by: Luo Focus <focus.luo@intel.com>

---
+++ test/gst-msdk/decode/12bit/hevc.py
@@ -0,0 +1,29 @@
+###
+### Copyright (C) 2018-2020 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from .....lib import *
+from ...util import *
+from ..decoder import DecoderTest
+
+spec = load_test_spec("hevc", "decode", "12bit")
+
+class default(DecoderTest):
+  def before(self):
+    # default metric
+    self.metric = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0)
+    self.caps   = platform.get_caps("decode", "hevc_12")
+    super(default, self).before()
+
+  @slash.requires(*platform.have_caps("decode", "hevc_12"))
+  @slash.requires(*have_gst_element("msdkh265dec"))
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      case        = case,
+      gstdecoder  = "h265parse ! msdkh265dec",
+    )
+    self.decode()
