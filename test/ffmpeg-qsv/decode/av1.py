4c3333c726dbe568d790f3e07e8be5b867e9af97
Luo Focus <focus.luo@intel.com>
2020-09-24 11:10:51 -0400

    ffmpeg-qsv enabled av1-8 decode testing
    ffmpeg-qsv enabled av1-8 decode testing
    
    Signed-off-by: Luo Focus <focus.luo@intel.com>

---
+++ test/ffmpeg-qsv/decode/av1.py
@@ -0,0 +1,34 @@
+###
+### Copyright (C) 2018-2020 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ..util import *
+from .decoder import DecoderTest
+
+spec = load_test_spec("av1", "decode", "8bit")
+
+class default(DecoderTest):
+  def before(self):
+    vars(self).update(
+      caps      = platform.get_caps("decode", "av1_8"),
+      ffdecoder = "av1_qsv",
+      # default metric
+      metric    = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0),
+    )
+    super(default, self).before()
+
+  @slash.requires(*platform.have_caps("decode", "av1_8"))
+  @slash.requires(*have_ffmpeg_decoder("av1_qsv"))
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+    vars(self).update(case = case)
+    self.decode()
+
+  def check_output(self):
+    m = re.search("No support for codec av1", self.output, re.MULTILINE)
+    assert m is None, "Failed to use hardware decode"
+    super(default, self).check_output()
