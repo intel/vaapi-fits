1e7e7035593ce22c4446f952dcb6b40eea09bff8
Luo Focus <focus.luo@intel.com>
2020-10-15 17:08:50 -0400

    gst-msdk added av1-8 decode support
    added av1 8bit decode implement support
    
    Signed-off-by: Luo Focus <focus.luo@intel.com>

---
+++ test/gst-msdk/decode/av1.py
@@ -0,0 +1,34 @@
+###
+### Copyright (C) 2018-2020 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ..util import *
+from .decoder import DecoderTest
+
+spec = load_test_spec("av1", "decode", "8bit")
+
+class default(DecoderTest):
+  def before(self):
+    # default metric
+    self.metric = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0)
+    self.caps   = platform.get_caps("decode", "av1_8")
+    super(default, self).before()
+
+  @slash.requires(*platform.have_caps("decode", "av1_8"))
+  @slash.requires(*have_gst_element("msdkav1dec"))
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+
+    dxmap = {".ivf" : "ivfparse", ".webm" : "matroskademux"}
+    ext = os.path.splitext(self.source)[1]
+    assert ext in dxmap.keys(), "Unrecognized source file extension {}".format(ext)
+
+    vars(self).update(
+      case        = case,
+      gstdecoder  = "{} ! msdkav1dec".format(dxmap[ext]),
+    )
+    self.decode()
