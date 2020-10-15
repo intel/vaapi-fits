0cdcd212fe657eae1e66a494fddeeb661d76f754
Luo Focus <focus.luo@intel.com>
2020-10-15 18:13:19 -0400

    vp9-12: gst-msdk add vp9 12bit decode implement
    add vp9 12bit decode implement for gst-msdk decode
    
    Signed-off-by: Luo Focus <focus.luo@intel.com>

---
+++ test/gst-msdk/decode/12bit/vp9.py
@@ -0,0 +1,34 @@
+###
+### Copyright (C) 2018-2020 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from .....lib import *
+from ...util import *
+from ..decoder import DecoderTest
+
+spec = load_test_spec("vp9", "decode", "12bit")
+
+class default(DecoderTest):
+  def before(self):
+    # default metric
+    self.metric = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0)
+    self.caps   = platform.get_caps("decode", "vp9_12")
+    super(default, self).before()
+
+  @slash.requires(*platform.have_caps("decode", "vp9_12"))
+  @slash.requires(*have_gst_element("msdkvp9dec"))
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+
+    dxmap = {".ivf" : "ivfparse", ".webm" : "matroskademux", ".mkv" : "matroskademux"}
+    ext = os.path.splitext(self.source)[1]
+    assert ext in dxmap.keys(), "Unrecognized source file extension {}".format(ext)
+
+    vars(self).update(
+      case        = case,
+      gstdecoder  = "{} ! msdkvp9dec".format(dxmap[ext]),
+    )
+    self.decode()
