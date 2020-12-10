c0fef2bd009c23355115a4d60555e11e6b0dd5be
Luo Focus <focus.luo@intel.com>
2020-12-10 16:21:21 -0500

    added ffmpeg-vaapi av1-10 decode support
    ffmpeg-vaapi add av1-10 decode support
    
    Signed-off-by: Luo Focus <focus.luo@intel.com>

---
+++ test/ffmpeg-vaapi/decode/10bit/av1.py
@@ -0,0 +1,25 @@
+###
+### Copyright (C) 2018-2020 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from .....lib import *
+from .....lib.ffmpeg.vaapi.util import *
+from .....lib.ffmpeg.vaapi.decoder import DecoderTest
+
+spec = load_test_spec("av1", "decode", "10bit")
+
+class default(DecoderTest):
+  def before(self):
+    # default metric
+    self.metric = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0)
+    self.caps   = platform.get_caps("decode", "av1_10")
+    super(default, self).before()
+
+  @slash.requires(*platform.have_caps("decode", "av1_10"))
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+    self.case = case
+    self.decode()
