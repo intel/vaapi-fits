intel/vaapi-fits

commit ea9407b377a1a4a5d44cb71b0ff817c6fdc464ba

WA: ffmpeg/vaapi: set -b_strategy for HEVCe on gen11+
Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

lib/ffmpeg/vaapi/transcoder.py
@@ -42,7 +42,8 @@ class TranscoderTest(slash.Test):
       ),
       "hevc-8" : dict(
         sw = (dict(maxres = (16384, 16384)), have_ffmpeg_encoder("libx265"), "libx265"),
-        hw = (platform.get_caps("encode", "hevc_8"), have_ffmpeg_encoder("hevc_vaapi"), "hevc_vaapi"),
+        # WA: LDB is not enabled by default for HEVCe on gen11+, yet.
+        hw = (platform.get_caps("encode", "hevc_8"), have_ffmpeg_encoder("hevc_vaapi"), "hevc_vaapi" if get_media()._get_gpu_gen() < 11 else "hevc_vaapi -b_strategy 1"),
       ),
       "mpeg2" : dict(
         sw = (dict(maxres = (2048, 2048)), have_ffmpeg_encoder("mpeg2video"), "mpeg2video"),

lib/ffmpeg/vaapi/encoder.py
@@ -66,6 +66,10 @@ class EncoderTest(slash.Test):
       self.level /= 10.0
       opts += " -level {level}"
 
+    # WA: LDB is not enabled by default for HEVCe on gen11+, yet.
+    if get_media()._get_gpu_gen() >= 11 and self.codec.startswith("hevc"):
+      opts += " -b_strategy 1"
+
     opts += " -vframes {frames} -y {encoded}"
 
     return opts

commit 2470b82958f3055b5c659c6740bb348358f91d97

ffmpeg/vaapi: fix "list" command error caused by WA
This fixes a bug introduced by ea9407b377a1a.

The gpu gen is only known during "run" command.  Thus, move
LDB workaround to test runtime so we don't break other
commands.

Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

lib/ffmpeg/vaapi/transcoder.py
@@ -42,8 +42,7 @@ class TranscoderTest(slash.Test):
       ),
       "hevc-8" : dict(
         sw = (dict(maxres = (16384, 16384)), have_ffmpeg_encoder("libx265"), "libx265"),
-        # WA: LDB is not enabled by default for HEVCe on gen11+, yet.
-        hw = (platform.get_caps("encode", "hevc_8"), have_ffmpeg_encoder("hevc_vaapi"), "hevc_vaapi" if get_media()._get_gpu_gen() < 11 else "hevc_vaapi -b_strategy 1"),
+        hw = (platform.get_caps("encode", "hevc_8"), have_ffmpeg_encoder("hevc_vaapi"), "hevc_vaapi"),
       ),
       "mpeg2" : dict(
         sw = (dict(maxres = (2048, 2048)), have_ffmpeg_encoder("mpeg2video"), "mpeg2video"),
@@ -171,6 +170,10 @@ class TranscoderTest(slash.Test):
       mode = output["mode"]
       encoder = self.get_encoder(codec, mode)
       ext = self.get_file_ext(codec)
+
+      # WA: LDB is not enabled by default for HEVCe on gen11+, yet.
+      if "hevc_vaapi" == encoder and get_media()._get_gpu_gen() > 10:
+        encoder += " -b_strategy 1"
 
       filters = []
       tmode = (self.mode, mode)