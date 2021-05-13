78d8a52183a760e1785ba2b10cbf95b29f10ff77
U. Artie Eoff <ullysses.a.eoff@intel.com>
2021-05-13 13:24:08 -0700

    test/gst-msdk/encode: common bases for lp/non-lp
    Create common base classes for lowpower and non-low-power
    in each codec test to reduce code duplication.
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/gst-msdk/encode/hevc.py
+++ test/gst-msdk/encode/hevc.py
@@ -13,25 +13,40 @@ spec_r2r  = load_test_spec("hevc", "encode", "8bit", "r2r")
 
 @slash.requires(*have_gst_element("msdkh265enc"))
 @slash.requires(*have_gst_element("msdkh265dec"))
-class HEVC8EncoderTest(EncoderTest):
+class HEVC8EncoderBaseTest(EncoderTest):
   def before(self):
+    super().before()
     vars(self).update(
       codec         = "hevc-8",
       gstencoder    = "msdkh265enc",
       gstdecoder    = "h265parse ! msdkh265dec hardware=true",
       gstmediatype  = "video/x-h265",
       gstparser     = "h265parse",
-      lowpower      = False,
     )
-    super(HEVC8EncoderTest, self).before()
 
   def get_file_ext(self):
     return "h265"
 
 @slash.requires(*platform.have_caps("encode", "hevc_8"))
+class HEVC8EncoderTest(HEVC8EncoderBaseTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      caps      = platform.get_caps("encode", "hevc_8"),
+      lowpower  = False,
+    )
+
+@slash.requires(*platform.have_caps("vdenc", "hevc_8"))
+class HEVC8EncoderLPTest(HEVC8EncoderBaseTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      caps      = platform.get_caps("vdenc", "hevc_8"),
+      lowpower  = True,
+    )
+
 class cqp(HEVC8EncoderTest):
   def init(self, tspec, case, gop, slices, bframes, qp, quality, profile):
-    self.caps = platform.get_caps("encode", "hevc_8")
     vars(self).update(tspec[case].copy())
     vars(self).update(
       bframes = bframes,
@@ -55,16 +70,13 @@ class cqp(HEVC8EncoderTest):
     vars(self).setdefault("r2r", 5)
     self.encode()
 
-@slash.requires(*platform.have_caps("vdenc", "hevc_8"))
-class cqp_lp(HEVC8EncoderTest):
+class cqp_lp(HEVC8EncoderLPTest):
   def init(self, tspec, case, gop, slices, qp, quality, profile):
-    self.caps = platform.get_caps("vdenc", "hevc_8")
     vars(self).update(tspec[case].copy())
     vars(self).update(
       case    = case,
       gop     = gop,
       qp      = qp,
-      lowpower= True,
       quality = quality,
       profile = profile,
       rcmode  = "cqp",
@@ -82,10 +94,8 @@ class cqp_lp(HEVC8EncoderTest):
     vars(self).setdefault("r2r", 5)
     self.encode()
 
-@slash.requires(*platform.have_caps("encode", "hevc_8"))
 class cbr(HEVC8EncoderTest):
   def init(self, tspec, case, gop, slices, bframes, bitrate, fps, profile):
-    self.caps = platform.get_caps("encode", "hevc_8")
     vars(self).update(tspec[case].copy())
     vars(self).update(
       bframes = bframes,
@@ -111,17 +121,14 @@ class cbr(HEVC8EncoderTest):
     vars(self).setdefault("r2r", 5)
     self.encode()
 
-@slash.requires(*platform.have_caps("vdenc", "hevc_8"))
-class cbr_lp(HEVC8EncoderTest):
+class cbr_lp(HEVC8EncoderLPTest):
   def init(self, tspec, case, gop, slices, bitrate, fps, profile):
-    self.caps = platform.get_caps("vdenc", "hevc_8")
     vars(self).update(tspec[case].copy())
     vars(self).update(
       bitrate = bitrate,
       case    = case,
       fps     = fps,
       gop     = gop,
-      lowpower= True,
       maxrate = bitrate,
       minrate = bitrate,
       profile = profile,
@@ -140,10 +147,8 @@ class cbr_lp(HEVC8EncoderTest):
     vars(self).setdefault("r2r", 5)
     self.encode()
 
-@slash.requires(*platform.have_caps("encode", "hevc_8"))
 class vbr(HEVC8EncoderTest):
   def init(self, tspec, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
-    self.caps = platform.get_caps("encode", "hevc_8")
     vars(self).update(tspec[case].copy())
     vars(self).update(
       bframes = bframes,
@@ -172,17 +177,14 @@ class vbr(HEVC8EncoderTest):
     vars(self).setdefault("r2r", 5)
     self.encode()
 
-@slash.requires(*platform.have_caps("vdenc", "hevc_8"))
-class vbr_lp(HEVC8EncoderTest):
+class vbr_lp(HEVC8EncoderLPTest):
   def init(self, tspec, case, gop, slices, bitrate, fps, quality, refs, profile):
-    self.caps = platform.get_caps("vdenc", "hevc_8")
     vars(self).update(tspec[case].copy())
     vars(self).update(
       bitrate = bitrate,
       case    = case,
       fps     = fps,
       gop     = gop,
-      lowpower= True,
       # target percentage 50%
       maxrate = bitrate * 2,
       minrate = bitrate,
