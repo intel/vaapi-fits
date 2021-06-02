a118beb79ba3c313ba88b1a1e0a3932b53da5289
Bin Wu <bin1.wu@intel.com>
2021-06-02 14:30:27 +0800

    Add skip_test_by_parameter on lib/util
    Try to skip duplicate case with unused parameter
    which is not support by plugin
    
    Signed-off-by: Bin Wu <bin1.wu@intel.com>

--- lib/util.py
+++ lib/util.py
@@ -14,3 +14,20 @@ def skip_test_if_missing_features(test):
         format_value(
           "{platform}.{driver}.feature({feature}) not supported",
           **vars(test), feature = feature))
+
+case_parameter_list = {}
+def skip_test_by_parameter(class_name, skip_lst, **kwargs):
+    if class_name not in case_parameter_list:
+      case_parameter_list[class_name] = list()
+    case_dict = case_parameter_list[class_name]
+    parameter_str = ''
+    for k,v in kwargs.items():
+      if k in skip_lst:
+        slash.logger.notice("NOTICE: '{}' parameter unused (not supported by plugin)".format(k))
+      else:
+        parameter_str += '{}={} '.format(k, v)
+
+    if parameter_str not in case_dict:
+      case_dict.append(parameter_str)
+    else:
+      slash.skip_test("{} already run, because '{}' unused".format(parameter_str, ','.join(skip_lst)))
