c8d247b763d883abef6426e4078cc10c608229ac
U. Artie Eoff <ullysses.a.eoff@intel.com>
2021-06-22 21:21:54 -0700

    ffmpeg-qsv: add mfx runtime check test
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

---
+++ test/ffmpeg-qsv/general/runtime.py
@@ -0,0 +1,27 @@
+###
+### Copyright (C) 2021 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+import slash
+
+from ....lib.common import get_media
+from ....lib.ffmpeg.qsv.util import *
+from ....lib.mfx.runtime import MFXRuntimeTest
+
+@slash.requires(have_ffmpeg)
+@slash.requires(have_ffmpeg_qsv_accel)
+@slash.requires(using_compatible_driver)
+class detect(MFXRuntimeTest):
+  def before(self):
+    super().before()
+    self.renderDevice = get_media().render_device
+
+  def test(self):
+    self.check(
+      "ffmpeg -nostats -v verbose -init_hw_device qsv=qsv:hw"
+      " -qsv_device {renderDevice} -hwaccel qsv"
+      " -filter_hw_device qsv -f lavfi -i yuvtestsrc"
+      " -f null /dev/null".format(**vars(self))
+    )
