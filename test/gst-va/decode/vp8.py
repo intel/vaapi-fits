f74ca367a542b20b933b43d154b5de3d27a1939d
Bin Wu <bin1.wu@intel.com>
2021-07-27 22:09:35 +0800

    Add gst-va/decode/vp8 support
    Signed-off-by: Bin Wu <bin1.wu@intel.com>

---
+++ test/gst-va/decode/vp8.py
@@ -0,0 +1,34 @@
+###
+### Copyright (C) 2021 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ....lib.gstreamer.va.util import *
+from ....lib.gstreamer.va.decoder import DecoderTest
+
+spec = load_test_spec("vp8", "decode")
+
+@slash.requires(*platform.have_caps("decode", "vp8"))
+@slash.requires(*have_gst_element("vavp8dec"))
+class default(DecoderTest):
+  def before(self):
+    # default metric
+    self.metric = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0)
+    self.caps   = platform.get_caps("decode", "vp8")
+    super(default, self).before()
+
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+
+    dxmap = {".ivf" : "ivfparse", ".webm" : "matroskademux"}
+    ext = os.path.splitext(self.source)[1]
+    assert ext in dxmap.keys(), "Unrecognized source file extension {}".format(ext)
+
+    vars(self).update(
+      case        = case,
+      gstdecoder  = "{} ! vavp8dec".format(dxmap[ext]),
+    )
+    self.decode()
