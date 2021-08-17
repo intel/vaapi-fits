5f88334ec7e06249c0d94fe6ace24a0d9c911a84
U. Artie Eoff <ullysses.a.eoff@intel.com>
2021-08-17 15:34:17 -0700

    lib/ffmpeg/qsv: use common decode base
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- lib/ffmpeg/qsv/decoder.py
+++ lib/ffmpeg/qsv/decoder.py
@@ -1,85 +1,30 @@
 ###
-### Copyright (C) 2018-2019 Intel Corporation
+### Copyright (C) 2018-2021 Intel Corporation
 ###
 ### SPDX-License-Identifier: BSD-3-Clause
 ###
 
-from ....lib import *
-from .util import *
+import re
+import slash
 
-@slash.requires(have_ffmpeg)
+from ....lib.common import get_media
+from ....lib.ffmpeg.decoderbase import BaseDecoderTest
+from ....lib.ffmpeg.util import have_ffmpeg_hwaccel
+from ....lib.ffmpeg.qsv.util import using_compatible_driver
+
 @slash.requires(have_ffmpeg_hwaccel("qsv"))
 @slash.requires(using_compatible_driver)
-class DecoderTest(slash.Test):
+class DecoderTest(BaseDecoderTest):
   def before(self):
-    self.refctx = []
-    self.renderDevice = get_media().render_device
-
-  @timefn("ffmpeg")
-  def call_ffmpeg(self):
-    self.output = call(
-      "ffmpeg -init_hw_device qsv=qsv:hw_any,child_device={renderDevice} -hwaccel qsv"
-      " -hwaccel_output_format qsv -v verbose -c:v {ffdecoder} -i {source}"
-      " -vf 'hwdownload,format={hwformat}'"
-      " -pix_fmt {mformat} -f rawvideo -vsync passthrough"
-      " -autoscale 0 -vframes {frames} -y {decoded}".format(**vars(self)))
-
-  def gen_name(self):
-    name = "{case}_{width}x{height}_{format}"
-    if vars(self).get("r2r", None) is not None:
-      name += "_r2r"
-    return name
-
-  def validate_caps(self):
-    self.hwformat = map_best_hw_format(self.format, self.caps["fmts"])
-    self.mformat = mapformat(self.format)
-    if None in [self.hwformat, self.mformat]:
-      slash.skip_test("{format} format not supported".format(**vars(self)))
-
-    maxw, maxh = self.caps["maxres"]
-    if self.width > maxw or self.height > maxh:
-      slash.skip_test(
-        format_value(
-          "{platform}.{driver}.{width}x{height} not supported", **vars(self)))
-
-    skip_test_if_missing_features(self)
-
-  def decode(self):
-    self.validate_caps()
-
-    get_media().test_call_timeout = vars(self).get("call_timeout", 0)
-
-    name = self.gen_name().format(**vars(self))
-    self.decoded = get_media()._test_artifact("{}.yuv".format(name))
-    self.call_ffmpeg()
-
-    if vars(self).get("r2r", None) is not None:
-      assert type(self.r2r) is int and self.r2r > 1, "invalid r2r value"
-      md5ref = md5(self.decoded)
-      get_media()._set_test_details(md5_ref = md5ref)
-      for i in range(1, self.r2r):
-        self.decoded = get_media()._test_artifact("{}_{}.yuv".format(name, i))
-        self.call_ffmpeg()
-        result = md5(self.decoded)
-        get_media()._set_test_details(**{"md5_{:03}".format(i) : result})
-        assert result == md5ref, "r2r md5 mismatch"
-        # delete decoded file after each iteration
-        get_media()._purge_test_artifact(self.decoded)
-    else:
-      self.check_output()
-      self.check_metrics()
+    super().before()
+    self.hwaccel = "qsv"
 
   def check_output(self):
+    super().check_output()
+
     m = re.search(
       "not supported for hardware decode", self.output, re.MULTILINE)
     assert m is None, "Failed to use hardware decode"
 
-    m = re.search(
-      "hwaccel initialisation returned error", self.output, re.MULTILINE)
-    assert m is None, "Failed to use hardware decode"
-
     m = re.search("Initialize MFX session", self.output, re.MULTILINE)
     assert m is not None, "It appears that the QSV plugin did not load"
-
-  def check_metrics(self):
-    check_metric(**vars(self))
