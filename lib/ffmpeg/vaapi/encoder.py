f173d4cdfd8ae11f30fceeab4d9204092368712a
U. Artie Eoff <ullysses.a.eoff@intel.com>
2021-08-24 11:50:09 -0700

    lib/ffmpeg/vaapi: use common encode base
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- lib/ffmpeg/vaapi/encoder.py
+++ lib/ffmpeg/vaapi/encoder.py
@@ -1,186 +1,45 @@
 ###
-### Copyright (C) 2018-2019 Intel Corporation
+### Copyright (C) 2018-2021 Intel Corporation
 ###
 ### SPDX-License-Identifier: BSD-3-Clause
 ###
 
-from ....lib import *
-from .util import *
+import re
+import slash
 
-@slash.requires(have_ffmpeg)
+from ....lib.ffmpeg.encoderbase import BaseEncoderTest
+from ....lib.ffmpeg.util import have_ffmpeg_hwaccel
+from ....lib.ffmpeg.vaapi.util import mapprofile
+
 @slash.requires(have_ffmpeg_hwaccel("vaapi"))
-class EncoderTest(slash.Test):
-  def gen_input_opts(self):
-    opts = "-f rawvideo -pix_fmt {mformat} -s:v {width}x{height}"
-
-    if vars(self).get("fps", None) is not None:
-      opts += " -r:v {fps}"
-
-    opts += " -i {source}"
-
-    return opts
-
-  def gen_output_opts(self):
-    opts = "-vf 'format={hwformat},hwupload' -c:v {ffenc}"
-
-    # BUG: It appears there's a ffmpeg bug with yuv420p hwupload when using
-    # i965 driver.  Need to report upstream ffmpeg!
-
-    if vars(self).get("profile", None) is not None:
-      opts += " -profile:v {mprofile}"
-
-    if self.codec not in ["jpeg",]:
-      opts += " -rc_mode {rcmodeu}"
-
-    if vars(self).get("gop", None) is not None:
-      opts += " -g {gop}"
-    if vars(self).get("qp", None) is not None:
-      if self.codec in ["vp8", "vp9",]:
-        opts += " -global_quality {qp}"
-      elif self.codec in ["mpeg2"]:
-        opts += " -global_quality {mqp}"
-      else:
-        opts += " -qp {qp}"
-    if vars(self).get("quality", None) is not None:
-      if self.codec in ["jpeg",]:
-        opts += " -global_quality {quality}"
-      else:
-        opts += " -quality {quality}"
-    if vars(self).get("slices", None) is not None:
-      opts += " -slices {slices}"
-    if vars(self).get("bframes", None) is not None:
-      opts += " -bf {bframes}"
-    if vars(self).get("minrate", None) is not None:
-      opts += " -b:v {minrate}k"
-    if vars(self).get("maxrate", None) is not None:
-      opts += " -maxrate {maxrate}k"
-    if vars(self).get("refmode", None) is not None:
-      opts += " -refs {refs}"
-    if vars(self).get("lowpower", None) is not None:
-      opts += " -low_power {lowpower}"
-    if vars(self).get("loopshp", None) is not None:
-      opts += " -loop_filter_sharpness {loopshp}"
-    if vars(self).get("looplvl", None) is not None:
-      opts += " -loop_filter_level {looplvl}"
-    if vars(self).get("level", None) is not None:
-      self.level /= 10.0
-      opts += " -level {level}"
-
-    # WA: LDB is not enabled by default for HEVCe on gen11+, yet.
-    if get_media()._get_gpu_gen() >= 11 and self.codec.startswith("hevc"):
-      opts += " -b_strategy 1"
-
-    opts += " -vframes {frames} -y {encoded}"
-
-    return opts
-
-  def gen_name(self):
-    name = "{case}-{rcmode}"
-    if vars(self).get("profile", None) is not None:
-      name += "-{profile}"
-    if vars(self).get("fps", None) is not None:
-      name += "-{fps}"
-    if vars(self).get("gop", None) is not None:
-      name += "-{gop}"
-    if vars(self).get("qp", None) is not None:
-      name += "-{qp}"
-    if vars(self).get("slices", None) is not None:
-      name += "-{slices}"
-    if vars(self).get("quality", None) is not None:
-      name += "-{quality}"
-    if vars(self).get("bframes", None) is not None:
-      name += "-{bframes}"
-    if vars(self).get("minrate", None) is not None:
-      name += "-{minrate}k"
-    if vars(self).get("maxrate", None) is not None:
-      name += "-{maxrate}k"
-    if vars(self).get("refmode", None) is not None:
-      name += "-{refs}"
-    if vars(self).get("lowpower", None) is not None:
-      name += "-{lowpower}"
-    if vars(self).get("loopshp", None) is not None:
-      name += "-{loopshp}"
-    if vars(self).get("looplvl", None) is not None:
-      name += "-{looplvl}"
-    if vars(self).get("r2r", None) is not None:
-      name += "-r2r"
-    if vars(self).get("level", None) is not None:
-      name += "-{level}"
-
-    return name
-
+class EncoderTest(BaseEncoderTest):
   def before(self):
-    self.refctx = []
-    self.renderDevice = get_media().render_device
+    super().before()
+    self.hwaccel = "vaapi"
 
-  @timefn("ffmpeg")
-  def call_ffmpeg(self, iopts, oopts):
-    self.output = call(
-      "ffmpeg -hwaccel vaapi -vaapi_device {renderDevice} -v verbose"
-      " {iopts} {oopts}".format(renderDevice = self.renderDevice, iopts = iopts, oopts = oopts))
+  def get_vaapi_profile(self):
+    raise NotImplementedError
+
+  def map_profile(self):
+    return mapprofile(self.codec, self.profile)
+
+  def gen_qp_opts(self):
+    if self.codec in ["vp8", "vp9",]:
+      return " -global_quality {qp}"
+    if self.codec in ["mpeg2"]:
+      return " -global_quality {mqp}"
+    return " -qp {qp}"
+
+  def gen_quality_opts(self):
+    if self.codec in ["jpeg"]:
+      return " -global_quality {quality}"
+    return " -quality {quality}"
 
   def validate_caps(self):
-    # BUG: FFmpeg fails to support I420 hwupload even though i965 supports it.
-    ifmts = list(set(self.caps["fmts"]) - set(["I420"]))
-    self.hwformat = map_best_hw_format(self.format, ifmts)
-    self.mformat = mapformat(self.format)
-    if None in [self.hwformat, self.mformat]:
-      slash.skip_test("{format} not supported".format(**vars(self)))
-
-    skip_test_if_missing_features(self)
-
-    maxw, maxh = self.caps["maxres"]
-    if self.width > maxw or self.height > maxh:
-      slash.skip_test(
-        format_value(
-          "{platform}.{driver}.{width}x{height} not supported", **vars(self)))
-
-    if vars(self).get("slices", 1) > 1 and not self.caps.get("multislice", True):
-      slash.skip_test(
-        format_value(
-          "{platform}.{driver}.slice > 1 unsupported in this mode", **vars(self)))
-
-    if not self.caps.get(self.rcmode, True):
-      slash.skip_test(
-        format_value(
-          "{platform}.{driver}.{rcmode} unsupported in this mode", **vars(self)))
-
-    if vars(self).get("profile", None) is not None:
-      self.mprofile = mapprofile(self.codec, self.profile)
-      if self.mprofile is None:
-        slash.skip_test("{profile} profile is not supported".format(**vars(self)))
-
-    vars(self).update(rcmodeu = self.rcmode.upper())
-
-  def encode(self):
-    self.validate_caps()
-
-    get_media().test_call_timeout = vars(self).get("call_timeout", 0)
-
-    iopts = self.gen_input_opts()
-    oopts = self.gen_output_opts()
-    name  = self.gen_name().format(**vars(self))
-    ext   = self.get_file_ext()
-
-    self.encoded = get_media()._test_artifact("{}.{}".format(name, ext))
-    self.call_ffmpeg(iopts.format(**vars(self)), oopts.format(**vars(self)))
-
-    if vars(self).get("r2r", None) is not None:
-      assert type(self.r2r) is int and self.r2r > 1, "invalid r2r value"
-      md5ref = md5(self.encoded)
-      get_media()._set_test_details(md5_ref = md5ref)
-      for i in range(1, self.r2r):
-        self.encoded = get_media()._test_artifact("{}_{}.{}".format(name, i, ext))
-        self.call_ffmpeg(iopts.format(**vars(self)), oopts.format(**vars(self)))
-        result = md5(self.encoded)
-        get_media()._set_test_details(**{"md5_{:03}".format(i) : result})
-        assert result == md5ref, "r2r md5 mismatch"
-        # delete encoded file after each iteration
-        get_media()._purge_test_artifact(self.encoded)
-    else:
-      self.check_output()
-      self.check_bitrate()
-      self.check_metrics()
+    super().validate_caps()
+    self.ffencoder = self.ffenc
+    if self.codec not in ["jpeg"]:
+      self.rcmodeu = self.rcmode.upper()
 
   def check_output(self):
     # profile
@@ -221,47 +80,3 @@ class EncoderTest(slash.Test):
     ]
     m = re.search(ipbmsgs[ipbmode], self.output, re.MULTILINE)
     assert m is not None, "Possible incorrect IPB mode used"
-
-    if vars(self).get("level", None) is not None:
-      self.check_level()
-
-  def check_metrics(self):
-    iopts = "-i {encoded}"
-    oopts = (
-      "-pix_fmt {mformat} -f rawvideo -vsync passthrough"
-      " -vframes {frames} -y {decoded}")
-    name = (self.gen_name() + "-{width}x{height}-{format}").format(**vars(self))
-    self.decoded = get_media()._test_artifact("{}.yuv".format(name))
-    self.call_ffmpeg(iopts.format(**vars(self)), oopts.format(**vars(self)))
-
-    get_media().baseline.check_psnr(
-      psnr = calculate_psnr(
-        self.source, self.decoded,
-        self.width, self.height,
-        self.frames, self.format),
-      context = self.refctx,
-    )
-
-  def check_bitrate(self):
-    encsize = os.path.getsize(self.encoded)
-    bitrate_actual = encsize * 8 * vars(self).get("fps", 25) / 1024.0 / self.frames
-    get_media()._set_test_details(
-      size_encoded = encsize,
-      bitrate_actual = "{:-.2f}".format(bitrate_actual))
-
-    if "cbr" == self.rcmode:
-      bitrate_gap = abs(bitrate_actual - self.bitrate) / self.bitrate
-      get_media()._set_test_details(bitrate_gap = "{:.2%}".format(bitrate_gap))
-
-      # acceptable bitrate within 10% of bitrate
-      assert(bitrate_gap <= 0.10)
-
-    elif "vbr" == self.rcmode:
-      # acceptable bitrate within 25% of minrate and 10% of maxrate
-      assert(self.minrate * 0.75 <= bitrate_actual <= self.maxrate * 1.10)
-
-  def check_level(self):
-    output = call(
-      "ffprobe -i {encoded} -v quiet -show_entries stream=level"
-      " -of default=nk=1:nw=1".format(**vars(self)))
-    assert float(output)/30 == self.level, "fail to set target level"
