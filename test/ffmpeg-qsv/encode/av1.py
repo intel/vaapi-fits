d12a015d60492a21b6f3a0a7c54b7639f1b45633
Focus Luo <focus.luo@intel.com>
2021-08-27 02:56:14 +0000

    av1-8 enc: ffmpeg-qsv added new av1 support
    ffmpeg-qsv added new encode av1 8bit support
    
    Signed-off-by: Focus Luo <focus.luo@intel.com>

---
+++ test/ffmpeg-qsv/encode/av1.py
@@ -0,0 +1,54 @@
+###
+### Copyright (C) 2019-2021 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ....lib.ffmpeg.qsv.util import *
+from ....lib.ffmpeg.qsv.encoder import EncoderTest
+
+spec = load_test_spec("av1", "encode", "8bit")
+
+@slash.requires(*have_ffmpeg_encoder("av1_qsv"))
+@slash.requires(*have_ffmpeg_decoder("av1_qsv"))
+class AV1EncoderBaseTest(EncoderTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      codec     = "av1-8",
+      ffencoder = "av1_qsv",
+      ffdecoder = "av1_qsv",
+    )
+
+  def get_file_ext(self):
+    return "ivf"
+
+@slash.requires(*platform.have_caps("vdenc", "av1_8"))
+class AV1EncoderLPTest(AV1EncoderBaseTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      caps      = platform.get_caps("vdenc", "av1_8"),
+      lowpower  = 1,
+    )
+
+class cqp_lp(AV1EncoderLPTest):
+  def init(self, tspec, case, gop, bframes, tilecols, tilerows,qp, quality, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      case      = case,
+      gop       = gop,
+      bframes   = bframes,
+      qp        = qp,
+      rcmode    = "cqp",
+      quality   = quality,
+      profile   = profile,
+      tilerows  = tilerows,
+      tilecols  = tilecols,
+    )
+
+  @slash.parametrize(*gen_av1_cqp_lp_parameters(spec))
+  def test(self, case, gop, bframes, tilecols, tilerows, qp, quality, profile):
+    self.init(spec, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
+    self.encode()
