85d1a4ca03468ca67099de61a6b4ed8c3483c314
U. Artie Eoff <ullysses.a.eoff@intel.com>
2021-08-30 16:31:10 -0700

    lib/gst: decouple decoder command-line from test class
    Move the gstreamer decoder command-line generator into
    separate classes for easier reuse in other test
    contexts.
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- lib/gstreamer/vaapi/decoder.py
+++ lib/gstreamer/vaapi/decoder.py
@@ -8,15 +8,17 @@ import os
 import slash
 
 from ....lib.common import get_media
-from ....lib.gstreamer.decoderbase import BaseDecoderTest
+from ....lib.gstreamer.decoderbase import BaseDecoderTest, Decoder as GstDecoder
 from ....lib.gstreamer.util import have_gst_element
 from ....lib.gstreamer.vaapi.util import mapformatu
 
+class Decoder(GstDecoder):
+  format = property(lambda s: mapformatu(super().format))
+
 @slash.requires(*have_gst_element("vaapi"))
 class DecoderTest(BaseDecoderTest):
+  DecoderClass = Decoder
+
   def before(self):
     super().before()
     os.environ["GST_VAAPI_DRM_DEVICE"] = get_media().render_device
-
-  def map_formatu(self):
-    return mapformatu(self.format)
