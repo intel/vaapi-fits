b3d21e0be582f7a713cc55883898c9ea246c876e
U. Artie Eoff <ullysses.a.eoff@intel.com>
2021-08-30 16:38:40 -0700

    lib/gst: decouple encoder command-line from test class
    Move the gstreamer encoder command-line generator into
    separate classes for easier reuse in other test
    contexts.
    
    Also, reuse the gstreamer decoder classes in the
    gstreamer encode test to check metrics.
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- lib/gstreamer/vaapi/encoder.py
+++ lib/gstreamer/vaapi/encoder.py
@@ -7,63 +7,79 @@
 import os
 import slash
 
-from ....lib.gstreamer.encoderbase import BaseEncoderTest
+from ....lib.gstreamer.encoderbase import BaseEncoderTest, Encoder as GstEncoder
 from ....lib.gstreamer.util import have_gst_element
-from ....lib.gstreamer.vaapi.util import mapprofile, map_best_hw_format, mapformat, mapformatu
-from ....lib.common import get_media
+from ....lib.gstreamer.vaapi.util import mapprofile, map_best_hw_format, mapformat
+from ....lib.gstreamer.vaapi.decoder import Decoder
+from ....lib.common import get_media, mapRangeInt
+
+class Encoder(GstEncoder):
+  @property
+  def hwformat(self):
+    return map_best_hw_format(super().format, self.props["caps"]["fmts"])
+
+  @property
+  def format(self):
+    return mapformat(super().format)
+
+  @property
+  def rcmode(self):
+    if self.codec in ["jpeg"]:
+      return ""
+    return f" rate-control={super().rcmode}"
+
+  @property
+  def qp(self):
+    def inner(qp):
+      if self.codec in ["mpeg2"]:
+        mqp = mapRangeInt(qp, [0, 100], [2, 62])
+        return f" quantizer={mqp}"
+      if self.codec in ["vp8", "vp9"]:
+        return f" yac-qi={qp}"
+      return f" init-qp={qp}"
+    return self.ifprop("qp", inner)
+
+  @property
+  def quality(self):
+    def inner(quality):
+      if self.codec in ["jpeg"]:
+        return f" quality={quality}"
+      return f" quality-level={quality}"
+    return self.ifprop("quality", inner)
+
+  @property
+  def lowpower(self):
+    def inner(lowpower):
+      return f" tune={'low-power' if lowpower else 'none'}"
+    return self.ifprop("lowpower", inner)
+
+  gop     = property(lambda s: s.ifprop("gop", " keyframe-period={gop}"))
+  slices  = property(lambda s: s.ifprop("slices", " num-slices={slices}"))
+  bframes = property(lambda s: s.ifprop("bframes", " max-bframes={bframes}"))
+  maxrate = property(lambda s: s.ifprop("maxrate", " bitrate={maxrate}"))
+  refmode = property(lambda s: s.ifprop("refmode", " ref-pic-mode={refmode}"))
+  refs    = property(lambda s: s.ifprop("refs", " refs={refs}"))
+  loopshp = property(lambda s: s.ifprop("loopshp", " sharpness-level={loopshp}"))
+  looplvl = property(lambda s: s.ifprop("looplvl", " loop-filter-level={looplvl}"))
+
+  @property
+  def gstencoder(self):
+    return (
+      f"{super().gstencoder}"
+      f"{self.rcmode}{self.gop}{self.qp}"
+      f"{self.quality}{self.slices}{self.bframes}"
+      f"{self.maxrate}{self.refmode}{self.refs}"
+      f"{self.lowpower}{self.loopshp}{self.looplvl}"
+    )
 
 @slash.requires(*have_gst_element("vaapi"))
 class EncoderTest(BaseEncoderTest):
+  EncoderClass = Encoder
+  DecoderClass = Decoder
+
   def before(self):
     super().before()
     os.environ["GST_VAAPI_DRM_DEVICE"] = get_media().render_device
 
   def map_profile(self):
     return mapprofile(self.codec, self.profile)
-
-  def map_best_hw_format(self):
-    return map_best_hw_format(self.format, self.caps["fmts"])
-
-  def map_format(self):
-    return mapformat(self.format)
-
-  def map_formatu(self):
-    return mapformatu(self.format)
-
-  def gen_encoder_opts(self):
-    opts = ""
-    if self.codec not in ["jpeg",]:
-      opts += " rate-control={rcmode}"
-    if vars(self).get("gop", None) is not None:
-      opts += " keyframe-period={gop}"
-    if vars(self).get("qp", None) is not None:
-      if self.codec in ["vp8", "vp9",]:
-        opts += " yac-qi={qp}"
-      elif self.codec in ["mpeg2"]:
-        opts += " quantizer={mqp}"
-      else:
-        opts += " init-qp={qp}"
-    if vars(self).get("quality", None) is not None:
-      if self.codec in ["jpeg",]:
-        opts += " quality={quality}"
-      else:
-        opts += " quality-level={quality}"
-    if vars(self).get("slices", None) is not None:
-      opts += " num-slices={slices}"
-    if vars(self).get("bframes", None) is not None:
-      opts += " max-bframes={bframes}"
-    if vars(self).get("maxrate", None) is not None:
-      opts += " bitrate={maxrate}"
-    if vars(self).get("refmode", None) is not None:
-      opts += " ref-pic-mode={refmode}"
-    if vars(self).get("refs", None) is not None:
-      opts += " refs={refs}"
-    if vars(self).get("lowpower", None) is not None:
-      opts += " tune="
-      opts += "low-power" if self.lowpower else "none"
-    if vars(self).get("loopshp", None) is not None:
-      opts += " sharpness-level={loopshp}"
-    if vars(self).get("looplvl", None) is not None:
-      opts += " loop-filter-level={looplvl}"
-
-    return opts
