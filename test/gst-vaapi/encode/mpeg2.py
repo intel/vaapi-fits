304c197e1e7b0c4c0eb9798c7548fdf2fa047950
U. Artie Eoff <ullysses.a.eoff@intel.com>
2021-08-30 20:45:51 -0700

    test/gst/mpeg2e: qp map moved to base
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/gst-vaapi/encode/mpeg2.py
+++ test/gst-vaapi/encode/mpeg2.py
@@ -36,7 +36,6 @@ class cqp(MPEG2EncoderTest):
       bframes = bframes,
       case    = case,
       gop     = gop,
-      mqp     = mapRangeInt(qp, [0, 100], [2, 62]),
       qp      = qp,
       quality = quality,
       rcmode  = "cqp",
