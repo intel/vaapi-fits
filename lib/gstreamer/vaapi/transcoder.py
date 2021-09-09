8a1fd33599d6bc934acda050db07011becf60bc6
Luo Focus <focus.luo@intel.com>
2021-09-09 14:31:39 -0400

    LP trans: gstreamer-vaapi added vdenc transcode impl
    gstreamer-vaapi added vdenc transcode implement
    
    Signed-off-by: Luo Focus <focus.luo@intel.com>

--- lib/gstreamer/vaapi/transcoder.py
+++ lib/gstreamer/vaapi/transcoder.py
@@ -49,10 +49,12 @@ class TranscoderTest(BaseTranscoderTest):
       "avc" : dict(
         sw = (dict(maxres = (16384, 16384)), have_gst_element("x264enc"), "x264enc ! video/x-h264,profile=main ! h264parse"),
         hw = (platform.get_caps("encode", "avc"), have_gst_element("vaapih264enc"), "vaapih264enc ! video/x-h264,profile=main ! h264parse"),
+        lp = (platform.get_caps("vdenc", "avc"), have_gst_element("vaapih264enc"), "vaapih264enc tune=low-power ! video/x-h264,profile=main ! h264parse"),
       ),
       "hevc-8" : dict(
         sw = (dict(maxres = (16384, 16384)), have_gst_element("x265enc"), "videoconvert chroma-mode=none dither=0 ! video/x-raw,format=I420 ! x265enc ! video/x-h265,profile=main ! h265parse"),
         hw = (platform.get_caps("encode", "hevc_8"), have_gst_element("vaapih265enc"), "vaapih265enc ! video/x-h265,profile=main ! h265parse"),
+        lp = (platform.get_caps("vdenc", "hevc_8"), have_gst_element("vaapih265enc"), "vaapih265enc tune=low-power ! video/x-h265,profile=main ! h265parse"),
       ),
       "mpeg2" : dict(
         sw = (dict(maxres = (2048, 2048)), have_gst_element("avenc_mpeg2video"), "avenc_mpeg2video ! mpegvideoparse"),
