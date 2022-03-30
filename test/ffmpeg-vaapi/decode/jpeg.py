5e9251dcc0a18477017dc9cfee668e8a12594d58
U. Artie Eoff <ullysses.a.eoff@intel.com>
2022-03-30 15:42:17 -0700

    ffmpeg-vaapi/decode/jpeg: force scale in/out range to be the same
    WA since ffmpeg e645a1ddb90a863e129108aad9aa7e2d417f3615, scale
    csc filter in/out range don't match in ffmpeg vaapi jpeg decode and causes
    wrong csc.  Thus, force the scaler in/out range to be the same when csc is
    needed.  This ensures the correct output is produced.  Alternative
    proposal was to use yuvj* formats... however, hwaccel doesn't support them
    and they may be removed in the future (deprecated).
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/ffmpeg-vaapi/decode/jpeg.py
+++ test/ffmpeg-vaapi/decode/jpeg.py
@@ -17,6 +17,15 @@ class default(DecoderTest):
     # default metric
     self.metric = dict(type = "ssim", miny = 0.99, minu = 0.99, minv = 0.99)
     self.caps   = platform.get_caps("decode", "jpeg")
+
+    # FIXME: WA since ffmpeg e645a1ddb90a863e129108aad9aa7e2d417f3615, scale
+    # csc filter in/out range don't match in ffmpeg vaapi jpeg decode and causes
+    # wrong csc.  Thus, force the scaler in/out range to be the same when csc is
+    # needed.  This ensures the correct output is produced.  Alternative
+    # proposal was to use yuvj* formats... however, hwaccel doesn't support them
+    # and they may be removed in the future (deprecated).
+    self.ffscale_range = "jpeg"
+
     super(default, self).before()
 
   @slash.parametrize(("case"), sorted(spec.keys()))
