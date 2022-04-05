2a81dbe907d4bbdf12c41573391e870ace1325a0
Xu Yefeng <yefengx.xu@intel.com>
2022-04-05 10:40:01 +0000

    vpp/scale_qsv: added scale_qsv test for ffmpeg-qsv
    enable scale_qsv test for ffmpeg-qsv, it use
    
    same cases and refs with vpp_qsv.
    
    Signed-off-by: Xu Yefeng <yefengx.xu@intel.com>

---
+++ test/ffmpeg-qsv/vpp/scale_qsv.py
@@ -0,0 +1,58 @@
+###
+### Copyright (C) 2018-2019 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ....lib.ffmpeg.qsv.util import *
+from ....lib.ffmpeg.qsv.vpp import VppTest
+
+spec      = load_test_spec("vpp", "scale")
+spec_r2r  = load_test_spec("vpp", "scale", "r2r")
+
+@slash.requires(*platform.have_caps("vpp", "scale"))
+class default(VppTest):
+  def before(self):
+    vars(self).update(
+      caps    = platform.get_caps("vpp", "scale"),
+      vpp_op  = "scale_qsv",
+    )
+    super(default, self).before()
+
+  def init(self, tspec, case, scale_width, scale_height):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      case          = case,
+      scale_height  = scale_height,
+      scale_width   = scale_width,
+    )
+
+  @slash.parametrize(*gen_vpp_scale_parameters(spec))
+  def test(self, case, scale_width, scale_height):
+    self.init(spec, case, scale_width, scale_height)
+    self.vpp()
+
+  @slash.parametrize(*gen_vpp_scale_parameters(spec_r2r))
+  def test_r2r(self, case, scale_width, scale_height):
+    self.init(spec_r2r, case, scale_width, scale_height)
+    vars(self).setdefault("r2r", 5)
+    self.vpp()
+
+  def check_metrics(self):
+    check_filesize(
+        self.decoded, self.scale_width, self.scale_height,
+        self.frames, self.format)
+
+    fmtref = format_value(self.reference, **vars(self))
+
+    ssim = calculate_ssim(
+      fmtref, self.decoded,
+      self.scale_width, self.scale_height,
+      self.frames, self.format)
+
+    get_media()._set_test_details(ssim = ssim)
+
+    assert 1.0 >= ssim[0] >= 0.97
+    assert 1.0 >= ssim[1] >= 0.97
+    assert 1.0 >= ssim[2] >= 0.97
