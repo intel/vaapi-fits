504b528919120a20582934f9a3d3ae2d7f8531e3
U. Artie Eoff <ullysses.a.eoff@intel.com>
2022-06-29 08:48:07 -0400

    gst-va: add hevc10e tests
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

---
+++ test/gst-va/encode/10bit/hevc.py
@@ -0,0 +1,207 @@
+###
+### Copyright (C) 2022 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from .....lib import *
+from .....lib.gstreamer.va.util import *
+from .....lib.gstreamer.va.encoder import EncoderTest
+
+spec      = load_test_spec("hevc", "encode", "10bit")
+spec_r2r  = load_test_spec("hevc", "encode", "10bit", "r2r")
+
+@slash.requires(*have_gst_element("vah265dec"))
+class HEVC10EncoderBaseTest(EncoderTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      codec         = "hevc-10",
+      gstdecoder    = "vah265dec",
+      gstmediatype  = "video/x-h265",
+      gstparser     = "h265parse",
+    )
+
+  def get_file_ext(self):
+    return "h265"
+
+@slash.requires(*have_gst_element("vah265enc"))
+@slash.requires(*platform.have_caps("encode", "hevc_10"))
+class HEVC10EncoderTest(HEVC10EncoderBaseTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      caps      = platform.get_caps("encode", "hevc_10"),
+      gstencoder= "vah265enc",
+      lowpower  = False,
+    )
+
+@slash.requires(*have_gst_element("vah265lpenc"))
+@slash.requires(*platform.have_caps("vdenc", "hevc_10"))
+class HEVC10EncoderLPTest(HEVC10EncoderBaseTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      caps      = platform.get_caps("vdenc", "hevc_10"),
+      gstencoder= "vah265lpenc",
+      lowpower  = True,
+    )
+
+class cqp(HEVC10EncoderTest):
+  def init(self, tspec, case, gop, slices, bframes, qp, quality, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bframes   = bframes,
+      case      = case,
+      gop       = gop,
+      profile   = profile,
+      qp        = qp,
+      quality   = quality,
+      rcmode    = "cqp",
+      slices    = slices,
+    )
+
+  @slash.parametrize(*gen_hevc_cqp_parameters(spec, ['main10']))
+  def test(self, case, gop, slices, bframes, qp, quality, profile):
+    self.init(spec, case, gop, slices, bframes, qp, quality, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_hevc_cqp_parameters(spec_r2r, ['main10']))
+  def test_r2r(self, case, gop, slices, bframes, qp, quality, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, qp, quality, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class cqp_lp(HEVC10EncoderLPTest):
+  def init(self, tspec, case, gop, slices, qp, quality, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      case      = case,
+      gop       = gop,
+      profile   = profile,
+      qp        = qp,
+      quality   = quality,
+      rcmode    = "cqp",
+      slices    = slices,
+    )
+
+  @slash.parametrize(*gen_hevc_cqp_lp_parameters(spec, ['main10']))
+  def test(self, case, gop, slices, qp, quality, profile):
+    self.init(spec, case, gop, slices, qp, quality, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_hevc_cqp_lp_parameters(spec_r2r, ['main10']))
+  def test_r2r(self, case, gop, slices, qp, quality, profile):
+    self.init(spec_r2r, case, gop, slices, qp, quality, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class cbr(HEVC10EncoderTest):
+  def init(self, tspec, case, gop, slices, bframes, bitrate, fps, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bframes   = bframes,
+      bitrate   = bitrate,
+      case      = case,
+      fps       = fps,
+      gop       = gop,
+      maxrate   = bitrate,
+      minrate   = bitrate,
+      profile   = profile,
+      rcmode    = "cbr",
+      slices    = slices,
+    )
+
+  @slash.parametrize(*gen_hevc_cbr_parameters(spec, ['main10']))
+  def test(self, case, gop, slices, bframes, bitrate, fps, profile):
+    self.init(spec, case, gop, slices, bframes, bitrate, fps, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_hevc_cbr_parameters(spec_r2r, ['main10']))
+  def test_r2r(self, case, gop, slices, bframes, bitrate, fps, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, bitrate, fps, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class cbr_lp(HEVC10EncoderLPTest):
+  def init(self, tspec, case, gop, slices, bitrate, fps, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bitrate   = bitrate,
+      case      = case,
+      fps       = fps,
+      gop       = gop,
+      maxrate   = bitrate,
+      minrate   = bitrate,
+      profile   = profile,
+      rcmode    = "cbr",
+      slices    = slices,
+    )
+
+  @slash.parametrize(*gen_hevc_cbr_lp_parameters(spec, ['main10']))
+  def test(self, case, gop, slices, bitrate, fps, profile):
+    self.init(spec, case, gop, slices, bitrate, fps, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_hevc_cbr_lp_parameters(spec_r2r, ['main10']))
+  def test_r2r(self, case, gop, slices, bitrate, fps, profile):
+    self.init(spec_r2r, case, gop, slices, bitrate, fps, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class vbr(HEVC10EncoderTest):
+  def init(self, tspec, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bframes   = bframes,
+      bitrate   = bitrate,
+      case      = case,
+      fps       = fps,
+      gop       = gop,
+      maxrate   = int(bitrate / 0.7),
+      minrate   = bitrate,
+      profile   = profile,
+      quality   = quality,
+      rcmode    = "vbr",
+      refs      = refs,
+      slices    = slices,
+    )
+
+  @slash.parametrize(*gen_hevc_vbr_parameters(spec, ['main10']))
+  def test(self, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
+    self.init(spec, case, gop, slices, bframes, bitrate, fps, quality, refs, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_hevc_vbr_parameters(spec_r2r, ['main10']))
+  def test_r2r(self, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, bitrate, fps, quality, refs, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class vbr_lp(HEVC10EncoderLPTest):
+  def init(self, tspec, case, gop, slices, bitrate, fps, quality, refs, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bitrate   = bitrate,
+      case      = case,
+      fps       = fps,
+      gop       = gop,
+      maxrate   = int(bitrate / 0.7),
+      minrate   = bitrate,
+      profile   = profile,
+      quality   = quality,
+      rcmode    = "vbr",
+      refs      = refs,
+      slices    = slices,
+    )
+
+  @slash.parametrize(*gen_hevc_vbr_lp_parameters(spec, ['main10']))
+  def test(self, case, gop, slices, bitrate, fps, quality, refs, profile):
+    self.init(spec, case, gop, slices, bitrate, fps, quality, refs, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_hevc_vbr_lp_parameters(spec_r2r, ['main10']))
+  def test_r2r(self, case, gop, slices, bitrate, fps, quality, refs, profile):
+    self.init(spec_r2r, case, gop, slices, bitrate, fps, quality, refs, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
