fadf9fd312adcd63cad8772a10316a9301088efd
U. Artie Eoff <ullysses.a.eoff@intel.com>
2023-02-02 11:17:40 -0500

    gst/util: add fps and psnr parsing
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- lib/gstreamer/util.py
+++ lib/gstreamer/util.py
@@ -12,6 +12,10 @@ def parse_inline_md5(msglog):
   return parse_inline_md5.pattern.search(msglog).group("actual")
 parse_inline_md5.pattern = re.compile("md5 = (?P<actual>[0-9a-fA-F]{32})$", re.MULTILINE)
 
+def parse_psnr_stats(filename, frames):
+  from ...lib.ffmpeg.util import parse_psnr_stats as ffparse_psnr_stats
+  return ffparse_psnr_stats(filename, frames)
+
 @memoize
 def have_gst():
   return try_call(f"which {exe2os('gst-launch-1.0')}") and try_call(f"which {exe2os('gst-inspect-1.0')}")
@@ -25,3 +29,7 @@ def have_gst_element(element):
 def gst_discover(filename):
   return call(f"{exe2os('gst-discoverer-1.0')}"
               " {}".format(filename))
+
+def gst_discover_fps(filename):
+  return gst_discover_fps.pattern.findall(gst_discover(filename))[-1]
+gst_discover_fps.pattern = re.compile("Frame rate: (?P<fps>[0-9]+)", re.MULTILINE)
