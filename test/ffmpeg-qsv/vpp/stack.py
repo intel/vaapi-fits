7aa23ad30a7456b35b1c7323d8121057a557d15a
U. Artie Eoff <ullysses.a.eoff@intel.com>
2023-02-27 13:11:53 -0500

    ffmpeg/qsv/vpp: add stack filter tests
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

---
+++ test/ffmpeg-qsv/vpp/stack.py
@@ -0,0 +1,58 @@
+###
+### Copyright (C) 2023 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ....lib.ffmpeg.qsv.util import *
+from ....lib.ffmpeg.qsv.vpp import VppTest
+
+spec = load_test_spec("vpp", "stack")
+
+@slash.requires(*platform.have_caps("vpp", "blend"))
+class default(VppTest):
+  def before(self):
+    vars(self).update(
+      caps    = platform.get_caps("vpp", "blend"),
+      metric  = dict(type = "md5"),
+      vpp_op  = "stack",
+    )
+    super().before()
+
+  @slash.requires(*have_ffmpeg_filter("hstack_qsv"))
+  @slash.parametrize(*gen_vpp_hstack_parameters(spec))
+  def test_hstack(self, case, inputs):
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      case    = case,
+      inputs  = inputs,
+      stack   = "hstack",
+    )
+    self.vpp()
+
+  @slash.requires(*have_ffmpeg_filter("vstack_qsv"))
+  @slash.parametrize(*gen_vpp_vstack_parameters(spec))
+  def test_vstack(self, case, inputs):
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      case    = case,
+      inputs  = inputs,
+      stack   = "vstack",
+    )
+    self.vpp()
+
+  @slash.requires(*have_ffmpeg_filter("xstack_qsv"))
+  @slash.parametrize(*gen_vpp_xstack_parameters(spec))
+  def test_xstack(self, case, rows, cols, tilew, tileh):
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      case    = case,
+      rows    = rows,
+      cols    = cols,
+      inputs  = rows * cols,
+      tilew   = tilew,
+      tileh   = tileh,
+      stack   = "xstack",
+    )
+    self.vpp()
