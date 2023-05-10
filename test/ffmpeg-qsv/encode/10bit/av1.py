e39bc67a0f36da912d41fed4aa3a31fb962b1513
U. Artie Eoff <ullysses.a.eoff@intel.com>
2023-05-10 12:23:42 -0400

    test/ffmpeg-qsv: add av1e 10bit R2R tests
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/ffmpeg-qsv/encode/10bit/av1.py
+++ test/ffmpeg-qsv/encode/10bit/av1.py
@@ -8,7 +8,8 @@ from .....lib import *
 from .....lib.ffmpeg.qsv.util import *
 from .....lib.ffmpeg.qsv.encoder import EncoderTest
 
-spec = load_test_spec("av1", "encode", "10bit")
+spec      = load_test_spec("av1", "encode", "10bit")
+spec_r2r  = load_test_spec("av1", "encode", "10bit", "r2r")
 
 @slash.requires(*have_ffmpeg_encoder("av1_qsv"))
 @slash.requires(*have_ffmpeg_decoder("av1_qsv"))
@@ -53,6 +54,12 @@ class cqp_lp(AV1EncoderLPTest):
     self.init(spec, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
     self.encode()
 
+  @slash.parametrize(*gen_av1_cqp_lp_parameters(spec_r2r))
+  def test_r2r(self, case, gop, bframes, tilecols, tilerows, qp, quality, profile):
+    self.init(spec_r2r, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
 class cbr_lp(AV1EncoderLPTest):
   def init(self, tspec, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile):
     vars(self).update(tspec[case].copy())
@@ -74,6 +81,12 @@ class cbr_lp(AV1EncoderLPTest):
   @slash.parametrize(*gen_av1_cbr_lp_parameters(spec))
   def test(self, case, gop, bframes, tilecols, tilerows, bitrate, quality, fps, profile):
     self.init(spec, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_av1_cbr_lp_parameters(spec_r2r))
+  def test_r2r(self, case, gop, bframes, tilecols, tilerows, bitrate, quality, fps, profile):
+    self.init(spec_r2r, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile)
+    vars(self).setdefault("r2r", 5)
     self.encode()
 
 class vbr_lp(AV1EncoderLPTest):
@@ -98,4 +111,10 @@ class vbr_lp(AV1EncoderLPTest):
   @slash.parametrize(*gen_av1_vbr_lp_parameters(spec))
   def test(self, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile):
     self.init(spec, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_av1_vbr_lp_parameters(spec_r2r))
+  def test_r2r(self, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile):
+    self.init(spec_r2r, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile)
+    vars(self).setdefault("r2r", 5)
     self.encode()
