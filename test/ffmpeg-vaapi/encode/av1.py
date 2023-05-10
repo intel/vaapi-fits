5bb30bf3799523c3c8405ffb736cd05b01a511d6
U. Artie Eoff <ullysses.a.eoff@intel.com>
2023-05-10 12:46:36 -0400

    test/ffmpeg-vaapi: add av1e 8bit R2R tests
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/ffmpeg-vaapi/encode/av1.py
+++ test/ffmpeg-vaapi/encode/av1.py
@@ -8,7 +8,8 @@ from ....lib import *
 from ....lib.ffmpeg.vaapi.util import *
 from ....lib.ffmpeg.vaapi.encoder import EncoderTest
 
-spec = load_test_spec("av1", "encode", "8bit")
+spec      = load_test_spec("av1", "encode", "8bit")
+spec_r2r  = load_test_spec("av1", "encode", "8bit", "r2r")
 
 @slash.requires(*have_ffmpeg_encoder("av1_vaapi"))
 class AV1EncoderBaseTest(EncoderTest):
@@ -54,6 +55,12 @@ class cqp_lp(AV1EncoderLPTest):
     self.init(spec, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
     self.encode()
 
+  @slash.parametrize(*gen_av1_cqp_lp_parameters(spec_r2r))
+  def test_r2r(self, case, gop, bframes, tilecols, tilerows, qp, quality, profile):
+    self.init(spec_r2r, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
 class cbr_lp(AV1EncoderLPTest):
   def init(self, tspec, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile):
     vars(self).update(tspec[case].copy())
@@ -75,4 +82,10 @@ class cbr_lp(AV1EncoderLPTest):
   @slash.parametrize(*gen_av1_cbr_lp_parameters(spec))
   def test(self, case, gop, bframes, tilecols, tilerows, bitrate, quality, fps, profile):
     self.init(spec, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_av1_cbr_lp_parameters(spec_r2r))
+  def test_r2r(self, case, gop, bframes, tilecols, tilerows, bitrate, quality, fps, profile):
+    self.init(spec_r2r, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile)
+    vars(self).setdefault("r2r", 5)
     self.encode()
