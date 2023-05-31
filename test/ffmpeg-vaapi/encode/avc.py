d7f9970634ec6d3f0abc1868f2c90696ce6ad17e
Focus Luo <focus.luo@intel.com>
2023-05-30 20:48:43 -0700

    [ffmpeg-vaapi] add avc lp bframes implement
    Signed-off-by: Focus Luo <focus.luo@intel.com>

--- test/ffmpeg-vaapi/encode/avc.py
+++ test/ffmpeg-vaapi/encode/avc.py
@@ -74,12 +74,10 @@ class cqp(AVCEncoderTest):
     self.encode()
 
 class cqp_lp(AVCEncoderLPTest):
-  def init(self, tspec, case, gop, slices, qp, quality, profile):
+  def init(self, tspec, case, gop, slices, bframes, qp, quality, profile):
     vars(self).update(tspec[case].copy())
     vars(self).update(
-      # WA: media-driver 89c6167 support B frame
-      # default option will insert B frame for venc
-      bframes   = 0,
+      bframes   = bframes,
       case      = case,
       gop       = gop,
       profile   = profile,
@@ -90,13 +88,13 @@ class cqp_lp(AVCEncoderLPTest):
     )
 
   @slash.parametrize(*gen_avc_cqp_lp_parameters(spec, ['high', 'main']))
-  def test(self, case, gop, slices, qp, quality, profile):
-    self.init(spec, case, gop, slices, qp, quality, profile)
+  def test(self, case, gop, slices, bframes, qp, quality, profile):
+    self.init(spec, case, gop, slices, bframes, qp, quality, profile)
     self.encode()
 
   @slash.parametrize(*gen_avc_cqp_lp_parameters(spec_r2r, ['high', 'main']))
-  def test_r2r(self, case, gop, slices, qp, quality, profile):
-    self.init(spec_r2r, case, gop, slices, qp, quality, profile)
+  def test_r2r(self, case, gop, slices, bframes, qp, quality, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, qp, quality, profile)
     vars(self).setdefault("r2r", 5)
     self.encode()
 
@@ -128,12 +126,10 @@ class cbr(AVCEncoderTest):
     self.encode()
 
 class cbr_lp(AVCEncoderLPTest):
-  def init(self, tspec, case, gop, slices, bitrate, fps, profile):
+  def init(self, tspec, case, gop, slices, bframes, bitrate, fps, profile):
     vars(self).update(tspec[case].copy())
     vars(self).update(
-      # WA: media-driver 89c6167 support B frame
-      # default option will insert B frame for venc
-      bframes   = 0,
+      bframes   = bframes,
       bitrate   = bitrate,
       case      = case,
       fps       = fps,
@@ -146,13 +142,13 @@ class cbr_lp(AVCEncoderLPTest):
     )
 
   @slash.parametrize(*gen_avc_cbr_lp_parameters(spec, ['high', 'main']))
-  def test(self, case, gop, slices, bitrate, fps, profile):
-    self.init(spec, case, gop, slices, bitrate, fps, profile)
+  def test(self, case, gop, slices, bframes, bitrate, fps, profile):
+    self.init(spec, case, gop, slices, bframes, bitrate, fps, profile)
     self.encode()
 
   @slash.parametrize(*gen_avc_cbr_lp_parameters(spec_r2r, ['high', 'main']))
-  def test_r2r(self, case, gop, slices, bitrate, fps, profile):
-    self.init(spec_r2r, case, gop, slices, bitrate, fps, profile)
+  def test_r2r(self, case, gop, slices, bframes, bitrate, fps, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, bitrate, fps, profile)
     vars(self).setdefault("r2r", 5)
     self.encode()
 
@@ -186,12 +182,10 @@ class vbr(AVCEncoderTest):
     self.encode()
 
 class vbr_lp(AVCEncoderLPTest):
-  def init(self, tspec, case, gop, slices, bitrate, fps, quality, refs, profile):
+  def init(self, tspec, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
     vars(self).update(tspec[case].copy())
     vars(self).update(
-      # WA: media-driver 89c6167 support B frame
-      # default option will insert B frame for venc
-      bframes   = 0,
+      bframes   = bframes,
       bitrate   = bitrate,
       case      = case,
       fps       = fps,
@@ -206,13 +200,13 @@ class vbr_lp(AVCEncoderLPTest):
     )
 
   @slash.parametrize(*gen_avc_vbr_lp_parameters(spec, ['high', 'main']))
-  def test(self, case, gop, slices, bitrate, fps, quality, refs, profile):
-    self.init(spec, case, gop, slices, bitrate, fps, quality, refs, profile)
+  def test(self, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
+    self.init(spec, case, gop, slices, bframes, bitrate, fps, quality, refs, profile)
     self.encode()
 
   @slash.parametrize(*gen_avc_vbr_lp_parameters(spec_r2r, ['high', 'main']))
-  def test_r2r(self, case, gop, slices, bitrate, fps, quality, refs, profile):
-    self.init(spec_r2r, case, gop, slices, bitrate, fps, quality, refs, profile)
+  def test_r2r(self, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, bitrate, fps, quality, refs, profile)
     vars(self).setdefault("r2r", 5)
     self.encode()
 
