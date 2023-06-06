b285f4e7eb6fe5398562f38f2aa81f314817f35e
Tong Wu <tong1.wu@intel.com>
2023-06-06 10:11:48 +0800

    lib: add ffmpeg-d3d12 support
    Signed-off-by: Tong Wu <tong1.wu@intel.com>

---
+++ test/ffmpeg-d3d12/decode/10bit/hevc.py
@@ -0,0 +1,25 @@
+###
+### Copyright (C) 2023 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from .....lib import *
+from .....lib.ffmpeg.d3d12.util import *
+from .....lib.ffmpeg.d3d12.decoder import DecoderTest
+
+spec = load_test_spec("hevc", "decode", "10bit")
+
+@slash.requires(*platform.have_caps("decode", "hevc_10"))
+class default(DecoderTest):
+  def before(self):
+    # default metric
+    self.metric = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0)
+    self.caps   = platform.get_caps("decode", "hevc_10")
+    super(default, self).before()
+
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+    self.case = case
+    self.decode()
