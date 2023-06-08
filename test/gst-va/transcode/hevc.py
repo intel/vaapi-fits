531c413aa110d265baedd9fb944e25e9350e77ab
Focus Luo <focus.luo@intel.com>
2023-06-08 10:48:43 +0800

    [gst-va] add hevc transcode support

---
+++ test/gst-va/transcode/hevc.py
@@ -0,0 +1,21 @@
+###
+### Copyright (C) 2023 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ....lib.gstreamer.va.util import *
+from ....lib.gstreamer.va.transcoder import TranscoderTest
+
+spec = load_test_spec("hevc", "transcode")
+
+class default(TranscoderTest):
+  @slash.parametrize(("case"), sorted_by_resolution(spec))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      case  = case,
+      codec = "hevc",
+    )
+    self.transcode()
