835f4c2a259b65029873d6a1b59b631addeeb925
Wang Wenqing <wenqingx.wang@intel.com>
2023-06-08 01:56:43 -0700

    [Gst-va] add vpp crop support
    Signed-off-by: Wang Wenqing <wenqingx.wang@intel.com>

---
+++ test/gst-va/vpp/crop.py
@@ -0,0 +1,35 @@
+###
+### Copyright (C) 2023 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ....lib.gstreamer.va.util import *
+from ....lib.gstreamer.va.vpp import VppTest
+
+spec = load_test_spec("vpp", "crop")
+
+@slash.requires(*platform.have_caps("vpp", "crop"))
+class default(VppTest):
+  def before(self):
+    vars(self).update(
+      caps    = platform.get_caps("vpp", "crop"),
+      metric  = dict(type = "md5"),
+      vpp_op  = "crop",
+    )
+    super(default, self).before()
+
+  @slash.parametrize(*gen_vpp_crop_parameters(spec))
+  def test(self, case, left, right, top, bottom):
+    vars(self).update(spec[case].copy())
+    vars(self).update(
+      bottom      = bottom,
+      case        = case,
+      crop_height = self.height - top - bottom,
+      crop_width  = self.width - left - right,
+      left        = left,
+      right       = right,
+      top         = top,
+    )
+    self.vpp()
