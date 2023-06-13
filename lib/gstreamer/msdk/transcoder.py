2e53324a5418b7f4f08a3eb5170ec15757ffd5b2
Mengkejiergeli Ba <mengkejiergeli.ba@intel.com>
2023-06-13 10:17:23 +0800

    gst-msdk: Add surface sharing cases between d3d11 and vpl

--- lib/gstreamer/msdk/transcoder.py
+++ lib/gstreamer/msdk/transcoder.py
@@ -22,11 +22,13 @@ class TranscoderTest(BaseTranscoderTest):
         sw = (dict(maxres = (16384, 16384)), have_gst_element("openh264dec"), "h264parse ! openh264dec ! videoconvert"),
         hw = (platform.get_caps("decode", "avc"), have_gst_element("msdkh264dec"), "h264parse ! msdkh264dec"),
         va_hw = (platform.get_caps("decode", "avc"), have_gst_element("vah264dec"), "h264parse ! vah264dec"),
+        d3d11_hw = (platform.get_caps("decode", "avc"), have_gst_element("d3d11h264dec"), "h264parse ! d3d11h264dec"),
       ),
       "hevc-8" : dict(
         sw = (dict(maxres = (16384, 16384)), have_gst_element("libde265dec"), "h265parse ! libde265dec ! videoconvert"),
         hw = (platform.get_caps("decode", "hevc_8"), have_gst_element("msdkh265dec"), "h265parse ! msdkh265dec"),
         va_hw = (platform.get_caps("decode", "hevc_8"), have_gst_element("vah265dec"), "h265parse ! vah265dec"),
+        d3d11_hw = (platform.get_caps("decode", "hevc_8"), have_gst_element("d3d11h265dec"), "h265parse ! d3d11h265dec"),
       ),
       "mpeg2" : dict(
         sw = (dict(maxres = (2048, 2048)), have_gst_element("mpeg2dec"), "mpegvideoparse ! mpeg2dec ! videoconvert"),
