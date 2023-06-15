704151ce52f8991bee5845ea5326e9125c2c5f48
Focus Luo <focus.luo@intel.com>
2023-06-14 19:12:38 -0700

    [gst-va] add vp9 10bit test implement
    Signed-off-by: Focus Luo <focus.luo@intel.com>

---
+++ test/gst-va/encode/10bit/vp9.py
@@ -0,0 +1,100 @@
+###
+### Copyright (C) 2023 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from .....lib import *
+from .....lib.gstreamer.va.util import *
+from .....lib.gstreamer.va.encoder import EncoderTest
+
+spec = load_test_spec("vp9", "encode", "10bit")
+
+@slash.requires(*have_gst_element("vavp9dec"))
+class VP9_10EncoderBaseTest(EncoderTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      codec         = "vp9",
+      gstdecoder    = "vavp9dec",
+      gstmediatype  = "video/x-vp9",
+      gstparser     = "vp9parse",
+      gstmuxer      = "matroskamux",
+      gstdemuxer    = "matroskademux",
+    )
+
+  def get_file_ext(self):
+    return "webm"
+
+@slash.requires(*have_gst_element("vavp9lpenc"))
+@slash.requires(*platform.have_caps("vdenc", "vp9_10"))
+class VP9_10EncoderLPTest(VP9_10EncoderBaseTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      caps      = platform.get_caps("vdenc", "vp9_10"),
+      gstencoder    = "vavp9lpenc",
+      lowpower  = True,
+    )
+
+class cqp_lp(VP9_10EncoderLPTest):
+  def init(self, tspec, case, ipmode, qp, quality, looplvl, loopshp):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      case      = case,
+      gop       = 30 if ipmode != 0 else 1,
+      looplvl   = looplvl,
+      loopshp   = loopshp,
+      qp        = qp,
+      quality   = quality,
+      rcmode    = "cqp",
+    )
+
+  @parametrize_with_unused(*gen_vp9_cqp_lp_parameters(spec), ['slices','refmode'])
+  def test(self, case, ipmode, qp, quality, slices, refmode, looplvl, loopshp):
+    self.init(spec, case, ipmode, qp, quality, looplvl, loopshp)
+    self.encode()
+
+class cbr_lp(VP9_10EncoderLPTest):
+  def init(self, tspec, case, gop, bitrate, fps, looplvl, loopshp):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bitrate   = bitrate,
+      case      = case,
+      fps       = fps,
+      frames    = vars(self).get("brframes", self.frames),
+      gop       = gop,
+      looplvl   = looplvl,
+      loopshp   = loopshp,
+      maxrate   = bitrate,
+      minrate   = bitrate,
+      rcmode    = "cbr",
+    )
+
+  @parametrize_with_unused(*gen_vp9_cbr_lp_parameters(spec), ['slices','refmode'])
+  def test(self, case, gop, bitrate, fps, slices, refmode, looplvl, loopshp):
+    self.init(spec, case, gop, bitrate, fps, looplvl, loopshp)
+    self.encode()
+
+class vbr_lp(VP9_10EncoderLPTest):
+  def init(self, tspec, case, gop, bitrate, fps, quality, looplvl, loopshp):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bitrate   = bitrate,
+      case      = case,
+      fps       = fps,
+      frames    = vars(self).get("brframes", self.frames),
+      gop       = gop,
+      looplvl   = looplvl,
+      loopshp   = loopshp,
+      # target percentage 50%
+      maxrate   = bitrate * 2,
+      minrate   = bitrate,
+      quality   = quality,
+      rcmode    = "vbr",
+    )
+
+  @parametrize_with_unused(*gen_vp9_vbr_lp_parameters(spec), ['slices','refmode'])
+  def test(self, case, gop, bitrate, fps, quality, slices, refmode, looplvl, loopshp):
+    self.init(spec, case, gop, bitrate, fps, quality, looplvl, loopshp)
+    self.encode()
