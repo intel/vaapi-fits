2c3e012c41e1d379d48aa2afe9fcaa7a52603d3e
Focus Luo <focus.luo@intel.com>
2023-09-26 17:35:51 +0000

    [MTL+] gstreamer add vp9-8 no-lowpower support
    Signed-off-by: Focus Luo <focus.luo@intel.com>

--- test/gst-msdk/encode/vp9.py
+++ test/gst-msdk/encode/vp9.py
@@ -29,6 +29,14 @@ class VP9EncoderBaseTest(EncoderTest):
   def get_file_ext(self):
     return "webm"
 
+@slash.requires(*platform.have_caps("encode", "vp9_8"))
+class VP9EncoderTest(VP9EncoderBaseTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      caps  = platform.get_caps("encode", "vp9_8"),
+    )
+
 @slash.requires(*platform.have_caps("vdenc", "vp9_8"))
 class VP9EncoderLPTest(VP9EncoderBaseTest):
   def before(self):
@@ -89,6 +97,83 @@ class cbr_lp(VP9EncoderLPTest):
     self.encode()
 
 class vbr_lp(VP9EncoderLPTest):
+  def init(self, tspec, case, gop, bitrate, fps, quality, slices):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bitrate   = bitrate,
+      case      = case,
+      fps       = fps,
+      frames    = vars(self).get("brframes", self.frames),
+      gop       = gop,
+      # target percentage 50%
+      maxrate   = bitrate * 2,
+      minrate   = bitrate,
+      quality   = quality,
+      rcmode    = "vbr",
+      slices    = slices,
+    )
+
+  @parametrize_with_unused(*gen_vp9_vbr_lp_parameters(spec), ['refmode', 'looplvl', 'loopshp'])
+  def test(self, case, gop, bitrate, fps, quality, slices, refmode, looplvl, loopshp):
+    self.init(spec, case, gop, bitrate, fps, quality, slices)
+    self.encode()
+
+  @parametrize_with_unused(*gen_vp9_vbr_lp_parameters(spec_r2r), ['refmode', 'looplvl', 'loopshp'])
+  def test_r2r(self, case, gop, bitrate, fps, quality, slices, refmode, looplvl, loopshp):
+    self.init(spec_r2r, case, gop, bitrate, fps, quality, slices)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class cqp(VP9EncoderTest):
+  def init(self, tspec, case, ipmode, qp, quality, slices):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      case      = case,
+      gop       = 30 if ipmode != 0 else 1,
+      qp        = qp,
+      quality   = quality,
+      rcmode    = "cqp",
+      slices    = slices,
+    )
+
+  @parametrize_with_unused(*gen_vp9_cqp_lp_parameters(spec), ['refmode', 'looplvl', 'loopshp'])
+  def test(self, case, ipmode, qp, quality, slices, refmode, looplvl, loopshp):
+    self.init(spec, case, ipmode, qp, quality, slices)
+    self.encode()
+
+  @parametrize_with_unused(*gen_vp9_cqp_lp_parameters(spec_r2r), ['refmode', 'looplvl', 'loopshp'])
+  def test_r2r(self, case, ipmode, qp, quality, slices, refmode, looplvl, loopshp):
+    self.init(spec_r2r, case, ipmode, qp, quality, slices)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class cbr(VP9EncoderTest):
+  def init(self, tspec, case, gop, bitrate, fps, slices):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bitrate   = bitrate,
+      case      = case,
+      fps       = fps,
+      frames    = vars(self).get("brframes", self.frames),
+      gop       = gop,
+      maxrate   = bitrate,
+      minrate   = bitrate,
+      rcmode    = "cbr",
+      slices    = slices,
+    )
+
+  @parametrize_with_unused(*gen_vp9_cbr_lp_parameters(spec), ['refmode', 'looplvl', 'loopshp'])
+  def test(self, case, gop, bitrate, fps, slices, refmode, looplvl, loopshp):
+    self.init(spec, case, gop, bitrate, fps, slices)
+    self.encode()
+
+  @parametrize_with_unused(*gen_vp9_cbr_lp_parameters(spec_r2r), ['refmode', 'looplvl', 'loopshp'])
+  def test_r2r(self, case, gop, bitrate, fps, slices, refmode, looplvl, loopshp):
+    self.init(spec_r2r, case, gop, bitrate, fps, slices)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class vbr(VP9EncoderTest):
   def init(self, tspec, case, gop, bitrate, fps, quality, slices):
     vars(self).update(tspec[case].copy())
     vars(self).update(
