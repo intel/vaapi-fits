b8bf4cc9be5223c83da396161028c40628b33d52
Focus Luo <focus.luo@intel.com>
2023-09-28 08:59:49 +0000

    [MTL+] gstreamer test add av1-10 no-lowpower mode support
    Signed-off-by: Focus Luo <focus.luo@intel.com>

--- test/gst-msdk/encode/10bit/av1.py
+++ test/gst-msdk/encode/10bit/av1.py
@@ -29,6 +29,14 @@ class AV1EncoderBaseTest(EncoderTest):
   def get_file_ext(self):
     return "webm"
 
+@slash.requires(*platform.have_caps("encode", "av1_10"))
+class AV1EncoderTest(AV1EncoderBaseTest):
+  def before(self):
+    super().before()
+    vars(self).update(
+      caps      = platform.get_caps("encode", "av1_10"),
+    )
+
 @slash.requires(*platform.have_caps("vdenc", "av1_10"))
 class AV1EncoderLPTest(AV1EncoderBaseTest):
   def before(self):
@@ -38,6 +46,91 @@ class AV1EncoderLPTest(AV1EncoderBaseTest):
       # NOTE: msdkav1enc does not have lowpower property.
       # msdkav1enc lowpower is hardcoded internally
     )
+
+class cqp(AV1EncoderTest):
+  def init(self, tspec, case, gop, bframes, tilecols, tilerows,qp, quality, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      case      = case,
+      gop       = gop,
+      bframes   = bframes,
+      qp        = qp,
+      rcmode    = "cqp",
+      quality   = quality,
+      profile   = profile,
+      tilerows  = tilerows,
+      tilecols  = tilecols,
+    )
+
+  @slash.parametrize(*gen_av1_cqp_parameters(spec))
+  def test(self, case, gop, bframes, tilecols, tilerows, qp, quality, profile):
+    self.init(spec, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_av1_cqp_parameters(spec_r2r))
+  def test_r2r(self, case, gop, bframes, tilecols, tilerows, qp, quality, profile):
+    self.init(spec_r2r, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class cbr(AV1EncoderTest):
+  def init(self, tspec, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bitrate = bitrate,
+      case    = case,
+      fps     = fps,
+      gop     = gop,
+      maxrate = bitrate,
+      minrate = bitrate,
+      profile = profile,
+      rcmode  = "cbr",
+      tilerows  = tilerows,
+      tilecols  = tilecols,
+      quality   = quality,
+      bframes   = bframes,
+    )
+
+  @slash.parametrize(*gen_av1_cbr_parameters(spec))
+  def test(self, case, gop, bframes, tilecols, tilerows, bitrate, quality, fps, profile):
+    self.init(spec, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_av1_cbr_parameters(spec_r2r))
+  def test_r2r(self, case, gop, bframes, tilecols, tilerows, bitrate, quality, fps, profile):
+    self.init(spec_r2r, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class vbr(AV1EncoderTest):
+  def init(self, tspec, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      bitrate = bitrate,
+      case    = case,
+      fps     = fps,
+      gop     = gop,
+      # target percentage 50%
+      maxrate = bitrate * 2,
+      minrate = bitrate,
+      profile = profile,
+      rcmode  = "vbr",
+      tilerows  = tilerows,
+      tilecols  = tilecols,
+      quality   = quality,
+      bframes   = bframes,
+    )
+
+  @slash.parametrize(*gen_av1_vbr_parameters(spec))
+  def test(self, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile):
+    self.init(spec, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_av1_vbr_parameters(spec_r2r))
+  def test_r2r(self, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile):
+    self.init(spec_r2r, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
 
 class cqp_lp(AV1EncoderLPTest):
   def init(self, tspec, case, gop, bframes, tilecols, tilerows,qp, quality, profile):
