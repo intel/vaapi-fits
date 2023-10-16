354ded25aa410a91b4606e4acd212ac2285eb7c2
U. Artie Eoff <ullysses.a.eoff@intel.com>
2023-10-16 10:25:45 -0400

    lib/formats: remove obsolete subsampling map
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- lib/formats.py
+++ lib/formats.py
@@ -63,26 +63,6 @@ class PixelFormat(str, enum.Enum):
       and self.bitdepth == pf.bitdepth
     )
 
-subsampling = {
-  "Y800" : ("YUV400",  8),
-  "I420" : ("YUV420",  8),
-  "NV12" : ("YUV420",  8),
-  "YV12" : ("YUV420",  8),
-  "P010" : ("YUV420", 10),
-  "P012" : ("YUV420", 12),
-  "I010" : ("YUV420", 10),
-  "422H" : ("YUV422",  8),
-  "422V" : ("YUV422",  8),
-  "YUY2" : ("YUV422",  8),
-  "Y210" : ("YUV422", 10),
-  "Y212" : ("YUV422", 12),
-  "444P" : ("YUV444",  8),
-  "AYUV" : ("YUV444",  8),
-  "VUYA" : ("YUV444",  8),
-  "Y410" : ("YUV444", 10),
-  "Y412" : ("YUV444", 12),
-}
-
 def match_best_format(fmt, choices):
   if fmt in choices:
     return fmt
