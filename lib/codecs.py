854d04556e2aa326627ef3acecf75a171c7c2636
U. Artie Eoff <ullysses.a.eoff@intel.com>
2023-10-17 10:30:53 -0400

    lib: enum Codecs
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

---
+++ lib/codecs.py
@@ -0,0 +1,24 @@
+###
+### Copyright (C) 2023 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+import enum
+
+@enum.unique
+class Codec(str, enum.Enum):
+  NONE  = "none"
+  RAW   = "raw"
+  AVC   = "avc"
+  HEVC  = "hevc"
+  AV1   = "av1"
+  VP9   = "vp9"
+  VP8   = "vp8"
+  JPEG  = "jpeg"
+  MJPEG = "mjpeg"
+  MPEG2 = "mpeg2"
+  VC1   = "vc1"
+
+  def __str__(self):
+    return self.value
