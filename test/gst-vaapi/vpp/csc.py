31026cd515b63415d1665fe24d060a8261942f95
Wang Hangjie <hangjiex.wang@intel.com>
2023-11-15 16:59:21 +0800

    test/gstreamer: add vpp/csc r2r test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

--- test/gst-vaapi/vpp/csc.py
+++ test/gst-vaapi/vpp/csc.py
@@ -9,6 +9,7 @@ from ....lib.gstreamer.vaapi.util import *
 from ....lib.gstreamer.vaapi.vpp import VppTest
 
 spec = load_test_spec("vpp", "csc")
+spec_r2r = load_test_spec("vpp", "csc", "r2r")
 
 @slash.requires(*platform.have_caps("vpp", "csc"))
 class default(VppTest):
@@ -23,4 +24,11 @@ class default(VppTest):
   def test(self, case, csc):
     vars(self).update(spec[case].copy())
     vars(self).update(case = case, csc = csc)
+    self.vpp()
+
+  @slash.parametrize(*gen_vpp_csc_parameters(spec_r2r))
+  def test_r2r(self, case, csc):
+    vars(self).update(spec[case].copy())
+    vars(self).update(case = case, csc = csc)
+    vars(self).setdefault("r2r", 5)
     self.vpp()
