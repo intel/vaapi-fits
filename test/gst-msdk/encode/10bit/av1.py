000da892243848d655c66428aad40ee9860531dc
Focus Luo <focus.luo@intel.com>
2023-11-19 17:33:03 -0800

    [av1 icq-10] gst-msdk added test implement
    Signed-off-by: Focus Luo <focus.luo@intel.com>

--- test/gst-msdk/encode/10bit/av1.py
+++ test/gst-msdk/encode/10bit/av1.py
@@ -37,6 +37,32 @@ class cqp(AV1_10EncoderTest):
     vars(self).setdefault("r2r", 5)
     self.encode()
 
+class icq(AV1_10EncoderTest):
+  def init(self, tspec, case, gop, bframes, tilecols, tilerows,qp, quality, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      case      = case,
+      gop       = gop,
+      bframes   = bframes,
+      qp        = qp,
+      rcmode    = "icq",
+      quality   = quality,
+      profile   = profile,
+      tilerows  = tilerows,
+      tilecols  = tilecols,
+    )
+
+  @slash.parametrize(*gen_av1_icq_parameters(spec))
+  def test(self, case, gop, bframes, tilecols, tilerows, qp, quality, profile):
+    self.init(spec, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_av1_icq_parameters(spec_r2r))
+  def test_r2r(self, case, gop, bframes, tilecols, tilerows, qp, quality, profile):
+    self.init(spec_r2r, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
 class cbr(AV1_10EncoderTest):
   def init(self, tspec, case, gop, bframes, tilecols, tilerows, bitrate, fps, quality, profile):
     vars(self).update(tspec[case].copy())
@@ -117,6 +143,32 @@ class cqp_lp(AV1_10EncoderLPTest):
     self.encode()
 
   @slash.parametrize(*gen_av1_cqp_lp_parameters(spec_r2r))
+  def test_r2r(self, case, gop, bframes, tilecols, tilerows, qp, quality, profile):
+    self.init(spec_r2r, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
+class icq_lp(AV1_10EncoderLPTest):
+  def init(self, tspec, case, gop, bframes, tilecols, tilerows,qp, quality, profile):
+    vars(self).update(tspec[case].copy())
+    vars(self).update(
+      case      = case,
+      gop       = gop,
+      bframes   = bframes,
+      qp        = qp,
+      rcmode    = "icq",
+      quality   = quality,
+      profile   = profile,
+      tilerows  = tilerows,
+      tilecols  = tilecols,
+    )
+
+  @slash.parametrize(*gen_av1_icq_lp_parameters(spec))
+  def test(self, case, gop, bframes, tilecols, tilerows, qp, quality, profile):
+    self.init(spec, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_av1_icq_lp_parameters(spec_r2r))
   def test_r2r(self, case, gop, bframes, tilecols, tilerows, qp, quality, profile):
     self.init(spec_r2r, case, gop, bframes, tilecols, tilerows, qp, quality, profile)
     vars(self).setdefault("r2r", 5)
