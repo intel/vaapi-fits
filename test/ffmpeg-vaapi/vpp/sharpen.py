0bf7bb71204567da47846a1bdf91cdd92c041831
Wang Hangjie <hangjiex.wang@intel.com>
2023-11-22 09:16:41 +0000

    test/ffmpeg-vaapi: add vpp/sharpen r2r test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

--- test/ffmpeg-vaapi/vpp/sharpen.py
+++ test/ffmpeg-vaapi/vpp/sharpen.py
@@ -8,7 +8,8 @@ from ....lib import *
 from ....lib.ffmpeg.vaapi.util import *
 from ....lib.ffmpeg.vaapi.vpp import VppTest
 
-spec = load_test_spec("vpp", "sharpen")
+spec      = load_test_spec("vpp", "sharpen")
+spec_r2r  = load_test_spec("vpp", "sharpen", "r2r")
 
 @slash.requires(*platform.have_caps("vpp", "sharpen"))
 @slash.requires(*have_ffmpeg_filter("sharpness_vaapi"))
@@ -31,4 +32,11 @@ class default(VppTest):
           "1280x720 resolution is known to cause GPU HANG with i965 driver")
         return
 
+    self.vpp()
+
+  @slash.parametrize(*gen_vpp_sharpen_parameters(spec_r2r))
+  def test_r2r(self, case, level):
+    vars(self).update(spec_r2r[case].copy())
+    vars(self).update(case = case, level = level)
+    vars(self).setdefault("r2r", 5)
     self.vpp()
