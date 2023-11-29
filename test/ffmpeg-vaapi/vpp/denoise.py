bfccc53ec5b02ae10ec35eadaf898c27df2486d9
Wang Hangjie <hangjiex.wang@intel.com>
2023-11-29 03:21:11 +0000

    test/ffmpeg-vaapi: add vpp/denoise r2r test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

--- test/ffmpeg-vaapi/vpp/denoise.py
+++ test/ffmpeg-vaapi/vpp/denoise.py
@@ -8,7 +8,8 @@ from ....lib import *
 from ....lib.ffmpeg.vaapi.util import *
 from ....lib.ffmpeg.vaapi.vpp import VppTest
 
-spec = load_test_spec("vpp", "denoise")
+spec      = load_test_spec("vpp", "denoise")
+spec_r2r  = load_test_spec("vpp", "denoise", "r2r")
 
 @slash.requires(*platform.have_caps("vpp", "denoise"))
 @slash.requires(*have_ffmpeg_filter("denoise_vaapi"))
@@ -24,4 +25,11 @@ class default(VppTest):
   def test(self, case, level):
     vars(self).update(spec[case].copy())
     vars(self).update(case = case, level = level)
+    self.vpp()
+
+  @slash.parametrize(*gen_vpp_denoise_parameters(spec_r2r))
+  def test_r2r(self, case, level):
+    vars(self).update(spec_r2r[case].copy())
+    vars(self).update(case = case, level = level)
+    vars(self).setdefault("r2r", 5)
     self.vpp()
