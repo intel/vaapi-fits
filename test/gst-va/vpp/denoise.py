00b34a7eb6a44e8e94938be3a1c0dda8af1a44e5
Wang Hangjie <hangjiex.wang@intel.com>
2023-11-29 03:26:00 +0000

    test/gst-va: add vpp/denoise r2r test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

--- test/gst-va/vpp/denoise.py
+++ test/gst-va/vpp/denoise.py
@@ -8,7 +8,8 @@ from ....lib import *
 from ....lib.gstreamer.va.util import *
 from ....lib.gstreamer.va.vpp import VppTest
 
-spec = load_test_spec("vpp", "denoise")
+spec      = load_test_spec("vpp", "denoise")
+spec_r2r  = load_test_spec("vpp", "denoise", "r2r")
 
 @slash.requires(*platform.have_caps("vpp", "denoise"))
 class default(VppTest):
@@ -26,4 +27,11 @@ class default(VppTest):
       case    = case,
       level   = level,
     )
+    self.vpp()
+
+  @slash.parametrize(*gen_vpp_denoise_parameters(spec_r2r))
+  def test_r2r(self, case, level):
+    vars(self).update(spec_r2r[case].copy())
+    vars(self).update(case = case, level = level)
+    vars(self).setdefault("r2r", 5)
     self.vpp()
