0b0c2ed401b6b7a6840b656e9e019c22b09f6c0f
Wang Hangjie <hangjiex.wang@intel.com>
2023-12-01 05:35:30 +0000

    test/ffmpeg-vaapi: add vpp/blend r2r test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

--- test/ffmpeg-vaapi/vpp/composite.py
+++ test/ffmpeg-vaapi/vpp/composite.py
@@ -8,7 +8,8 @@ from ....lib import *
 from ....lib.ffmpeg.vaapi.util import *
 from ....lib.ffmpeg.vaapi.vpp import VppTest
 
-spec = load_test_spec("vpp", "composite")
+spec      = load_test_spec("vpp", "composite")
+spec_r2r  = load_test_spec("vpp", "composite", "r2r")
 
 @slash.requires(*platform.have_caps("vpp", "blend"))
 @slash.requires(*have_ffmpeg_filter("overlay_vaapi"))
@@ -25,4 +26,11 @@ class default(VppTest):
   def test(self, case):
     vars(self).update(spec[case].copy())
     vars(self).update(case = case)
+    self.vpp()
+
+  @slash.parametrize(("case"), sorted(spec_r2r.keys()))
+  def test_r2r(self, case):
+    vars(self).update(spec_r2r[case].copy())
+    vars(self).update(case = case)
+    vars(self).setdefault("r2r", 5)
     self.vpp()
