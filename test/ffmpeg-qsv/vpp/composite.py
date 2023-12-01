ebb044ca1736047868e3a683d3e25763d9ea16b0
Wang Hangjie <hangjiex.wang@intel.com>
2023-12-01 05:26:55 +0000

    test/ffmpeg-qsv: add vpp/blend r2r test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

--- test/ffmpeg-qsv/vpp/composite.py
+++ test/ffmpeg-qsv/vpp/composite.py
@@ -9,6 +9,7 @@ from ....lib.ffmpeg.qsv.util import *
 from ....lib.ffmpeg.qsv.vpp import VppTest
 
 spec      = load_test_spec("vpp", "composite")
+spec_r2r  = load_test_spec("vpp", "composite", "r2r")
 
 @slash.requires(*platform.have_caps("vpp", "blend"))
 @slash.requires(*have_ffmpeg_filter("overlay_qsv"))
@@ -25,4 +26,11 @@ class default(VppTest):
   def test(self, case):
     vars(self).update(spec[case].copy())
     vars(self).update(case = case)
+    self.vpp()
+
+  @slash.parametrize(("case"), sorted(spec_r2r.keys()))
+  def test_r2r(self, case):
+    vars(self).update(spec_r2r[case].copy())
+    vars(self).update(case = case)
+    vars(self).setdefault("r2r", 5)
     self.vpp()
