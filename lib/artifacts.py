b01b548b74519d89856eed39fb1a1a9426380201
U. Artie Eoff <ullysses.a.eoff@intel.com>
2023-12-05 11:56:41 -0500

    artifacts: add media assets session decode cache
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- lib/artifacts.py
+++ lib/artifacts.py
@@ -49,3 +49,28 @@ class Artifacts():
     slash.add_critical_cleanup(self.purge, scope = scope, args = (absfile, scope))
     return absfile
 
+class MediaAssets:
+  # cache files that have already been decoded during the test session
+  _decoded = dict()
+
+  @classmethod
+  def raw(cls, test, **kwargs):
+    from .codecs import Codec
+    if vars(test).get("scodec", Codec.RAW) is Codec.RAW:
+      decoded = test.source
+    else:
+      decoded = cls._decoded.get(test.source, dict()).get(
+        (test.format, test.frames), None)
+
+    if decoded is None:
+      decoder = test.DecoderClass(
+        scope = Scope.SESSION,
+        frames = test.frames,
+        format = test.format,
+        source = test.source,
+        **kwargs,
+      )
+      decoder.decode()
+      decoded = decoder.decoded
+      cls._decoded.setdefault(test.source, dict())[(test.format, test.frames)] = decoded
+    return decoded
