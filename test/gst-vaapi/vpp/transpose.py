722f4049485a76ec6a28ae37d768b3749abeda61
Wang Hangjie <hangjiex.wang@intel.com>
2023-12-07 03:10:20 +0000

    test/gst-vaapi: add vpp/transpose r2r test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

--- test/gst-vaapi/vpp/transpose.py
+++ test/gst-vaapi/vpp/transpose.py
@@ -8,7 +8,8 @@ from ....lib import *
 from ....lib.gstreamer.vaapi.util import *
 from ....lib.gstreamer.vaapi.vpp import VppTest
 
-spec = load_test_spec("vpp", "transpose")
+spec      = load_test_spec("vpp", "transpose")
+spec_r2r  = load_test_spec("vpp", "transpose", "r2r")
 
 @slash.requires(*platform.have_caps("vpp", "transpose"))
 class default(VppTest):
@@ -34,4 +35,21 @@ class default(VppTest):
       slash.skip_test(
         "{degrees} {method} direction not supported".format(**vars(self)))
 
+    self.vpp()
+
+  @slash.parametrize(*gen_vpp_transpose_parameters(spec_r2r))
+  def test_r2r(self, case, degrees, method):
+    vars(self).update(spec_r2r[case].copy())
+    vars(self).update(
+      case      = case,
+      degrees   = degrees,
+      direction = map_transpose_direction(degrees, method),
+      method    = method,
+    )
+
+    if self.direction is None:
+      slash.skip_test(
+        "{degrees} {method} direction not supported".format(**vars(self)))
+
+    vars(self).setdefault("r2r", 5)
     self.vpp()
