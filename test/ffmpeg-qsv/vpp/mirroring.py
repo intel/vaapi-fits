11b178ff59ac900bd8182685d958475d67076795
Wang Hangjie <hangjiex.wang@intel.com>
2023-12-07 08:31:43 +0000

    test/ffmpeg-qsv: add vpp/mirroring r2r test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

--- test/ffmpeg-qsv/vpp/mirroring.py
+++ test/ffmpeg-qsv/vpp/mirroring.py
@@ -8,7 +8,8 @@ from ....lib import *
 from ....lib.ffmpeg.qsv.util import *
 from ....lib.ffmpeg.qsv.vpp import VppTest
 
-spec = load_test_spec("vpp", "mirroring")
+spec      = load_test_spec("vpp", "mirroring")
+spec_r2r  = load_test_spec("vpp", "mirroring", "r2r")
 
 @slash.requires(*platform.have_caps("vpp", "mirroring"))
 class default(VppTest):
@@ -33,4 +34,20 @@ class default(VppTest):
     if self.direction is None:
       slash.skip_test(f"{method} mirroring unsupported")
 
+    self.vpp()
+
+  @slash.parametrize(*gen_vpp_mirroring_parameters(spec_r2r))
+  def test_r2r(self, case, method):
+    vars(self).update(spec_r2r[case].copy())
+    vars(self).update(
+      case      = case,
+      degrees   = 0,
+      direction = map_transpose_direction(0, method),
+      method    = method,
+    )
+
+    if self.direction is None:
+      slash.skip_test(f"{method} mirroring unsupported")
+
+    vars(self).setdefault("r2r", 5)
     self.vpp()
