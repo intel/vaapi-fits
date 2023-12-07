2ac17e91377805c90643e9091a4b53e011a25dea
Wang Hangjie <hangjiex.wang@intel.com>
2023-12-07 08:49:36 +0000

    test/gst-va: add vpp/mirroring r2r test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

--- test/gst-va/vpp/mirroring.py
+++ test/gst-va/vpp/mirroring.py
@@ -8,7 +8,8 @@ from ....lib import *
 from ....lib.gstreamer.va.util import *
 from ....lib.gstreamer.va.vpp import VppTransposeTest as VppTest
 
-spec = load_test_spec("vpp", "mirroring")
+spec      = load_test_spec("vpp", "mirroring")
+spec_r2r  = load_test_spec("vpp", "mirroring","r2r")
 
 class default(VppTest):
   def before(self):
@@ -29,4 +30,21 @@ class default(VppTest):
       slash.skip_test(
         "{method} mirroring unsupported".format(**vars(self)))
 
+    self.vpp()
+
+  @slash.parametrize(*gen_vpp_mirroring_parameters(spec_r2r))
+  def test_r2r(self, case, method):
+    vars(self).update(spec_r2r[case].copy())
+    vars(self).update(
+      case      = case,
+      degrees   = 0,
+      direction = map_transpose_direction(0, method),
+      method    = method,
+    )
+
+    if self.direction is None:
+      slash.skip_test(
+        "{method} mirroring unsupported".format(**vars(self)))
+
+    vars(self).setdefault("r2r", 5)
     self.vpp()
