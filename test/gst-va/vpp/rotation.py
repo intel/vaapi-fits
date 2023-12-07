bfad74228baca9c37a68a440384cafe4a737bd2e
Wang Hangjie <hangjiex.wang@intel.com>
2023-12-07 07:56:01 +0000

    test/gst-va: add vpp/rotation r2r test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

--- test/gst-va/vpp/rotation.py
+++ test/gst-va/vpp/rotation.py
@@ -8,7 +8,8 @@ from ....lib import *
 from ....lib.gstreamer.va.util import *
 from ....lib.gstreamer.va.vpp import VppTransposeTest as VppTest
 
-spec = load_test_spec("vpp", "rotation")
+spec      = load_test_spec("vpp", "rotation")
+spec_r2r  = load_test_spec("vpp", "rotation", "r2r")
 
 class default(VppTest):
   def before(self):
@@ -29,4 +30,21 @@ class default(VppTest):
       slash.skip_test(
         "{degrees} rotation unsupported".format(**vars(self)))
 
+    self.vpp()
+
+  @slash.parametrize(*gen_vpp_rotation_parameters(spec_r2r))
+  def test_r2r(self, case, degrees):
+    vars(self).update(spec_r2r[case].copy())
+    vars(self).update(
+      case      = case,
+      degrees   = degrees,
+      direction = map_transpose_direction(degrees, None),
+      method    = None,
+    )
+
+    if self.direction is None:
+      slash.skip_test(
+        "{degrees} rotation unsupported".format(**vars(self)))
+
+    vars(self).setdefault("r2r", 5)
     self.vpp()
