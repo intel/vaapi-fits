4f515dbb79944f082500032d975374a130e416e3
Wang Hangjie <hangjiex.wang@intel.com>
2024-05-14 15:59:03 +0800

    test/ffmpeg-vaapi: add encode 12bit hevc r2r test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

--- test/ffmpeg-vaapi/encode/12bit/hevc.py
+++ test/ffmpeg-vaapi/encode/12bit/hevc.py
@@ -8,7 +8,8 @@ from .....lib import *
 from .....lib.ffmpeg.vaapi.util import *
 from .....lib.ffmpeg.vaapi.encoder import HEVC12EncoderTest
 
-spec = load_test_spec("hevc", "encode", "12bit")
+spec     = load_test_spec("hevc", "encode", "12bit")
+spec_r2r = load_test_spec("hevc", "encode", "12bit", "r2r")
 
 class cqp(HEVC12EncoderTest):
   def init(self, tspec, case, gop, slices, bframes, qp, quality, profile):
@@ -27,6 +28,12 @@ class cqp(HEVC12EncoderTest):
   @slash.parametrize(*gen_hevc_cqp_parameters(spec, ['main12']))
   def test(self, case, gop, slices, bframes, qp, quality, profile):
     self.init(spec, case, gop, slices, bframes, qp, quality, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_hevc_cqp_parameters(spec_r2r, ['main12']))
+  def test_r2r(self, case, gop, slices, bframes, qp, quality, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, qp, quality, profile)
+    vars(self).setdefault("r2r", 5)
     self.encode()
 
 class cbr(HEVC12EncoderTest):
@@ -50,6 +57,12 @@ class cbr(HEVC12EncoderTest):
     self.init(spec, case, gop, slices, bframes, bitrate, fps, profile)
     self.encode()
 
+  @slash.parametrize(*gen_hevc_cbr_parameters(spec_r2r, ['main12']))
+  def test_r2r(self, case, gop, slices, bframes, bitrate, fps, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, bitrate, fps, profile)
+    vars(self).setdefault("r2r", 5)
+    self.encode()
+
 class vbr(HEVC12EncoderTest):
   def init(self, tspec, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
     vars(self).update(tspec[case].copy())
@@ -71,4 +84,10 @@ class vbr(HEVC12EncoderTest):
   @slash.parametrize(*gen_hevc_vbr_parameters(spec, ['main12']))
   def test(self, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
     self.init(spec, case, gop, slices, bframes, bitrate, fps, quality, refs, profile)
+    self.encode()
+
+  @slash.parametrize(*gen_hevc_vbr_parameters(spec_r2r, ['main12']))
+  def test_r2r(self, case, gop, slices, bframes, bitrate, fps, quality, refs, profile):
+    self.init(spec_r2r, case, gop, slices, bframes, bitrate, fps, quality, refs, profile)
+    vars(self).setdefault("r2r", 5)
     self.encode()
