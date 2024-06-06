0be3b11ca91d603c88cdeaa1fdd0fc03b3fabe69
Wang Hangjie <hangjiex.wang@intel.com>
2024-06-05 23:33:11 -0700

    ffmpeg-qsv:Add 8/10bit VVC decode test
    Signed-off-by: Wang Hangjie <hangjiex.wang@intel.com>

---
+++ test/ffmpeg-qsv/decode/vvc.py
@@ -0,0 +1,29 @@
+###
+### Copyright (C) 2024 Intel Corporation
+###
+### SPDX-License-Identifier: BSD-3-Clause
+###
+
+from ....lib import *
+from ....lib.ffmpeg.qsv.util import *
+from ....lib.ffmpeg.qsv.decoder import DecoderTest
+
+spec = load_test_spec("vvc", "decode", "8bit")
+
+@slash.requires(*platform.have_caps("decode", "vvc_8"))
+@slash.requires(*have_ffmpeg_decoder("vvc_qsv"))
+class default(DecoderTest):
+  def before(self):
+    # default metric
+    vars(self).update(
+      caps        = platform.get_caps("decode", "vvc_8"),
+      ffdecoder   = "vvc_qsv",
+      metric      = dict(type = "ssim", miny = 1.0, minu = 1.0, minv = 1.0),
+    )
+    super().before()
+
+  @slash.parametrize(("case"), sorted(spec.keys()))
+  def test(self, case):
+    vars(self).update(spec[case].copy())
+    self.case = case
+    self.decode()
