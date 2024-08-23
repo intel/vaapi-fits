4faf45a1812db35638ce37ba4463981c5da17504
Focus Luo <focus.luo@intel.com>
2024-08-23 09:47:50 -0700

    [strapi] ffmpeg-qsv add dedicated strapi enc class
    Signed-off-by: Focus Luo <focus.luo@intel.com>

--- lib/mfx/api.py
+++ lib/mfx/api.py
@@ -44,76 +44,6 @@ class CodecProfile(IntEnum):
     MFX_PROFILE_VP9_2                       = 3
     MFX_PROFILE_VP9_3                       = 4
 
-class CodecLevel(IntEnum):
-    MFX_LEVEL_UNKNOWN                       =0 #/*!< Unspecified level. */
-    #/* H.264 level 1-1.3 */
-    MFX_LEVEL_AVC_1                         =10
-    MFX_LEVEL_AVC_1b                        =9
-    MFX_LEVEL_AVC_11                        =11
-    MFX_LEVEL_AVC_12                        =12
-    MFX_LEVEL_AVC_13                        =13
-    #/* H.264 level 2-2.2 */
-    MFX_LEVEL_AVC_2                         =20
-    MFX_LEVEL_AVC_21                        =21
-    MFX_LEVEL_AVC_22                        =22
-    #/* H.264 level 3-3.2 */
-    MFX_LEVEL_AVC_3                         =30
-    MFX_LEVEL_AVC_31                        =31
-    MFX_LEVEL_AVC_32                        =32
-    #/* H.264 level 4-4.2 */
-    MFX_LEVEL_AVC_4                         =40
-    MFX_LEVEL_AVC_41                        =41
-    MFX_LEVEL_AVC_42                        =42
-    #/* H.264 level 5-5.2 */
-    MFX_LEVEL_AVC_5                         =50
-    MFX_LEVEL_AVC_51                        =51
-    MFX_LEVEL_AVC_52                        =52
-    #/* H.264 level 6-6.2 */
-    MFX_LEVEL_AVC_6                         =60
-    MFX_LEVEL_AVC_61                        =61
-    MFX_LEVEL_AVC_62                        =62
-
-    #/* HEVC levels */
-    MFX_LEVEL_HEVC_1   = 10
-    MFX_LEVEL_HEVC_2   = 20
-    MFX_LEVEL_HEVC_21  = 21
-    MFX_LEVEL_HEVC_3   = 30
-    MFX_LEVEL_HEVC_31  = 31
-    MFX_LEVEL_HEVC_4   = 40
-    MFX_LEVEL_HEVC_41  = 41
-    MFX_LEVEL_HEVC_5   = 50
-    MFX_LEVEL_HEVC_51  = 51
-    MFX_LEVEL_HEVC_52  = 52
-    MFX_LEVEL_HEVC_6   = 60
-    MFX_LEVEL_HEVC_61  = 61
-    MFX_LEVEL_HEVC_62  = 62
-
-    #/* AV1 Levels */
-    MFX_LEVEL_AV1_2                         = 20
-    MFX_LEVEL_AV1_21                        = 21
-    MFX_LEVEL_AV1_22                        = 22
-    MFX_LEVEL_AV1_23                        = 23
-    MFX_LEVEL_AV1_3                         = 30
-    MFX_LEVEL_AV1_31                        = 31
-    MFX_LEVEL_AV1_32                        = 32
-    MFX_LEVEL_AV1_33                        = 33
-    MFX_LEVEL_AV1_4                         = 40
-    MFX_LEVEL_AV1_41                        = 41
-    MFX_LEVEL_AV1_42                        = 42
-    MFX_LEVEL_AV1_43                        = 43
-    MFX_LEVEL_AV1_5                         = 50
-    MFX_LEVEL_AV1_51                        = 51
-    MFX_LEVEL_AV1_52                        = 52
-    MFX_LEVEL_AV1_53                        = 53
-    MFX_LEVEL_AV1_6                         = 60
-    MFX_LEVEL_AV1_61                        = 61
-    MFX_LEVEL_AV1_62                        = 62
-    MFX_LEVEL_AV1_63                        = 63
-    MFX_LEVEL_AV1_7                         = 70
-    MFX_LEVEL_AV1_71                        = 71
-    MFX_LEVEL_AV1_72                        = 72
-    MFX_LEVEL_AV1_73                        = 73
-
 class RateControlMethod(IntEnum):
 #/*! The RateControlMethod enumerator itemizes bitrate control methods. */
     MFX_RATECONTROL_CBR       =1 #/*!< Use the constant bitrate control algorithm. */
@@ -168,6 +98,7 @@ class RateControlMethod(IntEnum):
     MFX_RATECONTROL_QVBR      =14
 
 class StringAPI(PropertyHandler):
+  level   = property(lambda s: s.ifprop("level", "CodecLevel={level}"))
   gop     = property(lambda s: s.ifprop("gop", "GopPicSize={gop}"))
   bframes = property(lambda s: s.ifprop("bframes", "GopRefDist={bframes}"))
   slices  = property(lambda s: s.ifprop("slices", "NumSlice={slices}"))
@@ -194,13 +125,6 @@ class StringAPI(PropertyHandler):
   @property
   def rcmode(self):
     return f"RateControlMethod={self.map_rcmode(self.props['rcmode'])}"
-
-  @property
-  def level(self):
-    def inner(level):
-      # TODO: need to define map_level
-      return f"CodecLevel={self.map_level(self.props['level'])}"
-    return self.ifprop("level", inner)
 
   @property
   def qp(self):
@@ -259,80 +183,6 @@ class StringAPI(PropertyHandler):
         "profile3"  : CodecProfile.MFX_PROFILE_VP9_3
       },
     }[codec][profile]
-
-  @classmethod
-  @memoize
-  def map_level(cls, codec, level):
-    # TODO: we could move this method to the enum class as "lookup"
-    # then call MfxCodecProfile.lookup(codec, profile)
-    return {
-      #/* H264 levels */
-      Codec.AVC     : {
-        10  : MFX_LEVEL_AV1_1,    
-        9   : MFX_LEVEL_AV1_1b,    
-        11  : MFX_LEVEL_AV1_11,    
-        12  : MFX_LEVEL_AV1_12,    
-        13  : MFX_LEVEL_AV1_13,    
-        20  : MFX_LEVEL_AV1_2,    
-        21  : MFX_LEVEL_AV1_21,    
-        22  : MFX_LEVEL_AV1_22,    
-        30  : MFX_LEVEL_AV1_3,    
-        31  : MFX_LEVEL_AV1_31,    
-        32  : MFX_LEVEL_AV1_32,    
-        40  : MFX_LEVEL_AV1_4,    
-        41  : MFX_LEVEL_AV1_41,    
-        42  : MFX_LEVEL_AV1_42,    
-        50  : MFX_LEVEL_AV1_5,    
-        51  : MFX_LEVEL_AV1_51,    
-        52  : MFX_LEVEL_AV1_52,    
-        60  : MFX_LEVEL_AV1_6,    
-        61  : MFX_LEVEL_AV1_61,    
-        62  : MFX_LEVEL_AV1_62,    
-      },
-      #/* HEVC levels */
-      Codec.HEVC   : {
-        10  : MFX_LEVEL_HEVC_1,    
-        20  : MFX_LEVEL_HEVC_2,    
-        21  : MFX_LEVEL_HEVC_21,    
-        30  : MFX_LEVEL_HEVC_3,    
-        31  : MFX_LEVEL_HEVC_31,    
-        40  : MFX_LEVEL_HEVC_4,    
-        41  : MFX_LEVEL_HEVC_41,    
-        50  : MFX_LEVEL_HEVC_5,    
-        51  : MFX_LEVEL_HEVC_51,    
-        52  : MFX_LEVEL_HEVC_52,    
-        60  : MFX_LEVEL_HEVC_6,    
-        61  : MFX_LEVEL_HEVC_61,    
-        62  : MFX_LEVEL_HEVC_62,    
-      },
-      #/* AV1 Levels */
-      Codec.AV1 : {
-        20  : MFX_LEVEL_AV1_2,    
-        21  : MFX_LEVEL_AV1_21,    
-        22  : MFX_LEVEL_AV1_22,    
-        23  : MFX_LEVEL_AV1_23,    
-        30  : MFX_LEVEL_AV1_3,    
-        31  : MFX_LEVEL_AV1_31,    
-        32  : MFX_LEVEL_AV1_32,    
-        33  : MFX_LEVEL_AV1_33,    
-        40  : MFX_LEVEL_AV1_4,    
-        41  : MFX_LEVEL_AV1_41,    
-        42  : MFX_LEVEL_AV1_42,    
-        43  : MFX_LEVEL_AV1_43,    
-        50  : MFX_LEVEL_AV1_5,    
-        51  : MFX_LEVEL_AV1_51,    
-        52  : MFX_LEVEL_AV1_52,    
-        53  : MFX_LEVEL_AV1_53,    
-        60  : MFX_LEVEL_AV1_6,    
-        61  : MFX_LEVEL_AV1_61,    
-        62  : MFX_LEVEL_AV1_62,    
-        63  : MFX_LEVEL_AV1_63,    
-        70  : MFX_LEVEL_AV1_7,    
-        71  : MFX_LEVEL_AV1_71,    
-        72  : MFX_LEVEL_AV1_72,    
-        73  : MFX_LEVEL_AV1_73,    
-      },
-    }[codec][level]
 
   @classmethod
   @memoize
